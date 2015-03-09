f399630ca8096f54d8e097c456d63718d69892a6
Roger Waldvogel <climberwoodi@gmx.ch>
2015-03-09 12:26:27 +0100

    Show new message only when new message available

--- client/app/modules/home/homeCtrl.js
+++ client/app/modules/home/homeCtrl.js
@@ -3,7 +3,9 @@
 module.exports = ['$scope', 'Modification', function ($scope, Modification) {
 
     var modification = Modification.get({forceShowModification: true}, function () {
-        $scope.messageText = modification.numberOfMessages + ' neue Meldungen';
+        if (modification.numberOfMessages > 0) {
+            $scope.messageText = modification.numberOfMessages + ' neue Meldungen';
+        }
     });
 
     $scope.$on('message.changed', function (event, numberOfMessages) {
