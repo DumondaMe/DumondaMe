4af888b06a29e4148ba501f8bc3c51c33b2336e3
Roger Waldvogel <climberwoodi@gmx.ch>
2015-03-13 09:57:40 +0100

    CSRF Header added.

--- server/server/lib/spec.js
+++ server/server/lib/spec.js
@@ -3,6 +3,7 @@
 var express = require('express'),
     passport = require('passport'),
     methodOverride = require('method-override'),
+    csrf = require('csurf'),
     auth = require('./auth'),
     userLib = require('./user')(),
     db = require('./database'),
@@ -21,6 +22,17 @@ module.exports = function (app) {
 
     app.on('middleware:before:urlEncoded', function () {
         app.use(methodOverride('X-HTTP-Method-Override'));
+    });
+
+    app.on('middleware:after:appsec', function () {
+        var env = process.env.NODE_ENV || 'development';
+        if ('testing' !== env) {
+            app.use(csrf());
+            app.use(function (req, res, next) {
+                res.cookie('XSRF-TOKEN', req.csrfToken());
+                next();
+            });
+        }
     });
 
     return {
