ed0bda31fe3f348115ebf5d9d36742b30107a363
Roger Waldvogel <climberwoodi@gmx.ch>
2015-07-16 14:38:08 +0200

    Bugfix to make expanding text working.

--- client/app/modules/directives/expandText/link.js
+++ client/app/modules/directives/expandText/link.js
@@ -5,9 +5,9 @@ module.exports = {
         return function ($scope, element) {
 
             $scope.$watch('description', function (newDescription) {
-                    var textElement = element.find('#ely-expand-text-description');
-                    if (newDescription) {
-                        if (textElement.context.scrollHeight > textElement.innerHeight()) {
+                    var textElement = element.find('.ely-expand-text-description');
+                    if (newDescription && textElement.length > 0) {
+                        if (textElement[0].scrollHeight > textElement[0].clientHeight) {
                             $scope.showExpand = true;
                         }
                     }
