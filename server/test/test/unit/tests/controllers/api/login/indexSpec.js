7cd811817608bb5a58abe2e26a4a8e0b59da118d
Roger Waldvogel <climberwoodi@gmx.ch>
2015-07-31 16:04:29 +0200

    Bugfix of unittest.

--- server/test/test/unit/tests/controllers/api/login/indexSpec.js
+++ server/test/test/unit/tests/controllers/api/login/indexSpec.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var bluebird = require('bluebird');
-var Promise = bluebird.Promise;
 var testee = require('../../../../../../../controllers/api/login/index');
 var request = require('../../../request');
 var passport = require('passport');
-var modification = require('../../../../../../../models/modification/modification')
+var modification = require('../../../../../../../models/modification/modification');
+var loginUser = require('../../../../../../../models/user/loginUser');
+var Promise = require('bluebird').Promise;
 var sinon = require('sinon');
 var expect = require('chai').expect;
 
@@ -21,14 +21,20 @@ describe('Unit Test controllers/api/login/index', function () {
         sandbox.restore();
     });
 
-    it('Successfully login a user - Return a 200', function () {
+    it('Successfully login a user - Return a 200', function (done) {
 
         testee(request.requestMock);
         var stubResponse = sandbox.stub(request.res, 'status'),
             stubPassport = sandbox.stub(passport, 'authenticate'),
             stubLogin = sandbox.stub(request.req, 'logIn'),
-            stubModification = sandbox.stub(modification, 'initModificationOnSession');
+            stubLoginUser = sandbox.stub(loginUser, 'setTimestamp'),
+            stubModification = sandbox.stub(modification, 'initModificationOnSession', function () {
+                stubModification.callArgWith(2, undefined);
+                expect(stubResponse.withArgs(200).calledOnce).to.be.true;
+                done();
+            });
 
+        stubLoginUser.returns(Promise.resolve());
         stubPassport.returns(function () {
         });
         stubResponse.returns({
@@ -39,8 +45,30 @@ describe('Unit Test controllers/api/login/index', function () {
         request.executePostRequest(request.req, request.res);
         stubPassport.callArgWith(1, undefined, {});
         stubLogin.callArgWith(1, undefined);
-        stubModification.callArgWith(2, undefined);
-        expect(stubResponse.withArgs(200).calledOnce).to.be.true;
+    });
+
+    it('Error is caused when setting the timestamp - Return a 200', function (done) {
+
+        testee(request.requestMock);
+        var stubResponse = sandbox.stub(request.res, 'status'),
+            stubPassport = sandbox.stub(passport, 'authenticate'),
+            stubLogin = sandbox.stub(request.req, 'logIn'),
+            stubLoginUser = sandbox.stub(loginUser, 'setTimestamp');
+
+        stubLoginUser.returns(Promise.reject({}));
+        stubPassport.returns(function () {
+
+        });
+        stubResponse.returns({
+            end: function () {
+                expect(stubResponse.withArgs(500).calledOnce).to.be.true;
+                done();
+            }
+        });
+
+        request.executePostRequest(request.req, request.res);
+        stubPassport.callArgWith(1, undefined, {});
+        stubLogin.callArgWith(1, undefined);
     });
 
     it('Error authentication of user failed - Return a 500', function () {
