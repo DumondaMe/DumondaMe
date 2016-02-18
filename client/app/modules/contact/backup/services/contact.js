9e9570d2bc99b8add7034cedafaf94632b472eca
Roger Waldvogel <climberwoodi@gmx.ch>
2016-02-18 14:19:53 +0100

    Removed backup directory.

--- client/app/modules/contact/backup/services/contact.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = ['$resource', function ($resource) {
-
-    return $resource('/api/user/contact', null, {
-        'delete': {method: 'POST', headers: {'X-HTTP-Method-Override': 'DELETE'}}
-    });
-}];
--- client/app/modules/contact/backup/services/contactDetail.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = ['$resource', function ($resource) {
-
-    return $resource('/api/user/contact/detail', null, {});
-}];
