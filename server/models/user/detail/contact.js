99713f87f0dc1f7cfb8ab7f94b0eb54c72eadb6b
Roger Waldvogel <climberwoodi@gmx.ch>
2016-02-18 14:18:25 +0100

    Show user in contact preview.

--- server/models/user/detail/contact.js
+++ server/models/user/detail/contact.js
@@ -2,6 +2,7 @@
 
 var db = require('./../../../neo4j');
 var userInfo = require('../userInfo');
+var _ = require('lodash');
 
 var numberOfContacts = function (contactId) {
     return db.cypher().match('(:User {userId: {contactId}})-[:IS_CONTACT]->(:User)')
@@ -15,28 +16,36 @@ var numberOfSameContacts = function (userId, contactId) {
         .end({contactId: contactId, userId: userId});
 };
 
-var getContacts = function (userId, contactId, contactsPerPage, skipContacts) {
+var setUserImageVisible = function (userId, contacts) {
+    _.each(contacts, function (contact) {
+        if (contact.userId === userId) {
+            contact.profileVisible = true;
+            contact.imageVisible = true;
+        }
+    })
+};
+
+var getContacts = function (userId, userDetailId, contactsPerPage, skipContacts) {
     var commands = [];
 
-    commands.push(numberOfContacts(contactId).getCommand());
-    commands.push(numberOfSameContacts(userId, contactId).getCommand());
+    commands.push(numberOfContacts(userDetailId).getCommand());
+    commands.push(numberOfSameContacts(userId, userDetailId).getCommand());
 
-    return db.cypher().match('(contact:User {userId: {contactId}})-[:IS_CONTACT]->(contactOfContact:User)')
-        .with('contact, contactOfContact')
-        .where("contactOfContact.userId <> {userId}")
-        .with('contact, contactOfContact')
-        .match("(contactOfContact)-[vr:HAS_PRIVACY|HAS_PRIVACY_NO_CONTACT]->(privacy:Privacy)")
-        .optionalMatch('(contactOfContact)-[rContact:IS_CONTACT]->(:User {userId: {userId}})')
-        .with("contact, rContact, contactOfContact, privacy, vr")
+    return db.cypher().match('(:User {userId: {userDetailId}})-[:IS_CONTACT]->(contactOfUser:User)')
+        .with('contactOfUser')
+        .match("(contactOfUser)-[vr:HAS_PRIVACY|HAS_PRIVACY_NO_CONTACT]->(privacy:Privacy)")
+        .optionalMatch('(contactOfUser)-[rContact:IS_CONTACT]->(:User {userId: {userId}})')
+        .with("rContact, contactOfUser, privacy, vr")
         .where("(rContact IS NULL AND type(vr) = 'HAS_PRIVACY_NO_CONTACT') OR (rContact.type = vr.type AND type(vr) = 'HAS_PRIVACY')")
-        .return('contactOfContact.name AS name, contactOfContact.userId AS userId, privacy.profile AS profileVisible, privacy.image AS imageVisible')
+        .return('contactOfUser.name AS name, contactOfUser.userId AS userId, privacy.profile AS profileVisible, privacy.image AS imageVisible')
         .orderBy('name')
         .skip('{skipContacts}')
         .limit('{contactsPerPage}')
-        .end({contactId: contactId, userId: userId, contactsPerPage: contactsPerPage, skipContacts: skipContacts})
+        .end({userDetailId: userDetailId, userId: userId, contactsPerPage: contactsPerPage, skipContacts: skipContacts})
         .send(commands)
         .then(function (resp) {
             userInfo.addImageForPreview(resp[2]);
+            setUserImageVisible(userId, resp[2]);
             return {
                 numberOfContacts: resp[0][0].numberOfContacts,
                 numberOfSameContacts: resp[1][0].numberOfSameContacts,
@@ -47,5 +56,5 @@ var getContacts = function (userId, contactId, contactsPerPage, skipContacts) {
 
 
 module.exports = {
-    getContacts:  getContacts
+    getContacts: getContacts
 };
