17bb121d0b2c3b1321573080a699c35b33d8218b
Roger Waldvogel <climberwoodi@gmx.ch>
2016-02-29 18:08:29 +0100

    Moved old code to backup

--- client/app/modules/settings/index.js
+++ client/app/modules/settings/index.js
@@ -2,12 +2,6 @@
 
 var app = angular.module('elyoosApp');
 
-app.controller('ProfileCtrl', require('./profileCtrl'));
-app.controller('PasswordCtrl', require('./passwordCtrl'));
-app.controller('PrivacyCtrl', require('./privacyCtrl'));
-app.controller('RenamePrivacyCtrl', require('./renamePrivacyCtrl'));
-app.controller('DeletePrivacyCtrl', require('./deletePrivacyCtrl'));
-
 app.factory('Profile', require('./services/profile'));
 app.factory('Privacy', require('./services/privacy'));
 app.factory('Password', require('./services/password'));
@@ -19,12 +13,7 @@ app.config(['$stateProvider', function ($stateProvider) {
     $stateProvider
         .state('settings', {
             abstract: true,
-            url: '/settings',
-            views: {
-                header: {
-                    templateUrl: 'app/modules/navigation/loggedInHeader.html'
-                }
-            }
+            url: '/settings'
         })
         .state('settings.profile', {
             url: '/profile',
@@ -33,18 +22,7 @@ app.config(['$stateProvider', function ($stateProvider) {
                     templateUrl: 'app/modules/settings/profile.html',
                     controller: 'ProfileCtrl'
                 }
-            },
-            hasNavigation: true
-        })
-        .state('settings.profile.changePassword', {
-            url: '/newPassword',
-            views: {
-                'content@': {
-                    templateUrl: 'app/modules/settings/changePassword.html',
-                    controller: 'PasswordCtrl'
-                }
-            },
-            hasNavigation: true
+            }
         })
         .state('settings.privacy', {
             url: '/privacy',
@@ -53,7 +31,6 @@ app.config(['$stateProvider', function ($stateProvider) {
                     templateUrl: 'app/modules/settings/privacy.html',
                     controller: 'PrivacyCtrl'
                 }
-            },
-            hasNavigation: true
+            }
         });
 }]);
