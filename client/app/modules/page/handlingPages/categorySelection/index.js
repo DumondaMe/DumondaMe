1416315574081328d26bee2fccae86c8ccc764ce
Roger Waldvogel <climberwoodi@gmx.ch>
2016-03-10 16:03:31 +0100

    Old code moved to backup.

--- client/app/modules/page/handlingPages/categorySelection/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = angular.module('elyoosApp');
-var directive = require('./directive.js');
-
-app.directive(directive.name, directive.directive);
-
-app.service('PageCategoryHandler', require('./services/categoryHandler'));
-app.service('PageLoader', require('./services/pageLoader'));
--- client/app/modules/page/index.js
+++ client/app/modules/page/index.js
@@ -2,26 +2,6 @@
 
 var app = angular.module('elyoosApp');
 
-app.controller('GetPageAndExtendCtrl', require('./userRecommendation/getPageAndExtendCtrl'));
-
-app.controller('PageDetailCtrl', require('./pageDetail/pageDetailCtrl'));
-app.controller('PageDetailEducationCtrl', require('./pageDetail/pageDetailEducationCtrl'));
-app.controller('pageHeaderActivityPreviewCtrl', require('./pageDetail/pageHeaderActivityPreviewCtrl'));
-app.controller('AddRemoveRecommendationCtrl', require('./addRemoveRecommendationCtrl'));
-
-app.service('PageRecommendationAllContact', require('./services/pageRecommendationAllContact'));
-app.service('PageRecommendationOtherUser', require('./services/pageRecommendationOtherUser'));
-app.service('PageDetail', require('./services/pageDetail'));
-app.service('SearchPage', require('./services/searchPage'));
-app.service('PopularPages', require('./services/popularPages'));
-app.service('PageCategories', require('./services/categories'));
-app.service('PageUserRecommendation', require('./services/pageUserRecommendation'));
-app.service('PageUserAdministration', require('./services/pageUserAdministration'));
-app.service('PageSearchUserRecommendation', require('./services/pageSearchUserRecommendation'));
-app.service('PageSearchUserAdministratedPage', require('./services/pageSearchUserAdministratedPage'));
-app.service('PageLeftNavElements', require('./services/leftNavElements'));
-
-app.service('PageDetailReview', require('./pageDetail/services/pageDetailReview'));
 
 app.config(['$stateProvider', function ($stateProvider) {
 
@@ -37,8 +17,7 @@ app.config(['$stateProvider', function ($stateProvider) {
                 'content@': {
                     template: '<ely-page-overview></ely-page-overview>'
                 }
-            },
-            hasNavigation: true
+            }
         })
         .state('page.detail', {
             url: '/detail/{label}/{pageId}',
@@ -47,8 +26,7 @@ app.config(['$stateProvider', function ($stateProvider) {
                     templateUrl: 'app/modules/page/pageDetail/pageDetail.html',
                     controller: 'PageDetailCtrl'
                 }
-            },
-            hasNavigation: true
+            }
         })
         .state('page.edit', {
             url: '/edit/{label}/{pageId}',
@@ -56,8 +34,7 @@ app.config(['$stateProvider', function ($stateProvider) {
                 'content@': {
                     template: '<ely-page-handling-pages></ely-page-handling-pages>'
                 }
-            },
-            hasNavigation: true
+            }
         })
         .state('page.create', {
             url: '/create',
@@ -65,8 +42,7 @@ app.config(['$stateProvider', function ($stateProvider) {
                 'content@': {
                     template: '<ely-page-handling-pages></ely-page-handling-pages>'
                 }
-            },
-            hasNavigation: true
+            }
         })
         .state('page.userRecommendation', {
             url: '/user/recommendation',
@@ -74,8 +50,7 @@ app.config(['$stateProvider', function ($stateProvider) {
                 'content@': {
                     template: '<ely-page-user-recommendation></ely-page-user-recommendation>'
                 }
-            },
-            hasNavigation: true
+            }
         })
         .state('page.userPageAdmin', {
             url: '/user/admin',
@@ -83,7 +58,6 @@ app.config(['$stateProvider', function ($stateProvider) {
                 'content@': {
                     template: '<ely-page-user-administrator></ely-page-user-administrator>'
                 }
-            },
-            hasNavigation: true
+            }
         });
 }]);
