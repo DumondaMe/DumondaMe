c7a63ef86360f99365b204324cc854f7c2a7e251
Roger Waldvogel <climberwoodi@gmx.ch>
2016-03-24 11:18:42 +0100

    Delete a page when not recommendate by other users.

--- server/models/user/page/security.js
+++ server/models/user/page/security.js
@@ -6,26 +6,51 @@ var logger = requireLogger.getLogger(__filename);
 
 var checkAllowedToEditPage = function (userId, pageId, req) {
 
-    function checkUserAllowedToEditPage(resp) {
+    function userNotAllowedToEditPage(resp) {
         return resp.length === 0;
     }
-
-    var params = {
-        userId: userId,
-        pageId: pageId
-    };
 
     return db.cypher()
         .match("(page:Page {pageId: {pageId}})<-[:IS_ADMIN]-(user:User {userId: {userId}})")
         .return("user.userId AS userId")
-        .end(params).send()
+        .end({userId: userId, pageId: pageId}).send()
         .then(function (resp) {
-            if (checkUserAllowedToEditPage(resp)) {
+            if (userNotAllowedToEditPage(resp)) {
                 return exceptions.getInvalidOperation('User tried to edit page with no Admin rights ' + pageId, logger, req);
             }
         });
 };
 
+var checkAllowedToDeletePage = function (userId, pageId, req) {
+
+    var commands = [];
+
+    function userNotAdmin(resp) {
+        return resp[1].length === 0;
+    }
+
+    function pageAlreadyRated(resp) {
+        return resp[0].length > 0;
+    }
+
+    commands.push(db.cypher().match("(otherUser:User)-[:RECOMMENDS]->(:Recommendation)-[:RECOMMENDS]->(page:Page {pageId: {pageId}})")
+        .where("otherUser.userId <> {userId}").return("otherUser").end({userId: userId, pageId: pageId}).getCommand());
+
+    return db.cypher()
+        .match("(page:Page {pageId: {pageId}})<-[:IS_ADMIN]-(user:User {userId: {userId}})")
+        .return("page.pageId AS pageId")
+        .end({userId: userId, pageId: pageId}).send(commands)
+        .then(function (resp) {
+            if (userNotAdmin(resp)) {
+                return exceptions.getInvalidOperation('User tried to delete a page where he is not admin' + pageId, logger, req);
+            }
+            if (pageAlreadyRated(resp)) {
+                return exceptions.getInvalidOperation('Page is already rated by other users' + pageId, logger, req);
+            }
+        });
+};
+
 module.exports = {
-    checkAllowedToEditPage: checkAllowedToEditPage
+    checkAllowedToEditPage: checkAllowedToEditPage,
+    checkAllowedToDeletePage: checkAllowedToDeletePage
 };
---
+++ server/test/test/e2e/tests/user/page/deletePageSpec.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var users = require('../../util/user');
+var db = require('../../util/db');
+var requestHandler = require('../../util/request');
+var moment = require('moment');
+
+describe('Integration Tests for deleting a page', function () {
+
+    var requestAgent, startTime;
+
+    beforeEach(function () {
+
+        var commands = [];
+        startTime = Math.floor(moment.utc().valueOf() / 1000);
+        return db.clearDatabase().then(function () {
+            commands.push(db.cypher().create("(:User {email: 'user@irgendwo.ch', password: '$2a$10$JlKlyw9RSpt3.nt78L6VCe0Kw5KW4SPRaCGSPMmpW821opXpMgKAm', name: 'user Meier', surname: 'Meier', forename:'user', userId: '1'})").end().getCommand());
+            commands.push(db.cypher().create("(:User {name: 'user Meier2', userId: '2'})").end().getCommand());
+            commands.push(db.cypher().create("(:User {name: 'user Meier3', userId: '3'})").end().getCommand());
+            commands.push(db.cypher().create("(:User {name: 'user Meier4', userId: '4'})").end().getCommand());
+
+            commands.push(db.cypher().create("(:Page {title: 'page1Title', label: 'Book', description: 'page1', modified: 5090, pageId: '0'})").end().getCommand());
+            commands.push(db.cypher().create("(:Page {title: 'page2Title', label: 'Youtube', description: 'page2', modified: 5091, pageId: '1'})").end().getCommand());
+            commands.push(db.cypher().create("(:Page {title: 'page3Title', label: 'Youtube', description: 'page3', modified: 5092, pageId: '2'})").end().getCommand());
+
+            commands.push(db.cypher().match("(a:Page {pageId: '0'}), (b:User {userId: '1'})")
+                .create("(b)-[:IS_ADMIN]->(a)").end().getCommand());
+            commands.push(db.cypher().match("(a:Page {pageId: '1'}), (b:User {userId: '1'})")
+                .create("(b)-[:IS_ADMIN]->(a)").end().getCommand());
+            commands.push(db.cypher().match("(a:Page {pageId: '2'}), (b:User {userId: '2'})")
+                .create("(b)-[:IS_ADMIN]->(a)").end().getCommand());
+
+            commands.push(db.cypher().match("(a:Page {pageId: '0'}), (b:User {userId: '1'})")
+                .create("(b)-[:RECOMMENDS]->(:Recommendation {created: 507, recommendationId: '1', rating: 5})-[:RECOMMENDS]->(a)").end().getCommand());
+            commands.push(db.cypher().match("(a:Page {pageId: '1'}), (b:User {userId: '2'})")
+                .create("(b)-[:RECOMMENDS]->(:Recommendation {created: 508, recommendationId: '2', rating: 4})-[:RECOMMENDS]->(a)").end().getCommand());
+            
+            return db.cypher().create("(:User {name: 'user Meier5', userId: '5'})")
+                .end().send(commands);
+        });
+    });
+
+    afterEach(function () {
+        return requestHandler.logout();
+    });
+
+    it('Delete Successfully a page - Return 200', function () {
+
+        return requestHandler.login(users.validUser).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.del('/api/user/page', {pageId: '0'}, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+            return db.cypher().match("(page:Page {pageId: '0'})")
+                .return('page').end().send();
+        }).then(function (page) {
+            page.length.should.equals(0);
+            return db.cypher().match("(recommendation:Recommendation {recommendationId: '1'})")
+                .return('recommendation').end().send();
+        }).then(function (page) {
+            page.length.should.equals(0);
+        });
+    });
+
+    it('Delete a page failed because recommendation of other user exists - Return 400', function () {
+
+        return requestHandler.login(users.validUser).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.del('/api/user/page', {pageId: '1'}, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(400);
+            return db.cypher().match("(page:Page {pageId: '1'})")
+                .return('page').end().send();
+        }).then(function (page) {
+            page.length.should.equals(1);
+            return db.cypher().match("(recommendation:Recommendation {recommendationId: '2'})")
+                .return('recommendation').end().send();
+        }).then(function (page) {
+            page.length.should.equals(1);
+        });
+    });
+
+    it('Delete a page failed because user is not admin of page - Return 400', function () {
+
+        return requestHandler.login(users.validUser).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.del('/api/user/page', {pageId: '2'}, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(400);
+            return db.cypher().match("(page:Page {pageId: '2'})")
+                .return('page').end().send();
+        }).then(function (page) {
+            page.length.should.equals(1);
+        });
+    });
+    
+});
