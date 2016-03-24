1acd22eaad6be028a287ea1cc27450e2e9659a71
Roger Waldvogel <climberwoodi@gmx.ch>
2016-03-24 11:59:19 +0100

    Cypher improvement for delete request.

--- server/models/user/page/deletePage.js
+++ server/models/user/page/deletePage.js
@@ -7,9 +7,8 @@ var deletePage = function (userId, params, req) {
 
     return security.checkAllowedToDeletePage(userId, params.pageId, req).then(function () {
         return db.cypher().match("(user:User {userId: {userId}})-[isAdmin:IS_ADMIN]->(page:Page {pageId: {pageId}})")
-            .optionalMatch("(:Recommendation)-[recPinwallData:PINWALL_DATA]->(page)")
-            .optionalMatch("(user)-[recRel:RECOMMENDS]->(rec:Recommendation)-[recRel2:RECOMMENDS]->(page)")
-            .delete("isAdmin, page, recRel, rec, recRel2, recPinwallData")
+            .optionalMatch("(user)-[recRel:RECOMMENDS]->(rec:Recommendation)-[recRel2:RECOMMENDS|PINWALL_DATA]->(page)")
+            .delete("isAdmin, page, recRel, rec, recRel2")
             .end({userId: userId, pageId: params.pageId})
             .send();
     });
