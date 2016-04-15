0edb45f543b30b3ca996cbd6ac9ef2a22530e129
Roger Waldvogel <climberwoodi@gmx.ch>
2016-04-15 11:38:37 +0200

    Several small bugfixes.

--- dockerStartup.sh
+++ dockerStartup.sh
@@ -1,4 +1,4 @@
-sudo docker run --name elyoosDatabase -d --publish 7474:7474 --net="host" --volume $HOME/neo4j/data:/data --env=NEO4J_AUTH=none  neo4j/neo4j:2.3.2
-sudo docker run --name redisSessionStore -i redis:3.0.7
+sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_ALLOW_STORE_UPGRADE=true neo4j/neo4j:2.3.2
+sudo docker run --name redisSessionStore -d --net="host" redis:3.0.7
 
-sudo docker run --name elyoosWebserver -p 8080:8080 -i --net="host" jenkins/elyoos
+sudo docker run --name elyoosWebserver -d --net="host" jenkins/elyoos
