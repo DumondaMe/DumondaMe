efc332bd413538b0466cef6eb0c48ceb175d4a91
Roger Waldvogel <climberwoodi@gmx.ch>
2016-05-03 16:07:08 +0200

    Initial draft of formatting an Email when new Messages has been sent to the user.

--- server/lib/eMail/eMail.js
+++ server/lib/eMail/eMail.js
@@ -1,19 +1,41 @@
 'use strict';
 
+var path = require('path');
 var nodemailer = require('nodemailer');
+var EmailTemplate = require('email-templates').EmailTemplate;
 var sesTransport = require('nodemailer-ses-transport');
-var transporter = nodemailer.createTransport(sesTransport({rateLimit: 5, region: 'eu-west-1'}));
+var transporter = nodemailer.createTransport(sesTransport({rateLimit: 2, region: 'eu-west-1'}));
 var logger = requireLogger.getLogger(__filename);
+
+var templatesDir = path.resolve(__dirname, 'templates');
+var emailTemplates = {
+    newMessages: {
+        template: new EmailTemplate(path.join(templatesDir, 'newMessages')),
+        subject: 'Du hast neue Nachrichten'
+    }
+};
 
 
 var sendEMail = function (template, templateData, sendTo) {
-    transporter.sendMail({from: 'info@elyoos.com', to: sendTo, subject: 'Test', text: 'Hello World', html: '<b>Hello world </b>'},
-        function (error) {
+    if (emailTemplates.hasOwnProperty(template)) {
+        emailTemplates[template].template.render(templateData, function (error, results) {
             if (error) {
                 return logger.error(error);
             }
-            logger.info('Email sent to: ' + sendTo);
+            transporter.sendMail({
+                    from: 'Elyoos <info@elyoos.com>', to: sendTo, subject: emailTemplates[template].subject,
+                    text: results.text, html: results.html
+                },
+                function (error) {
+                    if (error) {
+                        return logger.error(error);
+                    }
+                    logger.info('Email sent to: ' + sendTo);
+                });
         });
+    } else {
+        logger.error("Message could not be sent. Template does not exist: " + template);
+    }
 };
 
 module.exports = {
--- server/models/emailService/jobs/messageReceivedJob.js
+++ server/models/emailService/jobs/messageReceivedJob.js
@@ -13,7 +13,8 @@ var processDefinition = function (data, done) {
         .then(function (resp) {
             if (resp[1].length === 1 && resp[1][0].hasOwnProperty('email') && resp[1][0].email.lastJobId === data.jobId) {
                 if (resp[0][0].totalUnreadMessages > 0) {
-                    email.sendEMail("newMessages", {numberOfUnreadMessages: resp[0][0].totalUnreadMessages}, resp[1][0].user.email);
+                    email.sendEMail("newMessages", {numberOfUnreadMessages: resp[0][0].totalUnreadMessages, forename: resp[1][0].user.forename}, 
+                        resp[1][0].user.email);
                 }
                 return db.cypher().match("(email:EMailNotification {userId: {userId}})").delete("email")
                     .end({userId: data.userId}).send().then(function () {
