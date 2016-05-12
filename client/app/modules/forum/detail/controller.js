ba25ebeaa9d8270b24c4749f48de3a50ad85d92a
Roger Waldvogel <climberwoodi@gmx.ch>
2016-05-12 10:25:09 +0200

    Wrap blog an messages when user used line-breaks. Removed forum temporary from navigation.

--- client/app/modules/forum/detail/controller.js
+++ client/app/modules/forum/detail/controller.js
@@ -1,10 +1,24 @@
 'use strict';
 
-module.exports = ['$stateParams', 'ForumQuestionDetail',
-    function ($stateParams, ForumQuestionDetail) {
+module.exports = ['$scope', '$stateParams', '$mdSidenav', 'ForumQuestionDetail', 'ToolbarService',
+    function ($scope, $stateParams, $mdSidenav, ForumQuestionDetail, ToolbarService) {
         var ctrl = this;
         ctrl.detail = ForumQuestionDetail.get({questionId: $stateParams.questionId}, function (resp) {
 
+        });
+
+        ctrl.openSideNavRight = function () {
+            $mdSidenav('rightQuestionNav').open();
+        };
+
+        $scope.isSideNavOpen = false;
+
+        $scope.$watch('isSideNavOpen', function (isOpen) {
+            if (isOpen) {
+                ToolbarService.disable();
+            } else {
+                ToolbarService.enable();
+            }
         });
     }];
 
