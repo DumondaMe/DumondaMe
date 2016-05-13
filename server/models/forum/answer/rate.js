1b92e2c7067893d363d8804a796717320d5977b2
Roger Waldvogel <climberwoodi@gmx.ch>
2016-05-13 08:44:48 +0200

    Positive rate a forum answer implemented.

---
+++ server/models/forum/answer/rate.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var db = require('./../../../neo4j');
+var moment = require('moment');
+var exceptions = require('./../../../lib/error/exceptions');
+var logger = requireLogger.getLogger(__filename);
+
+var rateExists = function (userId, answerId, req) {
+    return db.cypher().match("(:User {userId: {userId}})-[:RATE_POSITIVE]->(:ForumAnswer {answerId: {answerId}})")
+        .return("COUNT(*) AS numberOfAnswers").end({userId: userId, answerId: answerId}).send().then(function (resp) {
+            if (resp[0].numberOfAnswers > 0) {
+                return exceptions.getInvalidOperation("Rate for answer " + answerId + " exists already", logger, req);
+            }
+        });
+};
+
+var ratePositive = function (userId, answerId, req) {
+    
+    return rateExists(userId, answerId, req).then(function () {
+        var timeCreatedRating = Math.floor(moment.utc().valueOf() / 1000);
+        return db.cypher().match("(u:User {userId: {userId}}), (answer:ForumAnswer {answerId: {answerId}})")
+            .createUnique("(u)-[:RATE_POSITIVE {created: {timeCreatedRating}}]->(answer)")
+            .end({
+                userId: userId,
+                timeCreatedRating: timeCreatedRating,
+                answerId: answerId
+            })
+            .send();
+    });
+};
+
+
+module.exports = {
+    ratePositive: ratePositive
+};
---
+++ server/test/test/e2e/tests/user/forum/question/rateAnswerSpec.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var users = require('../../../util/user');
+var db = require('../../../util/db');
+var requestHandler = require('../../../util/request');
+var moment = require('moment');
+
+describe('Integration Tests for rating answers of a forum question', function () {
+
+    var requestAgent, startTime;
+
+    beforeEach(function () {
+        
+        startTime = Math.floor(moment.utc().valueOf() / 1000);
+        return db.clearDatabase().then(function () {
+            var commands = [];
+            commands.push(db.cypher().create("(:User {email: 'user@irgendwo.ch', password: '$2a$10$JlKlyw9RSpt3.nt78L6VCe0Kw5KW4SPRaCGSPMmpW821opXpMgKAm', name: 'user Meier', forename: 'user', surname: 'Meier', userId: '1'})")
+                .end().getCommand());
+
+            commands.push(db.cypher().match("(u:User {userId: '1'})")
+                .create("(u)-[:IS_ADMIN]->(:ForumQuestion {questionId: '0', description: 'forumQuestion', category: {category}, language: 'de'})")
+                .end({category: ['environmental']}).getCommand());
+
+            return db.cypher().match("(u:User {userId: '1'}), (forumQuestion:ForumQuestion {questionId: '0'})")
+                .create("(u)-[:IS_ADMIN]->(:ForumAnswer:ForumExplanation {answerId: '0', description: 'forumAnswer'})<-[:IS_ANSWER]-(forumQuestion)")
+                .end().send(commands);
+
+        });
+    });
+
+    afterEach(function () {
+        return requestHandler.logout();
+    });
+
+    it('Positive rate a answer - Return 200', function () {
+
+        return requestHandler.login(users.validUser).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.post('/api/user/forum/question/answer/rate', {
+                answerId: '0'
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+            return db.cypher().match("(answer:ForumAnswer {answerId: '0'})" +
+                "<-[:RATE_POSITIVE]-(:User {userId: '1'})")
+                .return('answer').end().send();
+        }).then(function (answer) {
+            answer.length.should.equals(1);
+        });
+    });
+
+    it('Positive rate a answer twice. Second rate is dismissed- Return 200', function () {
+
+        return requestHandler.login(users.validUser).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.post('/api/user/forum/question/answer/rate', {
+                answerId: '0'
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+            return requestHandler.post('/api/user/forum/question/answer/rate', {
+                answerId: '0'
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(400);
+            return db.cypher().match("(answer:ForumAnswer {answerId: '0'})" +
+                "<-[:RATE_POSITIVE]-(:User {userId: '1'})")
+                .return('answer').end().send();
+        }).then(function (answer) {
+            answer.length.should.equals(1);
+        });
+    });
+
+});
