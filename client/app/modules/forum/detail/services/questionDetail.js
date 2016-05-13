4f6bbbc212731099eed2378c68689167f45f6b62
Roger Waldvogel <climberwoodi@gmx.ch>
2016-05-13 09:23:55 +0200

    Api user to forum changed.

--- client/app/modules/forum/detail/services/questionDetail.js
+++ client/app/modules/forum/detail/services/questionDetail.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = ['$resource', function ($resource) {
-    return $resource('api/user/forum/question/detail');
+    return $resource('api/forum/question/detail');
 }];
