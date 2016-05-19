9e61310526274954d8889fba00f63ebdd6972937
Roger Waldvogel <climberwoodi@gmx.ch>
2016-05-19 14:41:16 +0200

    title length 160 and description 3000.

--- server/controllers/api/user/page/schema/schemaDefinitions.js
+++ server/controllers/api/user/page/schema/schemaDefinitions.js
@@ -5,7 +5,7 @@ var category = require("../../../../schema/category");
 module.exports = {
     id: {type: 'string', format: 'notEmptyString', maxLength: 30},
     category: category.category,
-    title: {type: 'string', format: 'notEmptyString', maxLength: 255},
-    description: {type: 'string', format: 'notEmptyString', maxLength: 10000},
+    title: {type: 'string', format: 'notEmptyString', maxLength: 160},
+    description: {type: 'string', format: 'notEmptyString', maxLength: 3000},
     link: {type: 'string', format: 'url', maxLength: 1000}
 };
