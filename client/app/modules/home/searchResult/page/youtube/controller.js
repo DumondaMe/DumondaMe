7995d3382a19f1e4ff5bf684a05595be9f13372c
Roger Waldvogel <climberwoodi@gmx.ch>
2016-05-27 12:26:34 +0200

    Show image preview instead of embbeding youtube.

--- client/app/modules/home/searchResult/page/youtube/controller.js
+++ client/app/modules/home/searchResult/page/youtube/controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
-module.exports = ['$state', 'Categories', function ($state, Categories) {
+module.exports = ['$state', 'Categories', 'YoutubeThumbnail', function ($state, Categories, YoutubeThumbnail) {
     var ctrl = this;
 
     ctrl.getCategory = Categories.getCategory;
     ctrl.getCategoryClass = Categories.getCategoryClass;
+
+    ctrl.getYoutubeImage = YoutubeThumbnail.getImage;
 
     ctrl.goToDetail = function () {
         $state.go('page.detail', {label: ctrl.page.label, pageId: ctrl.page.pageId});
