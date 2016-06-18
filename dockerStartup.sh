416349586c1b31bd96a69c57b8358406661930c4
Roger Waldvogel <climberwoodi@gmx.ch>
2016-06-18 11:54:31 +0200

    Use for neo4j database exact version number.

--- dockerStartup.sh
+++ dockerStartup.sh
@@ -1,4 +1,4 @@
-sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=200M --env=NEO4J_dbms_memory_heap_maxSize=400 neo4j:3.0
+sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=200M --env=NEO4J_dbms_memory_heap_maxSize=400 neo4j:3.0.2
 sudo docker run --name redisSessionStore -d --net="host" redis:3.0.7
 
 docker build -t jenkins/elyoos .
