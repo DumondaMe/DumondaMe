b13ec372b0cff279ebcf52059eb027d6db450b6a
Roger Waldvogel <climberwoodi@gmx.ch>
2016-07-02 14:29:45 +0200

    Added restart on failure.

--- dockerStartup.sh
+++ dockerStartup.sh
@@ -1,8 +1,8 @@
-sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=200M --env=NEO4J_dbms_memory_heap_maxSize=400 neo4j:3.0.2
-sudo docker run --name redisSessionStore -d --net="host" redis:3.0.7
+sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=200M --env=NEO4J_dbms_memory_heap_maxSize=400 --restart on-failure:10 neo4j:3.0.3
+sudo docker run --name redisSessionStore -d --net="host" --restart on-failure:10 redis:3.0.7
 
 docker build -t jenkins/elyoos .
-sudo docker run --name elyoosWebserver -d --net="host" jenkins/elyoos
+sudo docker run --name elyoosWebserver -d --restart on-failure:10 --net="host" jenkins/elyoos
 
 //Remove old docker images
 docker rm $(docker ps -a -q)
