51e49e4f5c987829d933c2e104f40460dadeb5dc
Roger Waldvogel <climberwoodi@gmx.ch>
2016-07-21 14:58:51 +0200

    Added test for hide image of recommended user.

--- server/models/user/pinwall/recommendedUser.js
+++ server/models/user/pinwall/recommendedUser.js
@@ -3,13 +3,14 @@
 var db = require('./../../../neo4j');
 
 var getRecommendedByContactUsers = function (userId) {
-    return db.cypher().match(`(user:User {userId: {userId}})-[:IS_CONTACT]->(:User)-[:IS_CONTACT]->(contactedUser:User)
-                              -[:HAS_PRIVACY_NO_CONTACT]->(privacyNoContact:Privacy)`)
+    return db.cypher().match(`(user:User {userId: {userId}})-[:IS_CONTACT]->(:User)-[:IS_CONTACT]->(contactedUser:User)`)
         .where("contactedUser.userId <> user.userId AND NOT (user)-[:IS_CONTACT]->(contactedUser)")
         .optionalMatch("(user)<-[relContactedUser:IS_CONTACT]-(contactedUser)-[privacyRel:HAS_PRIVACY]->(privacy:Privacy)")
         .where("privacyRel.type = relContactedUser.type")
+        .optionalMatch("(contactedUser:User)-[:HAS_PRIVACY_NO_CONTACT]->(privacyNoContact:Privacy)")
+        .where("NOT EXISTS((user)<-[:IS_CONTACT]-(contactedUser))")
         .with("user, contactedUser, privacyNoContact, relContactedUser, privacy, count(contactedUser) AS numberOfContacting")
-        .where(`NOT EXISTS((user)<-[:IS_CONTACT]-(contactedUser)) OR relContactedUser.contactAdded >= user.previousLastLogin`)
+        .where(`NOT EXISTS((user)<-[:IS_CONTACT]-(contactedUser)) OR relContactedUser.contactAdded < user.previousLastLogin`)
         .return(`numberOfContacting, contactedUser.userId AS userId, contactedUser.name AS name, 
                  privacy.profile AS profileVisible, privacy.image AS imageVisible, privacyNoContact.profile AS profileVisibleNoContact, 
                  privacyNoContact.image AS imageVisibleNoContact`)
@@ -19,14 +20,15 @@ var getRecommendedByContactUsers = function (userId) {
 };
 
 var getRecommendedUsers = function (userId) {
-    return db.cypher().match(`(contactingUser:User)-[:IS_CONTACT]->(contactedUser:User)-[:HAS_PRIVACY_NO_CONTACT]->(privacyNoContact:Privacy), 
-                              (user:User {userId: {userId}})`)
+    return db.cypher().match(`(contactingUser:User)-[:IS_CONTACT]->(contactedUser:User), (user:User {userId: {userId}})`)
         .where(`NOT (user)-[:IS_CONTACT]->()-[:IS_CONTACT]->(contactedUser) AND NOT (user)-[:IS_CONTACT]->(contactedUser) AND 
                 contactingUser.userId <> user.userId AND contactedUser.userId <> user.userId`)
         .optionalMatch("(user)<-[relContactedUser:IS_CONTACT]-(contactedUser)-[privacyRel:HAS_PRIVACY]->(privacy:Privacy)")
         .where("privacyRel.type = relContactedUser.type")
+        .optionalMatch("(contactedUser:User)-[:HAS_PRIVACY_NO_CONTACT]->(privacyNoContact:Privacy)")
+        .where("NOT EXISTS((user)<-[:IS_CONTACT]-(contactedUser))")
         .with("user, contactedUser, privacyNoContact, relContactedUser, privacy, count(contactedUser) AS numberOfContacting")
-        .where(`NOT EXISTS((user)<-[:IS_CONTACT]-(contactedUser)) OR relContactedUser.contactAdded >= user.previousLastLogin`)
+        .where(`NOT EXISTS((user)<-[:IS_CONTACT]-(contactedUser)) OR relContactedUser.contactAdded < user.previousLastLogin`)
         .return(`numberOfContacting, contactedUser.userId AS userId, contactedUser.name AS name, 
                  privacy.profile AS profileVisible, privacy.image AS imageVisible, privacyNoContact.profile AS profileVisibleNoContact, 
                  privacyNoContact.image AS imageVisibleNoContact`)
--- server/test/test/e2e/tests/user/home/homeRecommendedUserSpec.js
+++ server/test/test/e2e/tests/user/home/homeRecommendedUserSpec.js
@@ -106,40 +106,6 @@ describe('Integration Tests for getting contact recommendation on the home scree
         });
     });
 
-    /*it('Contact Recommendation for user with contacts (ignore contacting user)', function () {
-
-        dbDsl.createPrivacyNoContact(null, {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
-        dbDsl.createPrivacy(null, 'Freund', {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
-
-        dbDsl.createContactConnection('2', '3', 'Freund');
-        dbDsl.createContactConnection('8', '3', 'Freund');
-        dbDsl.createContactConnection('7', '3', 'Freund');
-        dbDsl.createContactConnection('6', '3', 'Freund');
-        dbDsl.createContactConnection('3', '1', 'Freund');
-
-        dbDsl.createContactConnection('1', '5', 'Freund');
-        dbDsl.createContactConnection('5', '2', 'Freund');
-        dbDsl.createContactConnection('5', '2', 'Freund');
-
-        return dbDsl.sendToDb().then(function () {
-            return requestHandler.login(users.validUser);
-        }).then(function (agent) {
-            requestAgent = agent;
-            return requestHandler.getWithData('/api/user/home', {
-                skipBlog: 0,
-                skipRecommendation: 0,
-                maxItems: 10
-            }, requestAgent);
-        }).then(function (res) {
-            res.status.should.equal(200);
-
-            res.body.recommendedUser.length.should.equals(1);
-            res.body.recommendedUser[0].userId.should.equals('2');
-            res.body.recommendedUser[0].name.should.equals('user Meier2');
-            res.body.recommendedUser[0].profileUrl.should.equals('profileImage/2/thumbnail.jpg');
-        });
-    });*/
-
     it('Contact Recommendation for user with contacts (ignore contacts of user)', function () {
 
         dbDsl.createPrivacyNoContact(null, {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
@@ -185,18 +151,18 @@ describe('Integration Tests for getting contact recommendation on the home scree
         dbDsl.createPrivacy(null, 'Freund', {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
 
         dbDsl.createContactConnection('2', '3', 'Freund');
-        dbDsl.createContactConnection('3', '1', 'Freund', 5001);
+        dbDsl.createContactConnection('3', '1', 'Freund', 4999);
 
         dbDsl.createContactConnection('1', '4', 'Freund');
         dbDsl.createContactConnection('4', '5', 'Freund');
-        dbDsl.createContactConnection('5', '1', 'Freund', 5001);
+        dbDsl.createContactConnection('5', '1', 'Freund', 4999);
 
         dbDsl.createContactConnection('6', '7', 'Freund');
-        dbDsl.createContactConnection('7', '1', 'Freund', 4999);
+        dbDsl.createContactConnection('7', '1', 'Freund', 5001);
 
         dbDsl.createContactConnection('1', '8', 'Freund');
         dbDsl.createContactConnection('8', '9', 'Freund');
-        dbDsl.createContactConnection('9', '1', 'Freund', 4999);
+        dbDsl.createContactConnection('9', '1', 'Freund', 5001);
 
         return dbDsl.sendToDb().then(function () {
             return requestHandler.login(users.validUser);
@@ -218,6 +184,146 @@ describe('Integration Tests for getting contact recommendation on the home scree
             res.body.recommendedUser[1].userId.should.equals('3');
             res.body.recommendedUser[1].name.should.equals('user Meier3');
             res.body.recommendedUser[1].profileUrl.should.equals('profileImage/3/thumbnail.jpg');
+        });
+    });
+
+    it('Hide recommended contact image (no contact relations to user, profile=false)', function () {
+
+        dbDsl.createPrivacyNoContact(null, {profile: false, image: true, profileData: true, contacts: true, pinwall: true});
+        dbDsl.createPrivacy(null, 'Freund', {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
+
+        dbDsl.createContactConnection('2', '3', 'Freund');
+
+        dbDsl.createContactConnection('1', '4', 'Freund');
+        dbDsl.createContactConnection('4', '5', 'Freund');
+
+        return dbDsl.sendToDb().then(function () {
+            return requestHandler.login(users.validUser);
+        }).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.getWithData('/api/user/home', {
+                skipBlog: 0,
+                skipRecommendation: 0,
+                maxItems: 10
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+
+            res.body.recommendedUser.length.should.equals(2);
+            res.body.recommendedUser[0].userId.should.equals('5');
+            res.body.recommendedUser[0].name.should.equals('user Meier5');
+            res.body.recommendedUser[0].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+
+            res.body.recommendedUser[1].userId.should.equals('3');
+            res.body.recommendedUser[1].name.should.equals('user Meier3');
+            res.body.recommendedUser[1].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+        });
+    });
+
+    it('Hide recommended contact image (no contact relations to user, image=false)', function () {
+
+        dbDsl.createPrivacyNoContact(null, {profile: true, image: false, profileData: true, contacts: true, pinwall: true});
+        dbDsl.createPrivacy(null, 'Freund', {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
+
+        dbDsl.createContactConnection('2', '3', 'Freund');
+
+        dbDsl.createContactConnection('1', '4', 'Freund');
+        dbDsl.createContactConnection('4', '5', 'Freund');
+
+        return dbDsl.sendToDb().then(function () {
+            return requestHandler.login(users.validUser);
+        }).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.getWithData('/api/user/home', {
+                skipBlog: 0,
+                skipRecommendation: 0,
+                maxItems: 10
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+
+            res.body.recommendedUser.length.should.equals(2);
+            res.body.recommendedUser[0].userId.should.equals('5');
+            res.body.recommendedUser[0].name.should.equals('user Meier5');
+            res.body.recommendedUser[0].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+
+            res.body.recommendedUser[1].userId.should.equals('3');
+            res.body.recommendedUser[1].name.should.equals('user Meier3');
+            res.body.recommendedUser[1].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+        });
+    });
+
+    it('Hide recommended contact image (contact relations to user, profile=false)', function () {
+
+        dbDsl.setUserLastLoginTime(5000);
+
+        dbDsl.createPrivacyNoContact(null, {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
+        dbDsl.createPrivacy(['3', '5'], 'Freund', {profile: false, image: true, profileData: true, contacts: true, pinwall: true});
+
+        dbDsl.createContactConnection('2', '3', 'Freund');
+        dbDsl.createContactConnection('3', '1', 'Freund', 4999);
+
+        dbDsl.createContactConnection('1', '4', 'Freund');
+        dbDsl.createContactConnection('4', '5', 'Freund');
+        dbDsl.createContactConnection('5', '1', 'Freund', 4999);
+
+        return dbDsl.sendToDb().then(function () {
+            return requestHandler.login(users.validUser);
+        }).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.getWithData('/api/user/home', {
+                skipBlog: 0,
+                skipRecommendation: 0,
+                maxItems: 10
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+
+            res.body.recommendedUser.length.should.equals(2);
+            res.body.recommendedUser[0].userId.should.equals('5');
+            res.body.recommendedUser[0].name.should.equals('user Meier5');
+            res.body.recommendedUser[0].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+
+            res.body.recommendedUser[1].userId.should.equals('3');
+            res.body.recommendedUser[1].name.should.equals('user Meier3');
+            res.body.recommendedUser[1].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+        });
+    });
+
+    it('Hide recommended contact image (contact relations to user, image=false)', function () {
+
+        dbDsl.setUserLastLoginTime(5000);
+
+        dbDsl.createPrivacyNoContact(null, {profile: true, image: true, profileData: true, contacts: true, pinwall: true});
+        dbDsl.createPrivacy(['3', '5'], 'Freund', {profile: true, image: false, profileData: true, contacts: true, pinwall: true});
+
+        dbDsl.createContactConnection('2', '3', 'Freund');
+        dbDsl.createContactConnection('3', '1', 'Freund', 4999);
+
+        dbDsl.createContactConnection('1', '4', 'Freund');
+        dbDsl.createContactConnection('4', '5', 'Freund');
+        dbDsl.createContactConnection('5', '1', 'Freund', 4999);
+
+        return dbDsl.sendToDb().then(function () {
+            return requestHandler.login(users.validUser);
+        }).then(function (agent) {
+            requestAgent = agent;
+            return requestHandler.getWithData('/api/user/home', {
+                skipBlog: 0,
+                skipRecommendation: 0,
+                maxItems: 10
+            }, requestAgent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+
+            res.body.recommendedUser.length.should.equals(2);
+            res.body.recommendedUser[0].userId.should.equals('5');
+            res.body.recommendedUser[0].name.should.equals('user Meier5');
+            res.body.recommendedUser[0].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
+
+            res.body.recommendedUser[1].userId.should.equals('3');
+            res.body.recommendedUser[1].name.should.equals('user Meier3');
+            res.body.recommendedUser[1].profileUrl.should.equals('profileImage/default/thumbnail.jpg');
         });
     });
 });
