08bd5e5ae14d144659082bbbfdb7de4bb29524f6
Roger Waldvogel <climberwoodi@gmx.ch>
2016-07-26 14:59:03 +0200

    Bugfix: Invalid date when open second time fixed. Removed gender.

--- client/app/modules/contact/detail/modal/userInfo/controller.js
+++ client/app/modules/contact/detail/modal/userInfo/controller.js
@@ -4,16 +4,10 @@ module.exports = ['ElyModal', 'moment', 'CountryCodeConverter',
     function (ElyModal, moment, CountryCodeConverter) {
         var ctrl = this;
 
-        if (ctrl.detail.female) {
-            ctrl.gender = 'Frau';
-        } else {
-            ctrl.gender = 'Mann';
-        }
-
         if (ctrl.detail.hasOwnProperty('country')) {
             ctrl.detail.country = CountryCodeConverter.getCountry(ctrl.detail.country);
         }
-        if (ctrl.detail.hasOwnProperty('birthday')) {
+        if (ctrl.detail.hasOwnProperty('birthday') && angular.isNumber(ctrl.detail.birthday)) {
             ctrl.detail.birthday = moment.unix(ctrl.detail.birthday).format('l');
         }
 
