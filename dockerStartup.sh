1aab8f1de3a2f1e145f68924c7ee6642082dbf5d
Roger Waldvogel <climberwoodi@gmx.ch>
2016-09-12 10:42:43 +0200

    Redis is now using version 3.2.3.

--- dockerStartup.sh
+++ dockerStartup.sh
@@ -1,5 +1,5 @@
 sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=200M --env=NEO4J_dbms_memory_heap_maxSize=400 --restart on-failure:10 neo4j:3.0.3
-sudo docker run --name redisSessionStore -d --net="host" --restart on-failure:10 redis:3.0.7
+sudo docker run --name redisSessionStore -d --net="host" --restart on-failure:10 redis:3.2.3
 
 docker build -t jenkins/elyoos .
 sudo docker run --name elyoosWebserver -d --restart on-failure:10 --net="host" jenkins/elyoos
