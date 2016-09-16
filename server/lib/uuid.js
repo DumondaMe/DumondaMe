b742a80a21776916ac4468edafc677313191d4e0
Roger Waldvogel <climberwoodi@gmx.ch>
2016-09-16 10:24:10 +0200

    Create random values for worker and datacenter.

--- server/lib/uuid.js
+++ server/lib/uuid.js
@@ -3,7 +3,7 @@
 var intformat = require('biguint-format');
 var FlakeId = require('flake-idgen');
 
-var flakeIdGen = new FlakeId();
+var flakeIdGen = new FlakeId({datacenter: Math.floor(Math.random() * 31) + 1, worker: Math.floor(Math.random() * 31) + 1});
 
 var generateUUID = function () {
     return intformat(flakeIdGen.next(), 'hex');
