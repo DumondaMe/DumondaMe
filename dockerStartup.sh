c9f4b80d7b96b55d3388ce1d0ea617383d0fb2c5
Roger Waldvogel <climberwoodi@gmx.ch>
2016-09-16 18:08:44 +0200

    Settings for integration test with jenkins.

--- dockerStartup.sh
+++ dockerStartup.sh
@@ -1,5 +1,5 @@
-sudo docker run --name elyoosDatabase -d --net="host" --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=200M --env=NEO4J_dbms_memory_heap_maxSize=400 --restart on-failure:10 neo4j:3.0.3
-sudo docker run --name redisSessionStore -d --net="host" --restart on-failure:10 redis:3.2.3
+sudo docker run --name elyoosDatabase -d --publish 7476:7474 --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=100M --env=NEO4J_dbms_memory_heap_maxSize=400 --restart on-failure:10 neo4j:3.0.3
+sudo docker run --name redisSessionStore -d --publish 6379:6379 --restart on-failure:10 redis:3.2.3
 
 docker build -t jenkins/elyoos .
 sudo docker run --name elyoosWebserver -d --restart on-failure:10 --net="host" jenkins/elyoos
