d03cb6e7a3662738cb16a0f28d11832629ae7b80
Roger Waldvogel <climberwoodi@gmx.ch>
2016-09-17 17:57:35 +0200

    Backup script for neo4j in production

---
+++ server/db/production/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PATH=/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
+neo4j-backup -host 127.0.0.1 -to /mnt/backup/neo4j-backup
+cd /mnt/backup/
+time=$(date +"%Y_%d_%m__%H_%M_%S")
+tar -zcf $time.tar.gz neo4j-backup/
+aws s3 cp $time.tar.gz s3://elyoosbackup/neo4j/
+rm -r neo4j-backup
+rm $time.tar.gz
