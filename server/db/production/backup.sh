cce30deeb4fabe670f85f50414266d66d5f1de19
Roger Waldvogel <climberwoodi@gmx.ch>
2016-09-17 18:01:27 +0200

    Changed position of day and month.

--- server/db/production/backup.sh
+++ server/db/production/backup.sh
@@ -3,7 +3,7 @@ PATH=/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr
 
 neo4j-backup -host 127.0.0.1 -to /mnt/backup/neo4j-backup
 cd /mnt/backup/
-time=$(date +"%Y_%d_%m__%H_%M_%S")
+time=$(date +"%Y_%m_%d__%H_%M_%S")
 tar -zcf $time.tar.gz neo4j-backup/
 aws s3 cp $time.tar.gz s3://elyoosbackup/neo4j/
 rm -r neo4j-backup
