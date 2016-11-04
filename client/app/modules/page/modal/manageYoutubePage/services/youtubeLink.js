eb6359424e0b2520ddfb2296ee35c069e553d16c
Roger Waldvogel <climberwoodi@gmx.ch>
2016-11-04 10:36:42 +0100

    Updated Test depedencies. Bugfix: YoutubeLink is now able to handle mobile links.

--- client/app/modules/page/modal/manageYoutubePage/services/youtubeLink.js
+++ client/app/modules/page/modal/manageYoutubePage/services/youtubeLink.js
@@ -3,11 +3,34 @@
 var getListElement = function (appendix) {
     var result = '';
     angular.forEach(appendix, function (appendixElement) {
-        if(appendixElement.indexOf('list=') !== -1) {
+        if (appendixElement.indexOf('list=') !== -1) {
             result = appendixElement;
         }
     });
     return result;
+};
+
+var getIdElement = function (appendix) {
+    var result = '';
+    angular.forEach(appendix, function (appendixElement) {
+        if (appendixElement.indexOf('v=') !== -1) {
+            result = appendixElement.replace('v=', '');
+        }
+    });
+    return result;
+};
+
+var getBaseUrl = function (link) {
+    if (link.indexOf('https://www.youtube.com/watch?v=') !== -1 && link.indexOf('list=') < 0) {
+        return 'https://www.youtube.com/watch?v=';
+    } else if(link.indexOf('https://www.youtube.com/watch?') !== -1) {
+        return 'https://www.youtube.com/watch?';
+    } else if(link.indexOf('https://m.youtube.com/watch?v=') !== -1 && link.indexOf('list=') < 0) {
+        return 'https://m.youtube.com/watch?v=';
+    } else if(link.indexOf('https://m.youtube.com/watch?') !== -1) {
+        return 'https://m.youtube.com/watch?';
+    }
+    return '';
 };
 
 module.exports = [
@@ -17,7 +40,7 @@ module.exports = [
         ctrl.isValidYoutubeLink = function (link) {
             var isValidLink = false;
             if (angular.isString(link)) {
-                if (link.indexOf('https://www.youtube.com/watch?v=') !== -1) {
+                if (link.indexOf('https://www.youtube.com/watch?') !== -1 || link.indexOf('https://m.youtube.com/watch?') !== -1) {
                     isValidLink = true;
                 }
             }
@@ -25,15 +48,16 @@ module.exports = [
         };
 
         ctrl.getYoutubeLink = function (link) {
-            var appendixLinkList;
+            var appendixLinkList, baseUrl, result = null;
             if (ctrl.isValidYoutubeLink(link)) {
-                if (link.indexOf('&list=') !== -1) {
-                    appendixLinkList = link.replace('https://www.youtube.com/watch?v=', '').split('&');
-                    link = 'https://www.youtube.com/embed/' + appendixLinkList[0] + '?' + getListElement(appendixLinkList);
+                baseUrl = getBaseUrl(link);
+                if (link.indexOf('list=') !== -1) {
+                    appendixLinkList = link.replace(baseUrl, '').split('&');
+                    result = 'https://www.youtube.com/embed/' + getIdElement(appendixLinkList) + '?' + getListElement(appendixLinkList);
                 } else {
-                    link = link.replace('https://www.youtube.com/watch?v=', 'https://www.youtube.com/embed/');
+                    result = link.replace(baseUrl, 'https://www.youtube.com/embed/');
                 }
             }
-            return link;
+            return result;
         };
     }];
