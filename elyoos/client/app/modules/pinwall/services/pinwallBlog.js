579941d57edb8f500bf7a242015a7783180ccc7a
Roger Waldvogel <climberwoodi@gmx.ch>
2016-12-15 13:35:16 +0100

    Removed PinwallBlogService in scroll request dependency.

--- elyoos/client/app/modules/pinwall/services/pinwallBlog.js
+++ elyoos/client/app/modules/pinwall/services/pinwallBlog.js
@@ -1,19 +1,15 @@
 'use strict';
 
-module.exports = [
-    function () {
-        var requestService = {}, pinwall = [];
-
-        this.registerRequestService = function (newRequestService) {
-            requestService = newRequestService;
-        };
+module.exports = ['HomeScrollRequest',
+    function (HomeScrollRequest) {
+        var pinwall = [];
 
         this.addPinwall = function (newPinwall) {
             pinwall = newPinwall;
         };
 
         this.removePinwallElement = function () {
-            return requestService.removedPinwallElement().then(function (newPinwall) {
+            return HomeScrollRequest.removedPinwallElement().then(function (newPinwall) {
                 pinwall.splice(0,pinwall.length); //Empty Array
                 angular.forEach(newPinwall.pinwall, function (pinwallElement, index) {
                     pinwall[index] = pinwallElement;
@@ -23,11 +19,11 @@ module.exports = [
 
         this.addBlog = function (blog) {
             pinwall.unshift(blog);
-            requestService.addedBlog();
+            HomeScrollRequest.addedBlog();
         };
 
         this.addRecommendation = function (recommendation) {
             pinwall.unshift(recommendation);
-            requestService.addedRecommendation();
+            HomeScrollRequest.addedRecommendation();
         };
     }];
