bea968c93285b23608fe97cc56a01d1dbb418b9f
Roger Waldvogel <climberwoodi@gmx.ch>
2016-12-15 15:45:51 +0100

    Implemented feedback overview.

---
+++ elyoosAdmin/server/models/feedback/overview.js
@@ -0,0 +1,87 @@
+'use strict';
+
+let db = requireDb();
+let _ = require('underscore');
+
+let getNumberOfOpenFeedback = function () {
+    return db.cypher().match(`(feedback:Feedback {status: 'open'})`)
+        .where(`feedback:Discussion OR feedback:Idea OR feedback:Bug`)
+        .return("count(feedback) AS numberOfOpenFeedback").end();
+};
+
+let getOverviewOfFeedback = function (params) {
+    return db.cypher().match(`(feedback:Feedback)`)
+        .where("NOT feedback:Discussion")
+        .optionalMatch("(feedbackParent)<-[:RECOMMENDS|COMMENT]-(feedback)")
+        .where("(feedback:Comment OR feedback:Recommendation)")
+        .with("feedback, feedbackParent")
+        .optionalMatch("(discussionOfParent:Feedback:Discussion)<-[:IS_IDEA]-(feedbackParent)")
+        .optionalMatch("(discussion:Feedback:Discussion)<-[:IS_IDEA]-(feedback)")
+        .return("feedback, feedbackParent, discussion, discussionOfParent, LABELS(feedback) AS label")
+        .orderBy("feedback.created DESC").skip("{skip}").limit("{maxItems}").end(params);
+};
+
+let getTypeIdea = function (feedback) {
+    let formattedFeedback = {};
+    formattedFeedback.title = feedback.feedback.title;
+    formattedFeedback.created = feedback.feedback.created;
+    formattedFeedback.feedbackId = feedback.feedback.feedbackId;
+    formattedFeedback.status = feedback.feedback.status;
+    formattedFeedback.type = 'idea';
+    if (_.contains(feedback.label, 'Bug')) {
+        formattedFeedback.type = 'bug';
+    }
+    if (_.contains(feedback.label, 'DiscussionIdea')) {
+        formattedFeedback.status = feedback.discussion.status;
+    }
+    return formattedFeedback;
+};
+
+let getTypeRecommendationAndComment = function (feedback) {
+    let formattedFeedback = {};
+    formattedFeedback.title = feedback.feedbackParent.title;
+    formattedFeedback.created = feedback.feedback.created;
+    formattedFeedback.feedbackId = feedback.feedbackParent.feedbackId;
+    formattedFeedback.status = feedback.feedbackParent.status;
+    formattedFeedback.type = 'recommendation';
+    if (_.contains(feedback.label, 'Comment')) {
+        formattedFeedback.type = 'comment';
+        formattedFeedback.text = feedback.feedback.text;
+    }
+    if (feedback.hasOwnProperty('discussionOfParent')) {
+        formattedFeedback.status = feedback.discussionOfParent.status;
+    }
+    return formattedFeedback;
+};
+
+let getFeedback = function (feedbacks) {
+    let formattedFeedbacks = [];
+    feedbacks.forEach(function (feedback) {
+        let formattedFeedback;
+        if (_.contains(feedback.label, 'Bug') || _.contains(feedback.label, 'Idea') || _.contains(feedback.label, 'DiscussionIdea')) {
+            formattedFeedback = getTypeIdea(feedback);
+        } else if (_.contains(feedback.label, 'Recommendation') || _.contains(feedback.label, 'Comment')) {
+            formattedFeedback = getTypeRecommendationAndComment(feedback);
+        }
+        formattedFeedbacks.push(formattedFeedback);
+    });
+    return formattedFeedbacks;
+};
+
+let getOverview = function (params) {
+
+    let commands = [];
+
+    commands.push(getOverviewOfFeedback(params).getCommand());
+
+    return getNumberOfOpenFeedback().send(commands).then(function (resp) {
+        return {
+            numberOfOpenFeedback: resp[1][0].numberOfOpenFeedback, feedback: getFeedback(resp[0])
+        };
+    });
+};
+
+
+module.exports = {
+    getOverview: getOverview
+};
---
+++ elyoosAdmin/server/test/test/e2e/tests/feedback/overviewSpec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+let users = require('elyoos-server-test-util').user;
+let dbDsl = require('elyoos-server-test-util').dbDSL;
+let requestHandler = require('elyoos-server-test-util').requestHandler;
+
+describe('Integration Tests for getting feedback overview', function () {
+
+    beforeEach(function () {
+        return dbDsl.init(3, true);
+    });
+
+    afterEach(function () {
+        return requestHandler.logout();
+    });
+
+
+    it('Getting feedback overview', function () {
+
+        dbDsl.createFeedbackBug('1', '1', 500);
+        dbDsl.createFeedbackIdea('2', '3', 506, 506, 'closed');
+
+        dbDsl.createFeedbackDiscussion('3', '1', 499);
+        dbDsl.createFeedbackDiscussion('4', '2', 508, 508, 'closed');
+
+        dbDsl.createFeedbackDiscussionIdea('5', '3', '1', 509);
+        dbDsl.createFeedbackDiscussionIdea('6', '4', '2', 510);
+
+        dbDsl.createFeedbackComment('6', '7', '3', 511);
+
+        dbDsl.createFeedbackRecommendation('1', '8', '2', 512);
+
+        return dbDsl.sendToDb().then(function () {
+            return requestHandler.login(users.validUser);
+        }).then(function (agent) {
+            return requestHandler.getWithData('/api/feedback/overview', {skip: 0, maxItems: 10}, agent);
+        }).then(function (res) {
+            res.status.should.equal(200);
+            res.body.numberOfOpenFeedback.should.equals(2);
+
+            res.body.feedback.length.should.equals(6);
+
+            res.body.feedback[0].title.should.equals("bug1Title");
+            res.body.feedback[0].created.should.equals(512);
+            res.body.feedback[0].feedbackId.should.equals("1");
+            res.body.feedback[0].status.should.equals("open");
+            res.body.feedback[0].type.should.equals("recommendation");
+
+            res.body.feedback[1].title.should.equals("discussionIdea6Title");
+            res.body.feedback[1].text.should.equals("comment7Text");
+            res.body.feedback[1].created.should.equals(511);
+            res.body.feedback[1].feedbackId.should.equals("6");
+            res.body.feedback[1].status.should.equals("closed");
+            res.body.feedback[1].type.should.equals("comment");
+
+            res.body.feedback[2].title.should.equals("discussionIdea6Title");
+            res.body.feedback[2].created.should.equals(510);
+            res.body.feedback[2].feedbackId.should.equals("6");
+            res.body.feedback[2].status.should.equals("closed");
+            res.body.feedback[2].type.should.equals("idea");
+
+            res.body.feedback[3].title.should.equals("discussionIdea5Title");
+            res.body.feedback[3].created.should.equals(509);
+            res.body.feedback[3].feedbackId.should.equals("5");
+            res.body.feedback[3].status.should.equals("open");
+            res.body.feedback[3].type.should.equals("idea");
+
+            res.body.feedback[4].title.should.equals("idea2Title");
+            res.body.feedback[4].created.should.equals(506);
+            res.body.feedback[4].feedbackId.should.equals("2");
+            res.body.feedback[4].status.should.equals("closed");
+            res.body.feedback[4].type.should.equals("idea");
+
+            res.body.feedback[5].title.should.equals("bug1Title");
+            res.body.feedback[5].created.should.equals(500);
+            res.body.feedback[5].feedbackId.should.equals("1");
+            res.body.feedback[5].status.should.equals("open");
+            res.body.feedback[5].type.should.equals("bug");
+        });
+    });
+});
