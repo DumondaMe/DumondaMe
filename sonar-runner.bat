97245871349a4e2a61de33c5b25b6f924d7c35cc
Roger Waldvogel <climberwoodi@gmx.ch>
2016-12-23 08:33:48 +0100

    Using pull and copy folders.

--- sonar-runner.bat
+++ sonar-runner.bat
@@ -1,12 +1,20 @@
+CALL git pull
 cd elyoos/client
+CALL npm update
 CALL grunt coverage
 cd ../server
+CALL npm update
+CALL npm install ../../commonLib/serverLib
+CALL npm install ../../commonLib/serverTestUtil
 CALL grunt coverage
 CALL grunt analysis
 cd ../../elyoosAdmin/client
+CALL npm update
 CALL grunt coverage
 cd ../server
+CALL npm update
+CALL npm install ../../commonLib/serverLib
+CALL npm install ../../commonLib/serverTestUtil
 CALL grunt coverage
 CALL grunt analysis
 cd ../../
-
