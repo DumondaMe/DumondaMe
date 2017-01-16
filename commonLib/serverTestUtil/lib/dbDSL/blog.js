6c94ca1e5228ec9ba20e15a94d815a508f203564
Roger Waldvogel <climberwoodi@gmx.ch>
2017-01-16 19:19:07 +0100

    Adding modification.

--- commonLib/serverTestUtil/lib/dbDSL/blog.js
+++ commonLib/serverTestUtil/lib/dbDSL/blog.js
@@ -3,14 +3,14 @@
 let db = require('../db');
 let dbConnectionHandling = require('./dbConnectionHandling');
 
-let createBlog = function (pageId, writerUserId, language, topic, created, visible, pictureHeight, title) {
+let createBlog = function (pageId, writerUserId, language, topic, created, visible, pictureHeight, title, modified = null) {
     title = title || `blog${pageId}Title`;
     dbConnectionHandling.getCommands().push(db.cypher().match('(user:User {userId: {writerUserId}})')
-        .create(`(:Blog:Page:PinwallElement {text: {text}, title: {title}, created: {created}, pageId: {pageId}, label: 'Blog',
+        .create(`(:Blog:Page:PinwallElement {text: {text}, title: {title}, created: {created}, modified: {modified}, pageId: {pageId}, label: 'Blog',
                                            heightPreviewImage: {pictureHeight}, topic: {topic}, language: {language}, visible: {visible}})
                                            <-[:WRITTEN]-(user)`)
         .end({
-            title: title, text: `blog${pageId}Text`, topic: topic, created: created, pageId: pageId,
+            title: title, text: `blog${pageId}Text`, topic: topic, created: created, modified: modified, pageId: pageId,
             language: language, visible: visible, pictureHeight: pictureHeight, writerUserId: writerUserId
         }).getCommand());
 };
