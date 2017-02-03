7a2b235dac062924b41e76fb5bf7b355f86b1eb2
Roger Waldvogel <climberwoodi@gmx.ch>
2017-02-03 11:25:06 +0100

    Initial implementation of getting event overview. Without filter.

--- commonLib/serverTestUtil/lib/dbDSL/events.js
+++ commonLib/serverTestUtil/lib/dbDSL/events.js
@@ -7,11 +7,11 @@ let createPageEventNewAddress = function (pageId, eventData, address) {
     address.description = address.description || null;
     dbConnectionHandling.getCommands().push(db.cypher().match(`(page:Page {pageId: {pageId}})`)
         .create(`(page)-[:EVENT]->(:Event {eventId: {eventId}, title: {title}, description: {description}, startDate: {startDate}, 
-                  endDate: {endDate}})-[:HAS]->(:Address {addressId: {addressId}, description: {addressDescription},
+                  endDate: {endDate}})-[:HAS]->(:Address {addressId: {addressId}, address: {address}, description: {addressDescription},
                   latitude: {addressLatitude}, longitude: {addressLongitude}})`)
         .end({
             pageId: pageId, eventId: eventData.eventId, title: eventData.title, description: eventData.description, startDate: eventData.startDate,
-            endDate: eventData.endDate, addressId: address.addressId, addressDescription: address.description,
+            endDate: eventData.endDate, addressId: address.addressId, addressDescription: address.description, address: address.address,
             addressLatitude: address.lat, addressLongitude: address.lng,
         }).getCommand());
 };
