46e93ad83eabc6e88dfa49fb6f202f3e8e93e19e
Roger Waldvogel <climberwoodi@gmx.ch>
2017-02-04 15:28:22 +0100

    Change name of address implemented.

--- elyoos/client/app/modules/page/modal/manageAddress/directive/controller.js
+++ elyoos/client/app/modules/page/modal/manageAddress/directive/controller.js
@@ -7,22 +7,32 @@ module.exports = ['AddressSuggestion', function (AddressSuggestion) {
 
     if (ctrl.isEditMode) {
         ctrl.selectedAddress = ctrl.actualAddress;
+        ctrl.actualAddressCompare = angular.copy(ctrl.actualAddress);
         ctrl.description = angular.copy(ctrl.actualDescription);
     }
 
+    ctrl.addressSearchChanged = function () {
+        ctrl.addressSearchIsValid = false;
+        if (angular.isString(ctrl.addressSearch) && ctrl.addressSearch.trim().length >= 3 && ctrl.addressSearch.trim().length <= 150) {
+            ctrl.addressSearchIsValid = true;
+        }
+    };
+
     ctrl.searchAddress = function () {
-        ctrl.requestStarted = true;
-        ctrl.addresses = AddressSuggestion.query({address: ctrl.data.address}, function () {
-            ctrl.requestStarted = false;
-            if (ctrl.addresses.length > 0) {
-                ctrl.lastRequestString = ctrl.data.address;
-                if (!ctrl.isEditMode) {
-                    ctrl.selectedAddress = ctrl.addresses[0];
+        if (angular.isString(ctrl.addressSearch) && ctrl.addressSearch.trim() !== "") {
+            ctrl.requestStarted = true;
+            ctrl.addresses = AddressSuggestion.query({address: ctrl.addressSearch}, function () {
+                ctrl.requestStarted = false;
+                if (ctrl.addresses.length > 0) {
+                    ctrl.lastRequestString = ctrl.addressSearch;
+                    if (!ctrl.isEditMode) {
+                        ctrl.selectedAddress = ctrl.addresses[0];
+                    }
                 }
-            }
-        }, function () {
-            ctrl.requestStarted = false;
-        });
+            }, function () {
+                ctrl.requestStarted = false;
+            });
+        }
     };
 
     ctrl.keyPressed = function ($event) {
@@ -35,9 +45,7 @@ module.exports = ['AddressSuggestion', function (AddressSuggestion) {
         ctrl.onSelected(ctrl.selectedAddress, ctrl.description);
         delete ctrl.selectedAddress;
         delete ctrl.lastRequestString;
-        if (ctrl.data) {
-            delete ctrl.data.address;
-        }
+        delete ctrl.addressSearch;
         ctrl.addresses = [];
     };
 }];
