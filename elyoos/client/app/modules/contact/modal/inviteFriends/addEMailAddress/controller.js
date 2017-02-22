dff1c6a6d2a08f05bf7e1addb6a2f9184db9cc46
Roger Waldvogel <climberwoodi@gmx.ch>
2017-02-22 15:54:30 +0100

    Email suggestion implemented.

--- elyoos/client/app/modules/contact/modal/inviteFriends/addEMailAddress/controller.js
+++ elyoos/client/app/modules/contact/modal/inviteFriends/addEMailAddress/controller.js
@@ -8,12 +8,18 @@ module.exports = ['CheckEmailAddresses', 'EmailAddressParser', function (CheckEm
     ctrl.emailChanged = function () {
         ctrl.emailAddressIsValid = CheckEmailAddresses.checkAddresses(ctrl.newEmailAddresses);
         ctrl.addEmailAddressForm.emailAddresses.$setValidity('ely-custom-message', ctrl.emailAddressIsValid);
-        if(ctrl.emailAddressIsValid) {
+        if (ctrl.emailAddressIsValid) {
             ctrl.overviewValidEmails = EmailAddressParser.parse(ctrl.newEmailAddresses);
         }
     };
 
     ctrl.addEmailAddresses = function () {
         ctrl.addedEmails(EmailAddressParser.parse(ctrl.newEmailAddresses));
+    };
+
+    ctrl.replaceEmailAddress = function (email) {
+        ctrl.newEmailAddresses = ctrl.newEmailAddresses.replace(email.email, email.suggestion);
+        email.email = email.suggestion;
+        delete email.suggestion;
     };
 }];
--- elyoos/client/app/modules/contact/modal/inviteFriends/addEMailAddress/services/emailAddressParser.js
+++ elyoos/client/app/modules/contact/modal/inviteFriends/addEMailAddress/services/emailAddressParser.js
@@ -1,12 +1,18 @@
 'use strict';
 
-module.exports = [function () {
+module.exports = ['Mailcheck', function (Mailcheck) {
 
     var format = function (addresses) {
         var result = [];
         addresses.forEach(function (address) {
             if (address.trim() !== '') {
-                result.push({email: address.trim(), manuallyAdded: true});
+                var suggestion = null;
+                Mailcheck.run({
+                    email: address.trim(), suggested: function (mailcheckSuggestion) {
+                        suggestion = mailcheckSuggestion.full;
+                    }
+                });
+                result.push({email: address.trim(), manuallyAdded: true, suggestion: suggestion});
             }
         });
         return result;
