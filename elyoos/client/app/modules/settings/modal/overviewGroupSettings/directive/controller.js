e52e42d565d01f5113b464495217ee948f2e1705
Roger Waldvogel <climberwoodi@gmx.ch>
2017-03-04 16:32:35 +0100

    Upload of new privacy settings implemented.

--- elyoos/client/app/modules/settings/modal/overviewGroupSettings/directive/controller.js
+++ elyoos/client/app/modules/settings/modal/overviewGroupSettings/directive/controller.js
@@ -1,20 +1,47 @@
 'use strict';
 
-module.exports = ['ElyModal', 'Privacy', 'PrivacySettingService', function (ElyModal, Privacy, PrivacySettingService) {
-    var ctrl = this;
+module.exports = ['$scope', 'ElyModal', 'Privacy', 'PrivacySettingService', 'errorToast',
+    function ($scope, ElyModal, Privacy, PrivacySettingService, errorToast) {
+        var ctrl = this, originalSettings;
+        ctrl.uploadStarted = true;
 
-    ctrl.cancel = function () {
-        ElyModal.cancel();
-    };
+        ctrl.cancel = function () {
+            ElyModal.cancel();
+        };
 
-    ctrl.settings = Privacy.get({}, function () {
-        ctrl.uploadStarted = false;
-        ctrl.groupNames = PrivacySettingService.getGroupNames(ctrl.settings.group);
-        ctrl.groupNamesProfileVisible = angular.copy(ctrl.groupNames);
-    });
+        $scope.settings = Privacy.get({}, function () {
+            ctrl.uploadStarted = false;
+            originalSettings = angular.copy($scope.settings);
+            ctrl.groupNames = PrivacySettingService.getGroupNames($scope.settings.group);
+            ctrl.groupNamesProfileVisible = angular.copy(ctrl.groupNames);
+        });
 
-    ctrl.profileVisibleChanged = function (selected) {
-        ctrl.settings = PrivacySettingService.setPrivacySettingProfileVisible(ctrl.settings, selected);
-        PrivacySettingService.setDisabled(ctrl.settings, ctrl.groupNames);
-    };
-}];
+        ctrl.profileVisibleChanged = function (selected) {
+            $scope.settings = PrivacySettingService.setPrivacySettingProfileVisible($scope.settings, selected);
+            PrivacySettingService.setDisabled($scope.settings, ctrl.groupNames);
+        };
+
+        ctrl.upload = function () {
+            ctrl.uploadStarted = true;
+            let newSettings = {
+                changePrivacySetting: {
+                    group: $scope.settings.group,
+                    noContact: $scope.settings.noContact
+                }
+            };
+            Privacy.save(newSettings, function () {
+                ctrl.uploadStarted = false;
+                originalSettings = angular.copy($scope.settings);
+                ctrl.uploadDisabled = true;
+            }, function () {
+                errorToast.showError('Es ist ein Fehler aufgetreten!');
+                ctrl.uploadStarted = false;
+            });
+        };
+
+        $scope.$watchCollection('settings', function (newSettings) {
+            if (newSettings && angular.isArray(newSettings.group)) {
+                ctrl.uploadDisabled = angular.equals(newSettings, originalSettings);
+            }
+        });
+    }];
