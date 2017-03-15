db7a30d996ccfa58b589eae0ea307a735bebfa51
Roger Waldvogel <climberwoodi@gmx.ch>
2017-03-15 11:32:16 +0100

    Mobile ready design implemented.

--- elyoos/client/app/modules/public/controller.js
+++ elyoos/client/app/modules/public/controller.js
@@ -1,7 +1,13 @@
 'use strict';
 
-module.exports = ['$state', '$stateParams', 'ElyModal',
-    function ($state, $stateParams, ElyModal) {
+module.exports = ['$state', '$stateParams', 'ElyModal', '$mdMedia',
+    function ($state, $stateParams, ElyModal, $mdMedia) {
+        var ctrl = this;
+        if ($mdMedia('gt-xs')) {
+            ctrl.screenPreviewImage = "app/img/start/screenPreview.jpg";
+        } else {
+            ctrl.screenPreviewImage = "app/img/start/screenPreviewMobile.jpg";
+        }
 
         if ($state.is('public.register.verify') && angular.isString($stateParams.linkId)) {
             ElyModal.show('LoginCtrl', 'app/modules/public/login/template.html', {});
