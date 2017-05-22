988c1d8b51908f0b671f4949c33013945bf3e1d9
Roger Waldvogel <climberwoodi@gmx.ch>
2017-05-22 16:20:24 +0200

    Neo4j update to version 3.2.0

--- documentation/setup/docker/dockerStartup.sh
+++ documentation/setup/docker/dockerStartup.sh
@@ -1,4 +1,4 @@
-sudo docker run --name elyoosDatabase -d --publish 7476:7474 --publish 7688:7687 --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=100M --env=NEO4J_dbms_memory_heap_maxSize=400 --restart on-failure:10 neo4j:3.1.1
+sudo docker run --name elyoosDatabase -d --publish 7476:7474 --publish 7688:7687 --volume /var/lib/neo4j/data:/data --env=NEO4J_AUTH=none --env=NEO4J_dbms_allowFormatMigration=true --env=NEO4J_dbms_memory_pagecache_size=100M --env=NEO4J_dbms_memory_heap_maxSize=400 --restart on-failure:10 neo4j:3.2.0
 sudo docker run --name redisSessionStore -d --publish 6379:6379 --restart on-failure:10 redis:3.2.3
 
 docker build -t jenkins/elyoos .
