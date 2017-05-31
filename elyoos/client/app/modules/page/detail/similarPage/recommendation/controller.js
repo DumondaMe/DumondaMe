a65b7a4a56cd1faafeed869fc9be411853ae1d3a
Roger Waldvogel <climberwoodi@gmx.ch>
2017-05-31 11:08:14 +0200

    Handling when error occurs.

--- elyoos/client/app/modules/page/detail/similarPage/recommendation/controller.js
+++ elyoos/client/app/modules/page/detail/similarPage/recommendation/controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-module.exports = ['PageRecommendation', 'BlogRecommendation', 'SimilarPageHandlingRecommendation',
-    function (PageRecommendation, BlogRecommendation, SimilarPageHandlingRecommendation) {
+module.exports = ['PageRecommendation', 'BlogRecommendation', 'SimilarPageHandlingRecommendation', 'errorToast',
+    function (PageRecommendation, BlogRecommendation, SimilarPageHandlingRecommendation, errorToast) {
         var ctrl = this;
 
         ctrl.addRecommendation = function () {
@@ -15,6 +15,9 @@ module.exports = ['PageRecommendation', 'BlogRecommendation', 'SimilarPageHandli
                 ctrl.pageRecommended = true;
                 SimilarPageHandlingRecommendation.pageRecommended();
                 ctrl.numberOfRecommendations++;
+            }, function () {
+                ctrl.uploadRunning = false;
+                errorToast.showError("Fehler beim empfehlen des Beitrages");
             });
         };
     }];
