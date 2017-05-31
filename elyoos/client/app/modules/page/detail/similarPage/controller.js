942dec2a40f7c76fb8f94b5a475bb270ccfa6341
Roger Waldvogel <climberwoodi@gmx.ch>
2017-05-31 09:29:45 +0200

    Improved visual appearance.

--- elyoos/client/app/modules/page/detail/similarPage/controller.js
+++ elyoos/client/app/modules/page/detail/similarPage/controller.js
@@ -2,8 +2,21 @@
 
 module.exports = ['SimilarPages',
     function (SimilarPages) {
-        var ctrl = this;
+        var ctrl = this, skip = 0;
+        ctrl.similarPages = {pages: []};
 
-        ctrl.similarPages = SimilarPages.get({pageId: ctrl.pageId, skip: 0, maxItems: 4});
+        ctrl.nextSimilarPages = function (maxItems) {
+            ctrl.requestRunning = true;
+            ctrl.requestPages = SimilarPages.get({pageId: ctrl.pageId, skip: skip, maxItems: maxItems}, function () {
+                ctrl.requestRunning = false;
+                ctrl.similarPages.pages = ctrl.similarPages.pages.concat(ctrl.requestPages.pages);
+                skip += ctrl.requestPages.pages.length;
+                if(ctrl.requestPages.pages.length < maxItems) {
+                    ctrl.hideMoreButton = true;
+                }
+            });
+        };
+
+        ctrl.nextSimilarPages(4);
     }];
 
