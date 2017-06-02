dd494cb6b51c2c719071aa0b47249459a832ff46
Roger Waldvogel <climberwoodi@gmx.ch>
2017-06-02 08:51:38 +0200

    Removed unused cookie tests.

--- elyoos/server/test/test/unit/tests/controllers/indexSpec.js
+++ elyoos/server/test/test/unit/tests/controllers/indexSpec.js
@@ -1,10 +1,7 @@
 'use strict';
 
 let testee = require('../../../../../controllers/index');
-let user = require('../../../../../models/user/user');
 let request = require('./../request');
-let bluebird = require('bluebird');
-let Promise = bluebird.Promise;
 let sinon = require('sinon');
 let expect = require('chai').expect;
 
@@ -20,58 +17,12 @@ describe('Unit Test controllers/index', function () {
         sandbox.restore();
     });
 
-    it('Return the index file with empty username for the cookie', function (done) {
-
-
-        testee(request.requestMock);
-        let stubCookie = sandbox.stub(request.res, 'cookie'),
-            stubSendfile = sandbox.stub(request.res, 'sendFile');
-
-        request.executeGetRequest(request.req, request.res);
-
-        expect(stubCookie.withArgs('user', '{"username":""}').calledOnce).to.be.true;
-        expect(stubSendfile.calledOnce).to.be.true;
-        done();
-    });
-
-    it('Return the index file with empty username for the cookie (Only session)', function (done) {
+    it('Return the index file', function (done) {
 
         testee(request.requestMock);
-        let stubCookie = sandbox.stub(request.res, 'cookie'),
-            stubSendfile = sandbox.stub(request.res, 'sendFile');
-        request.req = {session: {}};
+        let stubSendfile = sandbox.stub(request.res, 'sendFile');
 
         request.executeGetRequest(request.req, request.res);
-
-        expect(stubCookie.withArgs('user', '{"username":""}').calledOnce).to.be.true;
-        expect(stubSendfile.calledOnce).to.be.true;
-        done();
-    });
-
-    it('Return the index file with empty username for the cookie (Only session.passport)', function (done) {
-
-        testee(request.requestMock);
-        let stubCookie = sandbox.stub(request.res, 'cookie'),
-            stubSendfile = sandbox.stub(request.res, 'sendFile');
-        request.req = {session: {passport: {}}};
-
-        request.executeGetRequest(request.req, request.res);
-
-        expect(stubCookie.withArgs('user', '{"username":""}').calledOnce).to.be.true;
-        expect(stubSendfile.calledOnce).to.be.true;
-        done();
-    });
-
-    it('Return the index file with session username for the cookie', function (done) {
-
-        testee(request.requestMock);
-        let stubCookie = sandbox.stub(request.res, 'cookie'),
-            stubSendfile = sandbox.stub(request.res, 'sendFile');
-        request.req = {session: {passport: {user: 'user'}}};
-
-        request.executeGetRequest(request.req, request.res);
-
-        expect(stubCookie.withArgs('user', '{"username":"user"}').calledOnce).to.be.true;
         expect(stubSendfile.calledOnce).to.be.true;
         done();
     });
