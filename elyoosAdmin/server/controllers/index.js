5a5c8388d2198821a25c14eedb9798c80346b868
Roger Waldvogel <climberwoodi@gmx.ch>
2017-06-02 08:56:00 +0200

    Removed unused cookie.

--- elyoosAdmin/server/controllers/index.js
+++ elyoosAdmin/server/controllers/index.js
@@ -6,13 +6,6 @@ let logger = require('elyoos-server-lib').logging.getLogger(__filename);
 module.exports = function (router) {
 
     router.get('/*', function (req, res) {
-        let username = '';
-        if (req.session && req.session.passport && req.session.passport.user) {
-            username = req.session.passport.user;
-        }
-        res.cookie('user', JSON.stringify({
-            'username': username
-        }));
         res.sendFile(path.join(process.env.BASE_DIR, '../client/app/', 'index.html'));
         logger.info('Request of client code', req);
     });
