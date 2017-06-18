6ad5faaeb6494c054268c8d32084e5874758987d
Roger Waldvogel <climberwoodi@gmx.ch>
2017-06-18 18:01:50 +0200

    Using for serving static images s3 bucket. Removed pictures from git versioning.

--- elyoos/client/app/modules/public/controller.js
+++ elyoos/client/app/modules/public/controller.js
@@ -4,9 +4,9 @@ module.exports = ['$state', '$stateParams', 'ElyModal', '$mdMedia',
     function ($state, $stateParams, ElyModal, $mdMedia) {
         var ctrl = this;
         if ($mdMedia('gt-xs')) {
-            ctrl.screenPreviewImage = "app/img/start/screenPreview.jpg";
+            ctrl.screenPreviewImage = "https://s3.eu-central-1.amazonaws.com/elyoos.static/img/start/screenPreview.jpg";
         } else {
-            ctrl.screenPreviewImage = "app/img/start/screenPreviewMobile.jpg";
+            ctrl.screenPreviewImage = "https://s3.eu-central-1.amazonaws.com/elyoos.static/img/start/screenPreviewMobile.jpg";
         }
 
         if ($state.is('public.register.verify') && angular.isString($stateParams.linkId)) {
