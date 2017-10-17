7e3498d24508bcbcde0dd05f99a44c4ce24aa9c8
Roger Waldvogel <info@elyoos.org>
2017-10-17 13:38:47 +0200

    Update do async/await. Set EXPORT_TO_TC_PENDING when organisation changed.

--- elyoos/server/models/user/page/edit/genericPage.js
+++ elyoos/server/models/user/page/edit/genericPage.js
@@ -6,26 +6,29 @@ let image = require('./../../images/uploadImageCDN');
 let imagePage = require('./../imagePage');
 let security = require('./../security');
 
-let editGenericPage = function (userId, params, titlePicturePath, req) {
+let transitionConnectPending = function (pageId) {
+    return db.cypher().match(`(page:Page {pageId: {pageId}})<-[:EXPORT_TO_TC]-(export:TransitionConnectExport)`)
+        .merge(`(page)<-[:EXPORT_TO_TC_PENDING]-(export)`)
+        .end({pageId: pageId}).getCommand();
+};
 
-    return imagePage.checkImageSize(titlePicturePath, req).then(function () {
-        return security.checkAllowedToEditPage(userId, params.pageId, req);
-    }).then(function () {
-        return db.cypher().match("(page:Page {pageId: {pageId}})")
-            .set('page', {
-                topic: params.topic,
-                title: params.title,
-                description: params.description,
-                website: params.website,
-                language: params.language,
-                modified: time.getNowUtcTimestamp()
-            })
-            .end({pageId: params.pageId}).send();
-    }).then(function () {
-        if (typeof titlePicturePath === 'string' && titlePicturePath.trim() !== '') {
-            return image.uploadImage(titlePicturePath, 'pages', params.pageId, 450, 1000);
-        }
-    });
+let editGenericPage = async function (userId, params, titlePicturePath, req) {
+
+    await imagePage.checkImageSize(titlePicturePath, req);
+    await security.checkAllowedToEditPage(userId, params.pageId, req);
+    await db.cypher().match("(page:Page {pageId: {pageId}})")
+        .set('page', {
+            topic: params.topic,
+            title: params.title,
+            description: params.description,
+            website: params.website,
+            language: params.language,
+            modified: time.getNowUtcTimestamp()
+        })
+        .end({pageId: params.pageId}).send([transitionConnectPending(params.pageId)]);
+    if (typeof titlePicturePath === 'string' && titlePicturePath.trim() !== '') {
+        await image.uploadImage(titlePicturePath, 'pages', params.pageId, 450, 1000);
+    }
 };
 
 module.exports = {
--- elyoos/server/test/test/e2e/tests/user/page/editGenericPageSpec.js
+++ elyoos/server/test/test/e2e/tests/user/page/editGenericPageSpec.js
@@ -13,33 +13,30 @@ describe('Integration Tests for editing generic pages', function () {
 
     let startTime;
 
-    beforeEach(function () {
+    beforeEach(async function () {
 
         startTime = Math.floor(moment.utc().valueOf() / 1000);
-        return dbDsl.init(2).then(function () {
+        await dbDsl.init(2);
 
-            dbDsl.createGenericPage('1', {adminId: '1', language: ['de'], topic: ['health', 'personalDevelopment'], created: 100, website: 'www.example.org'}, [{
-                address: 'Zuerich',
-                lat: 47.376887,
-                lng: 8.541694,
-                addressId: '1'
-            }]);
-            dbDsl.createGenericPage('2', {adminId: '2', language: ['de'], topic: ['health', 'personalDevelopment'], created: 200, website: 'www.example2.org'}, [{
-                address: 'Zuerich1',
-                lat: 47.376887,
-                lng: 8.541694,
-                addressId: '2'
-            }]);
-
-            return dbDsl.sendToDb();
-        });
+        dbDsl.createGenericPage('1', {adminId: '1', language: ['de'], topic: ['health', 'personalDevelopment'], created: 100, website: 'www.example.org'}, [{
+            address: 'Zuerich',
+            lat: 47.376887,
+            lng: 8.541694,
+            addressId: '1'
+        }]);
+        dbDsl.createGenericPage('2', {adminId: '2', language: ['de'], topic: ['health', 'personalDevelopment'], created: 200, website: 'www.example2.org'}, [{
+            address: 'Zuerich1',
+            lat: 47.376887,
+            lng: 8.541694,
+            addressId: '2'
+        }]);
     });
 
     afterEach(function () {
         return requestHandler.logout();
     });
 
-    it('Edit a generic page without website - Return 200', function () {
+    it('Edit a generic page without website - Return 200', async function () {
         let editPage = {
             genericPage: {
                 pageId: '1',
@@ -51,35 +48,34 @@ describe('Integration Tests for editing generic pages', function () {
         };
         stubCDN.uploadFile.reset();
 
-        return requestHandler.login(users.validUser).then(function () {
-            return requestHandler.post('/api/user/page/edit', editPage);
-        }).then(function (res) {
-            res.status.should.equal(200);
-            return db.cypher().match("(page:Page {pageId: '1'})")
-                .return(`page`)
-                .end().send();
-        }).then(function (page) {
-            page.length.should.equals(1);
-            page[0].page.created.should.equals(100);
-            page[0].page.modified.should.be.at.least(startTime);
-            page[0].page.pageId.should.equals('1');
-            page[0].page.description.should.equals("description");
-            page[0].page.title.should.equals("title");
-            page[0].page.label.should.equals("Generic");
-            should.not.exist(page[0].page.website);
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await  requestHandler.post('/api/user/page/edit', editPage);
+        res.status.should.equal(200);
 
-            stubCDN.uploadFile.called.should.be.false;
+        let page = await db.cypher().match("(page:Page {pageId: '1'})")
+            .return(`page`)
+            .end().send();
+        page.length.should.equals(1);
+        page[0].page.created.should.equals(100);
+        page[0].page.modified.should.be.at.least(startTime);
+        page[0].page.pageId.should.equals('1');
+        page[0].page.description.should.equals("description");
+        page[0].page.title.should.equals("title");
+        page[0].page.label.should.equals("Generic");
+        should.not.exist(page[0].page.website);
 
-            page[0].page.topic.length.should.equals(2);
-            page[0].page.topic[0].should.equals('health');
-            page[0].page.topic[1].should.equals('socialDevelopment');
-            page[0].page.language.length.should.equals(2);
-            page[0].page.language[0].should.equals('en');
-            page[0].page.language[1].should.equals('fr');
-        });
+        stubCDN.uploadFile.called.should.be.false;
+
+        page[0].page.topic.length.should.equals(2);
+        page[0].page.topic[0].should.equals('health');
+        page[0].page.topic[1].should.equals('socialDevelopment');
+        page[0].page.language.length.should.equals(2);
+        page[0].page.language[0].should.equals('en');
+        page[0].page.language[1].should.equals('fr');
     });
 
-    it('Edit a generic page with website - Return 200', function () {
+    it('Edit a generic page with website - Return 200', async function () {
         let editPage = {
             genericPage: {
                 pageId: '1',
@@ -91,25 +87,61 @@ describe('Integration Tests for editing generic pages', function () {
             }
         };
         stubCDN.uploadFile.reset();
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/test.jpg');
+        res.status.should.equal(200);
 
-        return requestHandler.login(users.validUser).then(function () {
-            return requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/test.jpg');
-        }).then(function (res) {
-            res.status.should.equal(200);
-            return db.cypher().match("(page:Page {pageId: '1'})")
-                .return(`page`)
-                .end().send();
-        }).then(function (page) {
-            page.length.should.equals(1);
-            page[0].page.website.should.equals('www.elyoos.org');
+        let page = await db.cypher().match("(page:Page {pageId: '1'})")
+            .where(`NOT (page)<-[:EXPORT_TO_TC_PENDING]-(:TransitionConnectExport)`)
+            .return(`page`)
+            .end().send();
+        page.length.should.equals(1);
+        page[0].page.website.should.equals('www.elyoos.org');
 
-            stubCDN.uploadFile.calledWith(sinon.match.any, "pages/1/preview.jpg").should.be.true;
-            stubCDN.uploadFile.calledWith(sinon.match.any, "pages/1/normal.jpg").should.be.true;
-            stubCDN.uploadFile.calledWith(sinon.match.any, "pages/1/thumbnail.jpg").should.be.true;
-        });
+        stubCDN.uploadFile.calledWith(sinon.match.any, "pages/1/preview.jpg").should.be.true;
+        stubCDN.uploadFile.calledWith(sinon.match.any, "pages/1/normal.jpg").should.be.true;
+        stubCDN.uploadFile.calledWith(sinon.match.any, "pages/1/thumbnail.jpg").should.be.true;
     });
 
-    it('Edit of generic page without being administrator - Return 400', function () {
+    it('Edit a generic page does set EXPORT_TO_TC_PENDING', async function () {
+        let editPage = {
+            genericPage: {
+                pageId: '1',
+                topic: ['health', 'socialDevelopment'],
+                title: 'title',
+                description: 'description',
+                language: ['en', 'fr']
+            }
+        };
+
+        dbDsl.createTransitionConnectExportNode();
+        dbDsl.exportOrganizationToTransitionConnect({pageId: '1'});
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/page/edit', editPage);
+        res.status.should.equal(200);
+
+        let page = await db.cypher().match("(page:Page {pageId: '1'})<-[:EXPORT_TO_TC_PENDING]-(:TransitionConnectExport)")
+            .return(`page`)
+            .end().send();
+        page.length.should.equals(1);
+        page[0].page.created.should.equals(100);
+        page[0].page.modified.should.be.at.least(startTime);
+        page[0].page.pageId.should.equals('1');
+        page[0].page.description.should.equals("description");
+        page[0].page.title.should.equals("title");
+        page[0].page.label.should.equals("Generic");
+        should.not.exist(page[0].page.website);
+        page[0].page.topic.length.should.equals(2);
+        page[0].page.topic[0].should.equals('health');
+        page[0].page.topic[1].should.equals('socialDevelopment');
+        page[0].page.language.length.should.equals(2);
+        page[0].page.language[0].should.equals('en');
+        page[0].page.language[1].should.equals('fr');
+    });
+
+    it('Edit of generic page without being administrator - Return 400', async function () {
         let editPage = {
             genericPage: {
                 pageId: '2',
@@ -120,35 +152,33 @@ describe('Integration Tests for editing generic pages', function () {
             }
         };
         stubCDN.uploadFile.reset();
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/toSmallWidth.jpg');
+        res.status.should.equal(400);
 
-        return requestHandler.login(users.validUser).then(function () {
-            return requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/toSmallWidth.jpg');
-        }).then(function (res) {
-            res.status.should.equal(400);
-            return db.cypher().match("(page:Page {pageId: '2'})")
-                .return('page')
-                .end().send();
-        }).then(function (page) {
-            page.length.should.equals(1);
-            page[0].page.created.should.equals(200);
-            page[0].page.modified.should.equals(200);
-            page[0].page.pageId.should.equals('2');
-            page[0].page.description.should.equals("page2Description");
-            page[0].page.title.should.equals("generic2Title");
-            page[0].page.website.should.equals("www.example2.org");
-            page[0].page.label.should.equals("Generic");
+        let page = await db.cypher().match("(page:Page {pageId: '2'})")
+            .return('page')
+            .end().send();
+        page.length.should.equals(1);
+        page[0].page.created.should.equals(200);
+        page[0].page.modified.should.equals(200);
+        page[0].page.pageId.should.equals('2');
+        page[0].page.description.should.equals("page2Description");
+        page[0].page.title.should.equals("generic2Title");
+        page[0].page.website.should.equals("www.example2.org");
+        page[0].page.label.should.equals("Generic");
 
-            stubCDN.uploadFile.called.should.be.false;
+        stubCDN.uploadFile.called.should.be.false;
 
-            page[0].page.topic.length.should.equals(2);
-            page[0].page.topic[0].should.equals('health');
-            page[0].page.topic[1].should.equals('personalDevelopment');
-            page[0].page.language.length.should.equals(1);
-            page[0].page.language[0].should.equals('de');
-        });
+        page[0].page.topic.length.should.equals(2);
+        page[0].page.topic[0].should.equals('health');
+        page[0].page.topic[1].should.equals('personalDevelopment');
+        page[0].page.language.length.should.equals(1);
+        page[0].page.language[0].should.equals('de');
     });
 
-    it('Edit a generic page with to small width image - Return 400', function () {
+    it('Edit a generic page with to small width image - Return 400', async function () {
         let editPage = {
             genericPage: {
                 pageId: '1',
@@ -159,35 +189,33 @@ describe('Integration Tests for editing generic pages', function () {
             }
         };
         stubCDN.uploadFile.reset();
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/toSmallWidth.jpg');
+        res.status.should.equal(400);
 
-        return requestHandler.login(users.validUser).then(function () {
-            return requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/toSmallWidth.jpg');
-        }).then(function (res) {
-            res.status.should.equal(400);
-            return db.cypher().match("(page:Page {pageId: '1'})")
-                .return('page')
-                .end().send();
-        }).then(function (page) {
-            page.length.should.equals(1);
-            page[0].page.created.should.equals(100);
-            page[0].page.modified.should.equals(100);
-            page[0].page.pageId.should.equals('1');
-            page[0].page.description.should.equals("page1Description");
-            page[0].page.title.should.equals("generic1Title");
-            page[0].page.website.should.equals("www.example.org");
-            page[0].page.label.should.equals("Generic");
+        let page = await db.cypher().match("(page:Page {pageId: '1'})")
+            .return('page')
+            .end().send();
+        page.length.should.equals(1);
+        page[0].page.created.should.equals(100);
+        page[0].page.modified.should.equals(100);
+        page[0].page.pageId.should.equals('1');
+        page[0].page.description.should.equals("page1Description");
+        page[0].page.title.should.equals("generic1Title");
+        page[0].page.website.should.equals("www.example.org");
+        page[0].page.label.should.equals("Generic");
 
-            stubCDN.uploadFile.called.should.be.false;
+        stubCDN.uploadFile.called.should.be.false;
 
-            page[0].page.topic.length.should.equals(2);
-            page[0].page.topic[0].should.equals('health');
-            page[0].page.topic[1].should.equals('personalDevelopment');
-            page[0].page.language.length.should.equals(1);
-            page[0].page.language[0].should.equals('de');
-        });
+        page[0].page.topic.length.should.equals(2);
+        page[0].page.topic[0].should.equals('health');
+        page[0].page.topic[1].should.equals('personalDevelopment');
+        page[0].page.language.length.should.equals(1);
+        page[0].page.language[0].should.equals('de');
     });
 
-    it('Edit a generic page with to small height image - Return 400', function () {
+    it('Edit a generic page with to small height image - Return 400', async function () {
         let editPage = {
             genericPage: {
                 pageId: '1',
@@ -198,31 +226,29 @@ describe('Integration Tests for editing generic pages', function () {
             }
         };
         stubCDN.uploadFile.reset();
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/toSmallHeight.jpg');
+        res.status.should.equal(400);
 
-        return requestHandler.login(users.validUser).then(function () {
-            return requestHandler.post('/api/user/page/edit', editPage, './test/test/e2e/tests/user/page/toSmallHeight.jpg');
-        }).then(function (res) {
-            res.status.should.equal(400);
-            return db.cypher().match("(page:Page {pageId: '1'})")
-                .return('page')
-                .end().send();
-        }).then(function (page) {
-            page.length.should.equals(1);
-            page[0].page.created.should.equals(100);
-            page[0].page.modified.should.equals(100);
-            page[0].page.pageId.should.equals('1');
-            page[0].page.description.should.equals("page1Description");
-            page[0].page.title.should.equals("generic1Title");
-            page[0].page.website.should.equals("www.example.org");
-            page[0].page.label.should.equals("Generic");
+        let page = await db.cypher().match("(page:Page {pageId: '1'})")
+            .return('page')
+            .end().send();
+        page.length.should.equals(1);
+        page[0].page.created.should.equals(100);
+        page[0].page.modified.should.equals(100);
+        page[0].page.pageId.should.equals('1');
+        page[0].page.description.should.equals("page1Description");
+        page[0].page.title.should.equals("generic1Title");
+        page[0].page.website.should.equals("www.example.org");
+        page[0].page.label.should.equals("Generic");
 
-            stubCDN.uploadFile.called.should.be.false;
+        stubCDN.uploadFile.called.should.be.false;
 
-            page[0].page.topic.length.should.equals(2);
-            page[0].page.topic[0].should.equals('health');
-            page[0].page.topic[1].should.equals('personalDevelopment');
-            page[0].page.language.length.should.equals(1);
-            page[0].page.language[0].should.equals('de');
-        });
+        page[0].page.topic.length.should.equals(2);
+        page[0].page.topic[0].should.equals('health');
+        page[0].page.topic[1].should.equals('personalDevelopment');
+        page[0].page.language.length.should.equals(1);
+        page[0].page.language[0].should.equals('de');
     });
 });
