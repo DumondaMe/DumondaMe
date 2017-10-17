2079631d75393f9cf82839e42991796d710a9983
Roger Waldvogel <info@elyoos.org>
2017-10-17 10:39:38 +0200

    Using transaction with rollback for statements with 2 ore more write command.

--- commonLib/serverLib/lib/neo4j/cypher/neo4jCommunication.js
+++ commonLib/serverLib/lib/neo4j/cypher/neo4jCommunication.js
@@ -17,11 +17,41 @@ async function runCypherCommands(statementsToSend, session) {
     return results;
 }
 
+async function runCypherCommandsWithTransaction(statementsToSend, session) {
+    let results = [], tx = session.beginTransaction();
+    try {
+        for (let index = 0; index < statementsToSend.length; index++) {
+            let result, statement = statementsToSend[index];
+            result = await tx.run(statement.statement, statement.parameters);
+            results.push(parser.parseResult(result.records));
+        }
+        await tx.commit();
+    } catch (error) {
+        logger.error(error);
+        await tx.rollback();
+    }
+    return results;
+}
+
+let useTransaction = function (statementsToSend) {
+    let numberOfWriteCommands = 0;
+    for (let statement of statementsToSend) {
+        if (statement.isWriteCommand) {
+            numberOfWriteCommands++;
+        }
+    }
+    return numberOfWriteCommands >= 2;
+};
+
 let send = async function (statementsToSend, driver) {
     let results = [];
     const session = driver.session();
     try {
-        results = await runCypherCommands(statementsToSend, session);
+        if (useTransaction(statementsToSend)) {
+            results = await runCypherCommandsWithTransaction(statementsToSend, session);
+        } else {
+            results = await runCypherCommands(statementsToSend, session);
+        }
     } catch (ex) {
         logger.error(ex);
     }
