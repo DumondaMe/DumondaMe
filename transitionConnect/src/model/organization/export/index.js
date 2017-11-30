7747094e960b38faab103029233a8ab668335026
Roger Waldvogel <info@elyoos.org>
2017-11-30 16:43:54 +0100

    Export of locations implemented.

--- transitionConnect/src/model/organization/export/index.js
+++ transitionConnect/src/model/organization/export/index.js
@@ -4,16 +4,33 @@ let db = requireDb();
 let exceptions = require('elyoos-server-lib').exceptions;
 let logger = require('elyoos-server-lib').logging.getLogger(__filename);
 
+let convertLocations = function (locations) {
+    let locationsForJson = [];
+    for (let location of locations) {
+        locationsForJson.push({
+            address: location.address,
+            description: location.description,
+            geo: {latitude: location.latitude, longitude: location.longitude}
+        });
+    }
+    return locationsForJson;
+};
+
 let exportOrganisation = async function (id, req) {
     let resp = await db.cypher().match(`(:TransitionConnectExport)-[EXPORT_TO_TC]->
                             (org:Page {pageId: {pageId}})<-[:IS_ADMIN]-(admin:User)`)
         .with(`org, admin`)
         .orderBy(`admin.email`)
         .with(`org, collect(admin.email) AS admins`)
+        .optionalMatch(`(org)-[:HAS]->(address:Address)`)
+        .with(`org, admins, address`)
+        .orderBy(`address.addressId`)
         .return(`org.modified AS timestamp, org.title AS name, org.description AS description, 
-                 '' AS slogan, org.website AS website, org.topic AS categories, admins`)
+                 '' AS slogan, org.website AS website, org.topic AS categories, admins,
+                 collect(address) AS locations`)
         .end({pageId: id}).send();
     if (resp.length === 1) {
+        resp[0].locations = convertLocations(resp[0].locations);
         return resp[0];
     } else {
         return exceptions.getInvalidOperation(`organisation ${id} does not exist or is not exported to tc `, logger, req);
--- transitionConnect/test/test/e2e/tests/organisation/export/organizationDetailSpec.js
+++ transitionConnect/test/test/e2e/tests/organisation/export/organizationDetailSpec.js
@@ -12,22 +12,29 @@ describe('Integration Tests for exporting an organisation', function () {
         dbDsl.createGenericPage('1', {
             adminId: '1', created: 500, modified: 501, title: 'organization', description: 'description', website: 'www.link.org',
             language: ['de'], topic: ['health']
-        }, []);
+        }, [{
+            address: 'Zuerich',
+            description: 'ZuerichDescription',
+            lat: 47.376887,
+            lng: 8.541694,
+            addressId: '1'
+        }, {
+            address: 'Zuerich2',
+            description: 'ZuerichDescription2',
+            lat: 47.376888,
+            lng: 8.541695,
+            addressId: '2'
+        }]);
         dbDsl.createGenericPage('2', {
-            adminId: '2', created: 600, title: 'organization2', description: 'description2', website: 'www.link2.org',
-            language: ['fr'], topic: ['personalDevelopment', 'spiritual']
+            adminId: '1', created: 500, modified: 501, title: 'organization2', description: 'description2', website: 'www.link.org',
+            language: ['de'], topic: ['health']
         }, []);
         dbDsl.createGenericPage('3', {
-            adminId: '2', created: 601, title: 'organization3', description: 'description3', website: 'www.link3.org',
-            language: ['en'], topic: ['spiritual']
-        }, []);
-        dbDsl.createGenericPage('4', {
-            adminId: '2', created: 602, title: 'organization4', description: 'description4', website: 'www.link4.org',
-            language: ['en'], topic: ['spiritual']
+            adminId: '2', created: 600, title: 'organization3', description: 'description3', website: 'www.link2.org',
+            language: ['fr'], topic: ['personalDevelopment', 'spiritual']
         }, []);
         dbDsl.exportOrganisationToTransitionConnect({pageId: '1', timestampExportStarted: 800});
-        dbDsl.exportOrganisationToTransitionConnect({pageId: '2', timestampExportStarted: 801});
-        dbDsl.exportOrganisationToTransitionConnect({pageId: '3', timestampExportStarted: 802});
+        dbDsl.exportOrganisationToTransitionConnect({pageId: '2', timestampExportStarted: 800});
     });
 
     afterEach(function () {
@@ -36,7 +43,7 @@ describe('Integration Tests for exporting an organisation', function () {
 
     // Export of imported organization is not possible
 
-    it('Export an organisation', async function () {
+    it('Export an organisation with locations', async function () {
 
         dbDsl.addAdminToPage('3', '1');
 
@@ -55,15 +62,40 @@ describe('Integration Tests for exporting an organisation', function () {
         res.body.admins.length.should.equals(2);
         res.body.admins[0].should.equals('user3@irgendwo.ch');
         res.body.admins[1].should.equals('user@irgendwo.ch');
+        res.body.locations.length.should.equals(2);
+        res.body.locations[0].address.should.equals('Zuerich');
+        res.body.locations[0].description.should.equals('ZuerichDescription');
+        res.body.locations[0].geo.latitude.should.equals(47.376887);
+        res.body.locations[0].geo.longitude.should.equals(8.541694);
+        res.body.locations[1].address.should.equals('Zuerich2');
+        res.body.locations[1].description.should.equals('ZuerichDescription2');
+        res.body.locations[1].geo.latitude.should.equals(47.376888);
+        res.body.locations[1].geo.longitude.should.equals(8.541695);
     });
 
-    it('Not allowed to export organisation because not exported to tc', async function () {
-
-        dbDsl.addAdminToPage('3', '1');
+    it('Export an organisation without locations', async function () {
 
         await dbDsl.sendToDb();
         await requestHandler.login(users.validUser);
-        let res = await requestHandler.get('/api/v1/organisation/4');
+        let res = await requestHandler.get('/api/v1/organisation/2');
+        res.status.should.equal(200);
+
+        res.body.timestamp.should.equals(501);
+        res.body.name.should.equals('organization2');
+        res.body.description.should.equals('description2');
+        res.body.slogan.should.equals('');
+        res.body.website.should.equals('www.link.org');
+        res.body.categories.length.should.equals(1);
+        res.body.categories[0].should.equals('health');
+        res.body.admins.length.should.equals(1);
+        res.body.admins[0].should.equals('user@irgendwo.ch');
+        res.body.locations.length.should.equals(0);
+    });
+
+    it('Not allowed to export organisation because not exported to tc', async function () {
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/v1/organisation/3');
         res.status.should.equal(400);
     });
 
