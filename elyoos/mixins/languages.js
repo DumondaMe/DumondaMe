2f17f1312a510a9bc9ff4d6f227bf6fb8c043d78
Roger Waldvogel <info@elyoos.org>
2018-01-17 11:49:54 +0100

    Getting languages defined.

---
+++ elyoos/mixins/languages.js
@@ -0,0 +1,8 @@
+export default {
+    methods: {
+        getLanguages: function () {
+            return [{value: 'de', text: 'Deutsch'},
+                {value: 'en', text: 'English'}]
+        }
+    }
+}
