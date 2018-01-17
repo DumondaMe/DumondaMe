6aed77055faa7555e908221166cf44e0fd08c933
Roger Waldvogel <info@elyoos.org>
2018-01-17 11:49:29 +0100

    All topics with translation.

---
+++ elyoos/mixins/topics.js
@@ -0,0 +1,14 @@
+export default {
+    methods: {
+        getTopics: function () {
+            return [{value: 'health', text: this.$t('common:topic.health')},
+                {value: 'environmental', text: this.$t('common:topic.environmental')},
+                {value: 'spiritual', text: this.$t('common:topic.spiritual')},
+                {value: 'education', text: this.$t('common:topic.education')},
+                {value: 'personalDevelopment', text: this.$t('common:topic.personalDevelopment')},
+                {value: 'politics', text: this.$t('common:topic.politics')},
+                {value: 'economy', text: this.$t('common:topic.economy')},
+                {value: 'socialDevelopment', text: this.$t('common:topic.socialDevelopment')}]
+        }
+    }
+}
