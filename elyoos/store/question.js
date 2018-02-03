cafcb0f22f5428761edab405f6e2d0fc8144c71a
Roger Waldvogel <info@elyoos.org>
2018-02-03 15:20:40 +0100

    Mutation add text answer changed to add answer. Added youtube answer.

--- elyoos/store/question.js
+++ elyoos/store/question.js
@@ -6,16 +6,24 @@ export const mutations = {
     SET_QUESTION(state, question) {
         state.question = question;
     },
-    ADD_TEXT_ANSWER(state, textAnswer) {
-        state.question.textAnswers.push(textAnswer);
+    ADD_ANSWER(state, answer) {
+        state.question.answers.push(answer);
     }
 };
 
 export const actions = {
     async createTextAnswer({commit, state}, {answer}) {
         let response = await this.$axios.$post(`/user/question/answer/text/${state.question.questionId}`, {answer});
-        commit('ADD_TEXT_ANSWER', {
-            answer, created: response.created, creator: response.creator
+        commit('ADD_ANSWER', {
+            textId: response.textId, answer, created: response.created, creator: response.creator
         });
+    },
+    async createYoutubeAnswer({commit, state}, youtubeData) {
+        let response = await this.$axios.$post(`/user/question/answer/youtube/${state.question.questionId}`,
+            youtubeData);
+        youtubeData.youtubeId = response.youtubeId;
+        youtubeData.created = response.created;
+        youtubeData.creator = response.creator;
+        commit('ADD_ANSWER', youtubeData);
     }
 };
