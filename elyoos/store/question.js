cfbb0b56d9d7738e29d17e7788166c8ca95c7e99
Roger Waldvogel <info@elyoos.org>
2018-02-03 17:15:44 +0100

    Added isAdmin, idOnYoutube and linkEmbed.

--- elyoos/store/question.js
+++ elyoos/store/question.js
@@ -15,7 +15,7 @@ export const actions = {
     async createTextAnswer({commit, state}, {answer}) {
         let response = await this.$axios.$post(`/user/question/answer/text/${state.question.questionId}`, {answer});
         commit('ADD_ANSWER', {
-            answerId: response.answerId,
+            answerId: response.answerId, isAdmin: true,
             answerType: 'Text', answer, created: response.created, creator: response.creator
         });
     },
@@ -24,6 +24,9 @@ export const actions = {
             youtubeData);
         youtubeData.answerId = response.answerId;
         youtubeData.answerType = 'Youtube';
+        youtubeData.isAdmin = true;
+        youtubeData.idOnYoutube = response.idOnYoutube;
+        youtubeData.linkEmbed = `https://www.youtube.com/embed/${response.idOnYoutube}`;
         youtubeData.created = response.created;
         youtubeData.creator = response.creator;
         commit('ADD_ANSWER', youtubeData);
