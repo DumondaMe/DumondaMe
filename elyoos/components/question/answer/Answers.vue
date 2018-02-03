3f2c39b8f17412e146d058d62a75157913cb48c4
Roger Waldvogel <info@elyoos.org>
2018-02-03 15:17:35 +0100

    Container for all answers.

---
+++ elyoos/components/question/answer/Answers.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="elyoos-answer-container">
+        <div class="elyoos-answer-content">
+            <div v-for="answer in answers">
+                <text-card v-if="answer.textId" :answer="answer">
+                </text-card>
+                <youtube-card v-if="answer.youtubeId" :answer="answer">
+                </youtube-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TextCard from './card/Text.vue';
+    import YoutubeCard from './card/Youtube.vue';
+
+    export default {
+        components: {TextCard, YoutubeCard},
+        computed: {
+            answers() {
+                return this.$store.state.question.question.answers;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .elyoos-answer-container {
+        .elyoos-answer-content {
+            .ely-card {
+                .menu {
+                    button {
+                        margin: 0;
+                    }
+                }
+            }
+        }
+    }
+</style>
