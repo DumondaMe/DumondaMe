9923313783bdaed34865dd0be4083149533b9bef
Roger Waldvogel <info@elyoos.org>
2018-02-05 12:02:53 +0100

    Stub signed url.

--- commonLib/serverTestUtil/lib/stubCDN.js
+++ commonLib/serverTestUtil/lib/stubCDN.js
@@ -3,7 +3,8 @@
 let sinon = require('sinon');
 let Promise = require('bluebird');
 
-let getUrl;
+let getUrl; //Todo remove getUrl
+let getSignedUrl;
 let uploadFile;
 let copyFile;
 let deleteFolder;
@@ -23,18 +24,21 @@ module.exports = function () {
     return {
         stub: function (cdn) {
             getUrl = stubFunction(cdn, 'getUrl');
+            getSignedUrl = stubFunction(cdn, 'getSignedUrl');
             uploadFile = stubFunction(cdn, 'uploadFile');
             copyFile = stubFunction(cdn, 'copyFile');
             deleteFolder = stubFunction(cdn, 'deleteFolder');
             createFolderRegisterUser = stubFunction(cdn, 'createFolderRegisterUser');
 
             getUrl.returnsArg(0);
+            getSignedUrl.resolves(getSignedUrl.returnsArg(0));
             uploadFile.returns(Promise.resolve());
             copyFile.returns(Promise.resolve());
             deleteFolder.returns(Promise.resolve());
             createFolderRegisterUser.returns(Promise.resolve());
         },
         getUrl: getUrl,
+        getSignedUrl: getSignedUrl,
         uploadFile: uploadFile,
         copyFile: copyFile,
         deleteFolder: deleteFolder,
