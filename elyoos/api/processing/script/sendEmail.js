927d71fcb5ebebab4df666d04b8051224e8a0abb
Roger Waldvogel <info@elyoos.org>
2018-02-05 12:03:13 +0100

    Removed cdn config.

--- elyoos/api/processing/script/sendEmail.js
+++ elyoos/api/processing/script/sendEmail.js
@@ -1,11 +1,9 @@
 'use strict';
 
 let email = require('elyoos-server-lib').eMail;
-let cdnConfig = require('elyoos-server-lib').cdnConfig;
 let logger = require('elyoos-server-lib').logging.getLogger(__filename);
 
 let sendEmail = function () {
-    cdnConfig.config({bucket: 'elyoos.test'});
     email.sendEMail("invitePerson", {name: 'Roger Waldvogel', userId: '0'},
         'climberwoodi@gmx.ch');
     logger.info('Sent email');
