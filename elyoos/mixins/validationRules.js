516a5dc3613a3743f3eab5b97cb6d47eec03ed37
Roger Waldvogel <info@elyoos.org>
2018-02-08 11:54:08 +0100

    Handling case when input is null.

--- elyoos/mixins/validationRules.js
+++ elyoos/mixins/validationRules.js
@@ -1,19 +1,19 @@
 export default {
     methods: {
         ruleFieldRequired: function (text) {
-            return (v) => v.trim() !== '' || text;
+            return (v) => (typeof v === 'string' && v.trim()) !== '' || text;
         },
         ruleSelectRequired: function (text) {
             return (v) => !!v || text;
         },
         ruleSelectMultipleRequired: function (text) {
-            return (v) => (v.length && v.length > 0) || text;
+            return (v) => (typeof v === 'string' && v.length > 0) || text;
         },
         ruleToManyChars: function (text, length) {
-            return (v) => v.length <= length || text;
+            return (v) => (typeof v === 'string' && v.length <= length) || text;
         },
         ruleMinLength: function (text, minLength) {
-            return (v) => v.trim().length >= minLength || text;
+            return (v) => (typeof v === 'string' && v.trim().length >= minLength) || text;
         },
         rulePasswordChars: function (text) {
             return (v) => (/(?=.*[A-Z])/.test(v) && /(?=.*[0-9])/.test(v)) || text;
