401570624f364a1197de1e8499e5ec410b7756df
Roger Waldvogel <info@elyoos.org>
2018-02-16 15:08:19 +0100

    Don't use type. For compatibility reasons at the moment type as parameter is not removed.

--- commonLib/serverTestUtil/lib/dbDSL/contactConnection.js
+++ commonLib/serverTestUtil/lib/dbDSL/contactConnection.js
@@ -4,14 +4,11 @@ let db = require('../db');
 let dbConnectionHandling = require('./dbConnectionHandling');
 
 let createContactConnection = function (userId, contactId, type, contactAdded) {
-    if (!type) {
-        type = 'Freund';
-    }
     contactAdded = contactAdded || null;
     dbConnectionHandling.getCommands().push(db.cypher().match("(a:User {userId: {userId}}), (b:User {userId: {contactId}})")
-        .create("(a)-[:IS_CONTACT {type: {type}, contactAdded: {contactAdded}}]->(b)").end({
+        .create("(a)-[:IS_CONTACT {contactAdded: {contactAdded}}]->(b)").end({
             userId: userId, contactId: contactId,
-            contactAdded: contactAdded, type: type
+            contactAdded: contactAdded
         }).getCommand());
 };
 
