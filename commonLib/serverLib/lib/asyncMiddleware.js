2bc553334136b8302b2d4d2fa1501227abdb2365
Roger Waldvogel <info@elyoos.org>
2018-02-16 19:38:53 +0100

    Set user with id null when not on request.

--- commonLib/serverLib/lib/asyncMiddleware.js
+++ commonLib/serverLib/lib/asyncMiddleware.js
@@ -4,6 +4,9 @@ let Promise = require('bluebird');
 
 module.exports = fn =>
     (req, res, next) => {
-        Promise.resolve(fn(req, res, next))
+        if (!req.user) {
+            req.user = {id: null};
+        }
+        return Promise.resolve(fn(req, res, next))
             .catch(next);
     };
