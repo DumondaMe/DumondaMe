b5e5d69717cad5377440456b0ae5a42cecb30c41
Roger Waldvogel <info@elyoos.org>
2018-03-06 11:17:48 +0100

    Setting privacyMode and removed previous privacy settings.

--- elyoos/api/models/register/verifyRegisterUserRequest.js
+++ elyoos/api/models/register/verifyRegisterUserRequest.js
@@ -1,94 +1,79 @@
 'use strict';
 
-let db = requireDb();
-let exceptions = require('elyoos-server-lib').exceptions;
-let uuid = require('elyoos-server-lib').uuid;
-let time = require('elyoos-server-lib').time;
-let cdn = require('elyoos-server-lib').cdn;
-let logger = require('elyoos-server-lib').logging.getLogger(__filename);
+const db = requireDb();
+const exceptions = require('elyoos-server-lib').exceptions;
+const uuid = require('elyoos-server-lib').uuid;
+const time = require('elyoos-server-lib').time;
+const cdn = require('elyoos-server-lib').cdn;
+const logger = require('elyoos-server-lib').logging.getLogger(__filename);
 
-let EXPIRED = 60 * 60 * 12; // 12h
+const EXPIRED = 60 * 60 * 12; // 12h
 
-let deleteInvalidLinkIdRequest = function (linkId, req) {
-    return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})").delete("user")
-        .end({linkId: linkId}).send().then(function () {
-            return exceptions.getInvalidOperation(`linkId is expired ${linkId}`, logger, req);
-        });
+let deleteInvalidLinkIdRequest = async function (linkId, req) {
+    await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})").delete("user")
+        .end({linkId: linkId}).send();
+    return exceptions.getInvalidOperation(`linkId is expired ${linkId}`, logger, req);
 };
 
-let deleteRegisterRequest = function (emailNormalized, req) {
-    return db.cypher().match("(user:UserRegisterRequest {emailNormalized: {emailNormalized}})")
-        .delete("user").end({emailNormalized: emailNormalized}).send().then(function () {
-            return exceptions.getInvalidOperation(`User does already exist for email ${emailNormalized}`, logger, req);
-        });
+let deleteRegisterRequest = async function (emailNormalized, req) {
+    await db.cypher().match("(user:UserRegisterRequest {emailNormalized: {emailNormalized}})")
+        .delete("user").end({emailNormalized: emailNormalized}).send();
+    return exceptions.getInvalidOperation(`User does already exist for email ${emailNormalized}`, logger, req);
 };
 
-let checkValidLinkId = function (linkId, req) {
-    return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
+let checkValidLinkId = async function (linkId, req) {
+    let resp = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
         .return("user")
-        .end({linkId: linkId})
-        .send().then(function (resp) {
-            if (resp.length === 1) {
-                if (resp[0].user.registerDate > time.getNowUtcTimestamp() - EXPIRED) {
-                    return resp[0].user;
-                } else {
-                    return deleteInvalidLinkIdRequest(linkId, req);
-                }
-            } else {
-                return exceptions.getInvalidOperation(`Not found or more then one linkId ${linkId}`, logger, req);
-            }
-        });
+        .end({linkId: linkId}).send();
+    if (resp.length === 1) {
+        if (resp[0].user.registerDate > time.getNowUtcTimestamp() - EXPIRED) {
+            return resp[0].user;
+        } else {
+            return await deleteInvalidLinkIdRequest(linkId, req);
+        }
+    } else {
+        return exceptions.getInvalidOperation(`Not found or more then one linkId ${linkId}`, logger, req);
+    }
 };
 
-let checkUserExistsAlready = function (user, req) {
-    return db.cypher().match("(user:User {emailNormalized: {emailNormalized}})")
+let checkUserExistsAlready = async function (user, req) {
+    let resp = await db.cypher().match("(user:User {emailNormalized: {emailNormalized}})")
         .return("user")
-        .end({emailNormalized: user.emailNormalized})
-        .send().then(function (resp) {
-            if (resp.length === 1) {
-                return deleteRegisterRequest(user.emailNormalized, req);
-            }
-        });
+        .end({emailNormalized: user.emailNormalized}).send();
+    if (resp.length === 1) {
+        await deleteRegisterRequest(user.emailNormalized, req);
+    }
 };
 
-let verify = function (linkId, req) {
+let verify = async function (linkId, req) {
 
-    let userId, email;
-    return checkValidLinkId(linkId, req).then(function (user) {
-        return checkUserExistsAlready(user, req).then(function () {
-            let commands = [], privacy = {
-                profile: true,
-                profileData: true,
-                image: true,
-                contacts: true,
-                pinwall: true
-            };
-            delete user.linkId;
-            userId = uuid.generateUUID();
-            user.userId = userId;
+    let userId, email, commands = [];
+    let user = await checkValidLinkId(linkId, req);
+    await checkUserExistsAlready(user, req);
 
-            commands.push(db.cypher().match(`(user:UserRegisterRequest {linkId: {linkId}})`)
-                .return("user").end({linkId: linkId}).getCommand());
-            commands.push(db.cypher().create(`(:Privacy {privacy})<-[:HAS_PRIVACY {type: 'Freund'}]-(user:User {userData})
-                                          -[:HAS_PRIVACY_NO_CONTACT]->(:Privacy {privacy})`)
-                .end({userData: user, privacy: privacy}).getCommand());
-            commands.push(db.cypher().match(`(user:User {userId: {userId}}), (invitedUser:InvitedUser)<-[:HAS_INVITED]-(inviteUser:User)`)
-                .where(`invitedUser.emailNormalized = {email}`)
-                .createUnique(`(user)<-[:HAS_INVITED]-(inviteUser)`)
-                .end({userId: userId, email: user.emailNormalized}).getCommand());
-            commands.push(db.cypher().match(`(invitedUser:InvitedUser)<-[rel:HAS_INVITED]-(:User)`)
-                .where(`invitedUser.emailNormalized = {email}`).delete(`invitedUser, rel`)
-                .end({email: user.emailNormalized}).getCommand());
-            return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})").delete("user").end({linkId: linkId}).send(commands);
-        });
-    }).then(function (resp) {
-        email = resp[0][0].user.email;
-        return cdn.createFolderRegisterUser(userId);
-    }).then(function () {
-        return {email: email};
-    });
+    delete user.linkId;
+    userId = uuid.generateUUID();
+    user.userId = userId;
+    user.privacyMode = 'publicEl';
+
+    commands.push(db.cypher().match(`(user:UserRegisterRequest {linkId: {linkId}})`)
+        .return("user").end({linkId: linkId}).getCommand());
+    commands.push(db.cypher().create(`(user:User {userData})`)
+        .end({userData: user}).getCommand());
+    commands.push(db.cypher().match(`(user:User {userId: {userId}}), (invitedUser:InvitedUser)<-[:HAS_INVITED]-(inviteUser:User)`)
+        .where(`invitedUser.emailNormalized = {email}`)
+        .createUnique(`(user)<-[:HAS_INVITED]-(inviteUser)`)
+        .end({userId: userId, email: user.emailNormalized}).getCommand());
+    commands.push(db.cypher().match(`(invitedUser:InvitedUser)<-[rel:HAS_INVITED]-(:User)`)
+        .where(`invitedUser.emailNormalized = {email}`).delete(`invitedUser, rel`)
+        .end({email: user.emailNormalized}).getCommand());
+    let resp = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})").delete("user").end({linkId: linkId}).send(commands);
+
+    email = resp[0][0].user.email;
+    await cdn.createFolderRegisterUser(userId);
+    return {email: email};
 };
 
 module.exports = {
-    verify: verify
+    verify
 };
--- elyoos/api/test/test/e2e/tests/register/verifyRequestRegisterUserSpec.js
+++ elyoos/api/test/test/e2e/tests/register/verifyRequestRegisterUserSpec.js
@@ -1,12 +1,12 @@
 'use strict';
 
-let db = require('elyoos-server-test-util').db;
-let dbDsl = require('elyoos-server-test-util').dbDSL;
-let should = require('chai').should();
-let requestHandler = require('elyoos-server-test-util').requestHandler;
-let moment = require('moment');
-let randomstring = require("randomstring");
-let _ = require('lodash');
+const db = require('elyoos-server-test-util').db;
+const dbDsl = require('elyoos-server-test-util').dbDSL;
+const should = require('chai').should();
+const requestHandler = require('elyoos-server-test-util').requestHandler;
+const moment = require('moment');
+const randomstring = require("randomstring");
+const _ = require('lodash');
 
 describe('Integration Tests for verify registering a new user', function () {
 
@@ -22,179 +22,152 @@ describe('Integration Tests for verify registering a new user', function () {
             linkId: randomstring.generate(64)
         }, registerRequestUserExpired, startTime = Math.floor(moment.utc().valueOf() / 1000),
         registerRequestUserValidWithInvitation, registerRequestUserValidCaseSensitiveEmail;
-    beforeEach(function () {
-        return dbDsl.init(4).then(function () {
-            registerRequestUserExpired = _.cloneDeep(registerRequestUserValid);
-            registerRequestUserValidWithInvitation = _.cloneDeep(registerRequestUserValid);
-            registerRequestUserValidCaseSensitiveEmail = _.cloneDeep(registerRequestUserValid);
-            registerRequestUserValid.registerDate = startTime;
-            registerRequestUserValidCaseSensitiveEmail.registerDate = startTime;
-            registerRequestUserValidCaseSensitiveEmail.linkId = randomstring.generate(64);
-            registerRequestUserValidCaseSensitiveEmail.email = 'info@elyoos.org';
-            registerRequestUserExpired.registerDate = startTime - (60 * 60 * 12) - 1;
-            registerRequestUserExpired.linkId = randomstring.generate(64);
-            registerRequestUserExpired.email = 'INFO2@elyoos.org';
-            registerRequestUserExpired.emailNormalized = 'info2@elyoos.org';
-            registerRequestUserValidWithInvitation.email = 'info3@ELYOOS.org';
-            registerRequestUserValidWithInvitation.emailNormalized = 'info3@elyoos.org';
-            registerRequestUserValidWithInvitation.registerDate = startTime;
-            registerRequestUserValidWithInvitation.linkId = randomstring.generate(64);
-            dbDsl.createUserRegisterRequest(registerRequestUserValid);
-            dbDsl.createUserRegisterRequest(registerRequestUserValidCaseSensitiveEmail);
-            dbDsl.createUserRegisterRequest(registerRequestUserExpired);
-            dbDsl.createUserRegisterRequest(registerRequestUserValidWithInvitation);
+    beforeEach(async function () {
+        await dbDsl.init(4);
+        registerRequestUserExpired = _.cloneDeep(registerRequestUserValid);
+        registerRequestUserValidWithInvitation = _.cloneDeep(registerRequestUserValid);
+        registerRequestUserValidCaseSensitiveEmail = _.cloneDeep(registerRequestUserValid);
+        registerRequestUserValid.registerDate = startTime;
+        registerRequestUserValidCaseSensitiveEmail.registerDate = startTime;
+        registerRequestUserValidCaseSensitiveEmail.linkId = randomstring.generate(64);
+        registerRequestUserValidCaseSensitiveEmail.email = 'info@elyoos.org';
+        registerRequestUserExpired.registerDate = startTime - (60 * 60 * 12) - 1;
+        registerRequestUserExpired.linkId = randomstring.generate(64);
+        registerRequestUserExpired.email = 'INFO2@elyoos.org';
+        registerRequestUserExpired.emailNormalized = 'info2@elyoos.org';
+        registerRequestUserValidWithInvitation.email = 'info3@ELYOOS.org';
+        registerRequestUserValidWithInvitation.emailNormalized = 'info3@elyoos.org';
+        registerRequestUserValidWithInvitation.registerDate = startTime;
+        registerRequestUserValidWithInvitation.linkId = randomstring.generate(64);
+        dbDsl.createUserRegisterRequest(registerRequestUserValid);
+        dbDsl.createUserRegisterRequest(registerRequestUserValidCaseSensitiveEmail);
+        dbDsl.createUserRegisterRequest(registerRequestUserExpired);
+        dbDsl.createUserRegisterRequest(registerRequestUserValidWithInvitation);
 
-            dbDsl.invitationSentBeforeRegistration('2', [{email: 'info3@ELYOOS.org', emailNormalized: 'info3@elyoos.org'}]);
-            dbDsl.invitationSentBeforeRegistration('3', [{email: 'INFO3@elyoos.org', emailNormalized: 'info3@elyoos.org'}]);
-            dbDsl.invitationSentBeforeRegistration('4', [{email: 'info3@elyoos.org', emailNormalized: 'info3@elyoos.org'}]);
-            return dbDsl.sendToDb();
-        });
+        dbDsl.invitationSentBeforeRegistration('2', [{email: 'info3@ELYOOS.org', emailNormalized: 'info3@elyoos.org'}]);
+        dbDsl.invitationSentBeforeRegistration('3', [{email: 'INFO3@elyoos.org', emailNormalized: 'info3@elyoos.org'}]);
+        dbDsl.invitationSentBeforeRegistration('4', [{email: 'info3@elyoos.org', emailNormalized: 'info3@elyoos.org'}]);
+        await dbDsl.sendToDb();
     });
 
     afterEach(function () {
         return requestHandler.logout();
     });
 
-    it('Verify email address with valid linkId and create account- Return 200', function () {
+    it('Verify email address with valid linkId and create account', async function () {
 
-        return requestHandler.post('/api/register/verify', {linkId: registerRequestUserValid.linkId}).then(function (res) {
-            res.status.should.equal(200);
-            res.body.email.should.equals(registerRequestUserValid.email);
+        let res = await requestHandler.post('/api/register/verify', {linkId: registerRequestUserValid.linkId});
+        res.status.should.equal(200);
+        res.body.email.should.equals(registerRequestUserValid.email);
 
-            return db.cypher().match("(friendPrivacy:Privacy)<-[:HAS_PRIVACY {type: 'Freund'}]-(user:User {email: 'inFo@elYoos.org'})-[:HAS_PRIVACY_NO_CONTACT]->(noContactPrivacy:Privacy)")
-                .return('user, friendPrivacy, noContactPrivacy').end().send();
-        }).then(function (user) {
-            user.length.should.equals(1);
-            should.exist(user[0].user.userId);
-            user[0].user.emailNormalized.should.equals('info@elyoos.org');
-            user[0].user.name.should.equals('user Waldvogel');
-            should.not.exist(user[0].user.linkId);
-            user[0].user.forename.should.equals(registerRequestUserValid.forename);
-            user[0].user.surname.should.equals(registerRequestUserValid.surname);
-            user[0].user.latitude.should.equals(registerRequestUserValid.latitude);
-            user[0].user.longitude.should.equals(registerRequestUserValid.longitude);
-            user[0].user.registerDate.should.equals(startTime);
-            user[0].friendPrivacy.profile.should.be.true;
-            user[0].friendPrivacy.image.should.be.true;
-            user[0].friendPrivacy.contacts.should.be.true;
-            user[0].friendPrivacy.profileData.should.be.true;
-            user[0].friendPrivacy.pinwall.should.be.true;
-            user[0].noContactPrivacy.profile.should.be.true;
-            user[0].noContactPrivacy.image.should.be.true;
-            user[0].noContactPrivacy.contacts.should.be.true;
-            user[0].noContactPrivacy.profileData.should.be.true;
-            user[0].noContactPrivacy.pinwall.should.be.true;
-            return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
-                .return('user').end({linkId: registerRequestUserValid.linkId}).send();
-        }).then(function (user) {
-            user.length.should.equals(0);
-            return requestHandler.login({
-                'username': 'info@elyoos.org',
-                'password': '1'
-            });
+        let user = await db.cypher().match("(user:User {email: 'inFo@elYoos.org'})")
+            .return('user').end().send();
+
+        user.length.should.equals(1);
+        should.exist(user[0].user.userId);
+        user[0].user.emailNormalized.should.equals('info@elyoos.org');
+        user[0].user.name.should.equals('user Waldvogel');
+        should.not.exist(user[0].user.linkId);
+        user[0].user.forename.should.equals(registerRequestUserValid.forename);
+        user[0].user.surname.should.equals(registerRequestUserValid.surname);
+        user[0].user.latitude.should.equals(registerRequestUserValid.latitude);
+        user[0].user.longitude.should.equals(registerRequestUserValid.longitude);
+        user[0].user.registerDate.should.equals(startTime);
+        user[0].user.privacyMode.should.equals('publicEl');
+
+        user = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
+            .return('user').end({linkId: registerRequestUserValid.linkId}).send();
+        user.length.should.equals(0);
+
+        await requestHandler.login({
+            'username': 'info@elyoos.org',
+            'password': '1'
         });
     });
 
-    it('Verify email address with valid linkId and create account with invitations- Return 200', function () {
+    it('Verify email address with valid linkId and create account with invitations', async function () {
 
-        return requestHandler.post('/api/register/verify', {linkId: registerRequestUserValidWithInvitation.linkId}).then(function (res) {
-            res.status.should.equal(200);
-            res.body.email.should.equals(registerRequestUserValidWithInvitation.email);
+        let res = await requestHandler.post('/api/register/verify', {linkId: registerRequestUserValidWithInvitation.linkId});
+        res.status.should.equal(200);
+        res.body.email.should.equals(registerRequestUserValidWithInvitation.email);
 
-            return db.cypher().match("(friendPrivacy:Privacy)<-[:HAS_PRIVACY {type: 'Freund'}]-(user:User {email: 'info3@ELYOOS.org'})-[:HAS_PRIVACY_NO_CONTACT]->(noContactPrivacy:Privacy)")
-                .return('user, friendPrivacy, noContactPrivacy').end().send();
-        }).then(function (user) {
-            user.length.should.equals(1);
-            should.exist(user[0].user.userId);
-            user[0].user.emailNormalized.should.equals('info3@elyoos.org');
-            user[0].user.name.should.equals('user Waldvogel');
-            should.not.exist(user[0].user.linkId);
-            user[0].user.forename.should.equals(registerRequestUserValidWithInvitation.forename);
-            user[0].user.surname.should.equals(registerRequestUserValidWithInvitation.surname);
-            user[0].user.latitude.should.equals(registerRequestUserValid.latitude);
-            user[0].user.longitude.should.equals(registerRequestUserValid.longitude);
-            user[0].user.registerDate.should.equals(startTime);
-            user[0].friendPrivacy.profile.should.be.true;
-            user[0].friendPrivacy.image.should.be.true;
-            user[0].friendPrivacy.contacts.should.be.true;
-            user[0].friendPrivacy.profileData.should.be.true;
-            user[0].friendPrivacy.pinwall.should.be.true;
-            user[0].noContactPrivacy.profile.should.be.true;
-            user[0].noContactPrivacy.image.should.be.true;
-            user[0].noContactPrivacy.contacts.should.be.true;
-            user[0].noContactPrivacy.profileData.should.be.true;
-            user[0].noContactPrivacy.pinwall.should.be.true;
-            return db.cypher().match("(:User {email: 'info3@ELYOOS.org'})<-[:HAS_INVITED]-(user:User)")
-                .return('user').orderBy("user.userId").end().send();
-        }).then(function (user) {
-            user.length.should.equals(3);
-            user[0].user.userId.should.equals('2');
-            user[1].user.userId.should.equals('3');
-            user[2].user.userId.should.equals('4');
-            return db.cypher().match("(user:InvitedUser)")
-                .return('user').end().send();
-        }).then(function (invitedUser) {
-            invitedUser.length.should.equals(0);
-            return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
-                .return('user').end({linkId: registerRequestUserValidWithInvitation.linkId}).send();
-        }).then(function (user) {
-            user.length.should.equals(0);
-            return requestHandler.login({
-                'username': 'infO3@elyoos.org',
-                'password': '1'
-            });
+        let user = await db.cypher().match("(user:User {email: 'info3@ELYOOS.org'})")
+            .return('user').end().send();
+        user.length.should.equals(1);
+        should.exist(user[0].user.userId);
+        user[0].user.emailNormalized.should.equals('info3@elyoos.org');
+        user[0].user.name.should.equals('user Waldvogel');
+        should.not.exist(user[0].user.linkId);
+        user[0].user.forename.should.equals(registerRequestUserValidWithInvitation.forename);
+        user[0].user.surname.should.equals(registerRequestUserValidWithInvitation.surname);
+        user[0].user.latitude.should.equals(registerRequestUserValid.latitude);
+        user[0].user.longitude.should.equals(registerRequestUserValid.longitude);
+        user[0].user.registerDate.should.equals(startTime);
+        user[0].user.privacyMode.should.equals('publicEl');
+
+        user = await db.cypher().match("(:User {email: 'info3@ELYOOS.org'})<-[:HAS_INVITED]-(user:User)")
+            .return('user').orderBy("user.userId").end().send();
+        user.length.should.equals(3);
+        user[0].user.userId.should.equals('2');
+        user[1].user.userId.should.equals('3');
+        user[2].user.userId.should.equals('4');
+
+        let invitedUser = await db.cypher().match("(user:InvitedUser)")
+            .return('user').end().send();
+        invitedUser.length.should.equals(0);
+
+        user = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
+            .return('user').end({linkId: registerRequestUserValidWithInvitation.linkId}).send();
+        user.length.should.equals(0);
+        await requestHandler.login({
+            'username': 'infO3@elyoos.org',
+            'password': '1'
         });
     });
 
-    it('First verify is success. Second verify fails because of same normalized email', function () {
+    it('First verify is success. Second verify fails because of same normalized email', async function () {
 
-        return requestHandler.post('/api/register/verify', {linkId: registerRequestUserValid.linkId}).then(function (res) {
-            res.status.should.equal(200);
-            res.body.email.should.equals(registerRequestUserValid.email);
+        let res = await requestHandler.post('/api/register/verify', {linkId: registerRequestUserValid.linkId});
+        res.status.should.equal(200);
+        res.body.email.should.equals(registerRequestUserValid.email);
 
-            return db.cypher().match("(friendPrivacy:Privacy)<-[:HAS_PRIVACY {type: 'Freund'}]-(user:User {email: 'inFo@elYoos.org'})-[:HAS_PRIVACY_NO_CONTACT]->(noContactPrivacy:Privacy)")
-                .return('user').end().send();
-        }).then(function (user) {
-            user.length.should.equals(1);
-            return requestHandler.post('/api/register/verify', {linkId: registerRequestUserValidCaseSensitiveEmail.linkId});
-        }).then(function (res) {
-            res.status.should.equal(400);
-            return db.cypher().match("(friendPrivacy:Privacy)<-[:HAS_PRIVACY {type: 'Freund'}]-(user:User {email: 'info@elyoos.org'})-[:HAS_PRIVACY_NO_CONTACT]->(noContactPrivacy:Privacy)")
-                .return('user').end().send();
-        }).then(function (user) {
-            user.length.should.equals(0);
-            return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
-                .return('user').end({linkId: registerRequestUserValidCaseSensitiveEmail.linkId}).send();
-        }).then(function (user) {
-            user.length.should.equals(0);
-            return requestHandler.login({
-                'username': 'info@elyoos.org',
-                'password': '1'
-            });
+        let user = await db.cypher().match("(user:User {email: 'inFo@elYoos.org'})")
+            .return('user').end().send();
+        user.length.should.equals(1);
+
+        res = await requestHandler.post('/api/register/verify', {linkId: registerRequestUserValidCaseSensitiveEmail.linkId});
+        res.status.should.equal(400);
+
+        user = await db.cypher().match("(user:User {email: 'info@elyoos.org'})")
+            .return('user').end().send();
+        user.length.should.equals(0);
+
+        user = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
+            .return('user').end({linkId: registerRequestUserValidCaseSensitiveEmail.linkId}).send();
+        user.length.should.equals(0);
+        await requestHandler.login({
+            'username': 'info@elyoos.org',
+            'password': '1'
         });
     });
 
-    it('Send Error when linkId has expired - Return 400', function () {
+    it('Send Error when linkId has expired', async function () {
 
-        return requestHandler.post('/api/register/verify', {linkId: registerRequestUserExpired.linkId}).then(function (res) {
-            res.status.should.equal(400);
+        let res = await requestHandler.post('/api/register/verify', {linkId: registerRequestUserExpired.linkId});
+        res.status.should.equal(400);
 
-            return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
-                .return('user').end({linkId: registerRequestUserExpired.linkId}).send();
-        }).then(function (user) {
-            user.length.should.equals(0);
-        });
+        let user = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
+            .return('user').end({linkId: registerRequestUserExpired.linkId}).send();
+        user.length.should.equals(0);
     });
 
-    it('Send Error when linkId does not exist - Return 400', function () {
+    it('Send Error when linkId does not exist', async function () {
 
-        return requestHandler.post('/api/register/verify', {linkId: randomstring.generate(64)}).then(function (res) {
-            res.status.should.equal(400);
+        let res = await requestHandler.post('/api/register/verify', {linkId: randomstring.generate(64)});
+        res.status.should.equal(400);
 
-            return db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
-                .return('user').end({linkId: registerRequestUserValid.linkId}).send();
-        }).then(function (user) {
-            user.length.should.equals(1);
-        });
+        let user = await db.cypher().match("(user:UserRegisterRequest {linkId: {linkId}})")
+            .return('user').end({linkId: registerRequestUserValid.linkId}).send();
+        user.length.should.equals(1);
     });
 });
