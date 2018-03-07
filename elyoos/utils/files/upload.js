746872a5f54aab47802036607b87eedd9872f6a4
Roger Waldvogel <info@elyoos.org>
2018-03-07 17:39:30 +0100

    Removed unused code.

--- elyoos/utils/files/upload.js
+++ elyoos/utils/files/upload.js
@@ -2,8 +2,4 @@ export async function uploadFileToUrl(axios, file, uploadUrl) {
     const fd = new FormData();
     fd.append('file', file);
     await axios.$post(uploadUrl, fd);
-    /*await $http.post(uploadUrl, fd, {
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
-    });*/
 }
