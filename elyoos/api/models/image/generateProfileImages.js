3ffbbccbd49f11d748fe62c9c6bbda54eeeca01b
Roger Waldvogel <info@elyoos.org>
2018-03-07 14:05:09 +0100

    Setting bucket for file upload.

--- elyoos/api/models/image/generateProfileImages.js
+++ elyoos/api/models/image/generateProfileImages.js
@@ -8,30 +8,21 @@ let cdn = require('elyoos-server-lib').cdn;
 let tmp = require('tmp');
 
 module.exports = {
-    generateProfileImage: function (originalFilePath, userId) {
+    generateProfileImage: async function (originalFilePath, userId) {
         let preview = tmp.fileSync({postfix: '.jpg'}),
             thumbnail = tmp.fileSync({postfix: '.jpg'}),
             profile = tmp.fileSync({postfix: '.jpg'});
-        return gm.gm(originalFilePath).background('#FFFFFF').flatten().thumbAsync(100, 100, preview.name, 93)
-            .then(function () {
-                return gm.gm(originalFilePath).background('#FFFFFF').flatten().thumbAsync(48, 48, thumbnail.name, 97);
-            })
-            .then(function () {
-                return gm.gm(originalFilePath).background('#FFFFFF').flatten().thumbAsync(350, 350, profile.name, 92);
-            })
-            .then(function () {
-                return cdn.uploadFile(preview.name, 'profileImage/' + userId + '/profilePreview.jpg');
-            })
-            .then(function () {
-                return cdn.uploadFile(thumbnail.name, 'profileImage/' + userId + '/thumbnail.jpg');
-            })
-            .then(function () {
-                return cdn.uploadFile(profile.name, 'profileImage/' + userId + '/profile.jpg');
-            })
-            .then(function () {
-                preview.removeCallback();
-                thumbnail.removeCallback();
-                profile.removeCallback();
-            });
+
+        await gm.gm(originalFilePath).background('#FFFFFF').flatten().thumbAsync(100, 100, preview.name, 93);
+        await gm.gm(originalFilePath).background('#FFFFFF').flatten().thumbAsync(48, 48, thumbnail.name, 97);
+        await gm.gm(originalFilePath).background('#FFFFFF').flatten().thumbAsync(450, 450, profile.name, 92);
+
+        await cdn.uploadFile(preview.name, 'profileImage/' + userId + '/profilePreview.jpg', process.env.BUCKET_PRIVATE);
+        await cdn.uploadFile(thumbnail.name, 'profileImage/' + userId + '/thumbnail.jpg', process.env.BUCKET_PRIVATE);
+        await cdn.uploadFile(profile.name, 'profileImage/' + userId + '/profile.jpg', process.env.BUCKET_PRIVATE);
+
+        preview.removeCallback();
+        thumbnail.removeCallback();
+        profile.removeCallback();
     }
 };
