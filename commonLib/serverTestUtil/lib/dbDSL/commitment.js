870842bd6a34b05d408d289738287cb4019063d3
Roger Waldvogel <info@elyoos.org>
2018-03-24 10:19:22 +0100

    Add topics nodes to commitment.

--- commonLib/serverTestUtil/lib/dbDSL/commitment.js
+++ commonLib/serverTestUtil/lib/dbDSL/commitment.js
@@ -12,11 +12,12 @@ const createCommitment= function (answerId, data, addresses) {
     data.importTC = data.importTC ? ':ImportTC' : '';
     dbConnectionHandling.getCommands().push(db.cypher().match("(user:User {userId: {adminId}})")
         .create(`(user)-[:IS_ADMIN]->(commitment:Answer:Commitment${data.importTC} {title: {title}, language: {language}, description: {description}, modified: {modified}, modifiedAddress: {modifiedAddress}, 
-        created: {created}, topic: {topic}, answerId: {answerId}, website: {website}}) 
+        created: {created}, answerId: {answerId}, website: {website}}) 
         foreach (address in {addresses} | CREATE (commitment)-[:HAS]->(:Address {address: address.address, description: address.description, 
-        latitude: toFloat(address.lat), longitude: toFloat(address.lng), addressId: address.addressId}))`)
+        latitude: toFloat(address.lat), longitude: toFloat(address.lng), addressId: address.addressId}))
+        foreach (topic in {topics} | MERGE (:Topic {name: topic})-[:TOPIC]->(commitment))`)
         .end({
-            answerId: answerId, adminId: data.adminId, title: data.title, description: data.description, language: data.language, topic: data.topic, created: data.created,
+            answerId: answerId, adminId: data.adminId, title: data.title, description: data.description, language: data.language, topics: data.topics, created: data.created,
             modified: data.modified, modifiedAddress: data.modifiedAddress, addresses: addresses, website: data.website
         }).getCommand());
 };
