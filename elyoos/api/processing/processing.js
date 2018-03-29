527b0d9da2fe39073bbb0a9af9f4d44f5b10841e
Roger Waldvogel <info@elyoos.org>
2018-03-29 10:28:17 +0200

    Using sharp and added copy/resize commands of page to commitment images.

--- elyoos/api/processing/processing.js
+++ elyoos/api/processing/processing.js
@@ -1,9 +1,26 @@
-let logger = require('elyoos-server-lib').logging.getLogger(__filename);
+require('elyoos-server-lib').init('elyoos');
+
+const logger = require('elyoos-server-lib').logging.getLogger(__filename);
 require('fs-extra');
-let email = require('./script/sendEmail');
-let bluebird = require('bluebird');
+const copyCommitments = require('./script/copyCommitments');
+const bluebird = require('bluebird');
 bluebird.promisifyAll(require('gm').prototype);
 
-logger.info('Processing started');
+const dbConfig = require('elyoos-server-lib').databaseConfig;
 
-email.sendEmail();
+const runProcess = async function () {
+    await copyCommitments.processCommitment();
+};
+
+try {
+    dbConfig.connected.then(async function () {
+        logger.info('Processing started');
+        await runProcess();
+        process.exit(0);
+    });
+} catch(error) {
+    logger.error(error);
+    process.exit(1);
+}
+
+dbConfig.config({host: "bolt://localhost:7687"});
---
+++ elyoos/api/processing/script/copyCommitments.js
@@ -0,0 +1,44 @@
+const sharp = require('sharp');
+const db = require('elyoos-server-lib').neo4j;
+const cdn = require('elyoos-server-lib').cdn;
+const logger = require('elyoos-server-lib').logging.getLogger(__filename);
+
+const copyPagesToCommitment = async function (pages) {
+    for (let result of pages) {
+        try {
+            await cdn.copyFile(`pages/${result.answerId}/normal.jpg`, `commitment/${result.answerId}/title.jpg`,
+                process.env.BUCKET_PUBLIC)
+        } catch(error) {
+            logger.error(`Error copy page ${result.answerId}`);
+        }
+    }
+};
+
+const resizeTitleImage = async function (pages) {
+    for (let result of pages) {
+        try {
+            let titleImage = await cdn.getObject(`commitment/${result.answerId}/title.jpg`, process.env.BUCKET_PUBLIC);
+            let newTitleImage = await sharp(titleImage.Body).resize(300, 300)
+                .crop(sharp.strategy.entropy).jpeg({quality: 93})
+                .toBuffer();
+            let newTitleImage120x120 = await sharp(newTitleImage).resize(120, 120).jpeg({quality: 93})
+                .toBuffer();
+            await cdn.uploadBuffer(newTitleImage, `commitment/${result.answerId}/title.jpg`, process.env.BUCKET_PUBLIC);
+            await cdn.uploadBuffer(newTitleImage120x120, `commitment/${result.answerId}/120x120/title.jpg`, process.env.BUCKET_PUBLIC);
+        } catch(error) {
+            logger.error(`Error resize title image for commitment ${result.answerId}`);
+        }
+    }
+};
+
+const processCommitment = async function () {
+    let pages = await db.cypher().match(`(commitment:Commitment)`)
+        .return(`commitment.answerId AS answerId`)
+        .end().send();
+    await copyPagesToCommitment(pages);
+    await resizeTitleImage(pages);
+};
+
+module.exports = {
+    processCommitment
+};
