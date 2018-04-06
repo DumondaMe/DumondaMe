07f2e324b7f247a17137c47ef25541479110c74f
Roger Waldvogel <info@elyoos.org>
2018-04-06 11:31:26 +0200

    Using same style as for commitment detail.

--- elyoos/components/question/sidebar/GeneralInformation.vue
+++ elyoos/components/question/sidebar/GeneralInformation.vue
@@ -1,29 +1,40 @@
 <template>
     <div class="sidebar-container">
         <h3>{{$t("pages:detailQuestion.sidebar.generalInfo.title")}}</h3>
-        <div id="topic-container">
-            <span class="topic" v-for="topic in question.topics">{{topic}}</span>
-
+        <div class="question-info">
+            <v-icon class="info-icon">vpn_key</v-icon>
+            <div id="topic-container">
+                <span class="topic" v-for="(topic, index) in question.topics">
+                    {{topic}}<span v-if="index < question.topics.length - 1">, </span></span>
+            </div>
         </div>
         <div class="question-info">
-            {{$t("pages:detailQuestion.sidebar.generalInfo.createdBy")}}
+            <v-icon class="info-icon">account_box</v-icon>
+            <div class="question-info-content">
             <span class="user-name" v-if="question.isAdmin" @click="$router.push({name: 'user'})">
                 {{$t("common:you")}}</span>
-            <span class="user-name" v-else @click="$router.push({name: 'user-userId-slug',
+                <span class="user-name" v-else @click="$router.push({name: 'user-userId-slug',
                      params: {userId: question.creator.userId, slug: question.creator.slug}})">
                 {{question.creator.name}}</span>
+            </div>
         </div>
         <div class="question-info">
-            {{$t("pages:detailQuestion.sidebar.generalInfo.created")}} {{question.created | formatDate}}
+            <v-icon class="info-icon">create</v-icon>
+            <div class="question-info-content">
+                {{question.created | formatDate}}
+            </div>
         </div>
-        <div class="question-info">
-            {{$t("pages:detailQuestion.sidebar.generalInfo.language")}} {{getLanguageTranslatedText(question.language)}}
+        <div class="question-info" v-if="question.isAdmin">
+            <v-icon class="info-icon">chat_bubble_outline</v-icon>
+            <div class="question-info-content">
+                {{getLanguageTranslatedText(question.language)}}
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-     import language from '~/mixins/languages.js';
+    import language from '~/mixins/languages.js';
 
     export default {
         mixins: [language],
@@ -36,28 +47,32 @@
 </script>
 
 <style lang="scss">
-    #topic-container {
-        margin-bottom: 12px;
-        .topic {
-            margin-right: 8px;
-            background-color: #78909C;
-            color: #ffffff;
-            padding: 4px;
-            border-radius: 4px;
-            font-size: 12px;
-        }
-    }
-
     .question-info {
         font-size: 14px;
-        font-weight: 300;
         margin-bottom: 3px;
-        .user-name {
-            font-weight: 400;
-            cursor: pointer;
+
+        #topic-container {
+            margin-left: 30px;
+            .topic {
+                font-size: 14px;
+            }
         }
-        :hover.user-name {
-            text-decoration: underline;
+        .question-info-content {
+            margin-left: 30px;
+            .user-name {
+                font-weight: 500;
+                cursor: pointer;
+            }
+            :hover.user-name {
+                text-decoration: underline;
+            }
+        }
+        .info-icon {
+            float: left;
+            margin-top: 2px;
+            margin-right: 12px;
+            font-size: 18px;
+            color: #90A4AE;
         }
     }
 </style>
