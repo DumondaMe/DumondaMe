744312bb2f85cab764ffb222490f40a06486fb57
Roger Waldvogel <info@elyoos.org>
2018-04-07 14:44:31 +0200

    Search key words with fulltext search implemented.

---
+++ elyoos/api/api/api/topic/search.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const validation = require('elyoos-server-lib').jsonValidation;
+const searchTopic = requireModel('topic/search');
+const asyncMiddleware = require('elyoos-server-lib').asyncMiddleware;
+const auth = require('elyoos-server-lib').auth;
+const logger = require('elyoos-server-lib').logging.getLogger(__filename);
+
+const schemaSearchTopic = {
+    name: 'getSearchTopic',
+    type: 'object',
+    additionalProperties: false,
+    required: ['query'],
+    properties: {
+        query: {type: 'string', format: 'notEmptyString', maxLength: 30}
+    }
+};
+
+module.exports = function (router) {
+
+    router.get('/', auth.isAuthenticated(), asyncMiddleware(async (req, res) => {
+        const params = await validation.validateRequest(req, schemaSearchTopic, logger);
+        let response = await searchTopic.search(params.query);
+        res.status(200).json(response);
+    }));
+};
---
+++ elyoos/api/models/topic/search.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const db = requireDb();
+
+const getQueryString = function (query) {
+    let queryString = '';
+    for (let word of query.trim().split(' ')) {
+        queryString += `${word}~ `
+    }
+    return queryString.trim();
+};
+
+const search = async function (query) {
+    let queryString = getQueryString(query);
+
+    let result = await db.cypher().call(`apoc.index.search("keywords", {queryString}) YIELD node, weight`)
+        .optionalMatch(`(node)<-[:TOPIC]-(ref)`)
+        .return(`node.name AS topic, weight, COUNT(ref) AS numberOfRef`)
+        .orderBy(`weight DESC, numberOfRef DESC`)
+        .limit(`10`).end({queryString}).send();
+    return {topics: result.map(topic => topic.topic)};
+};
+
+module.exports = {
+    search
+};
