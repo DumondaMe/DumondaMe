16cf4fd76e3f907f07afae47fe8426cc1008d030
Roger Waldvogel <info@elyoos.org>
2018-05-29 16:41:32 +0200

    Loading of up coming and past events implemented. Show message when no events available.

--- elyoos/components/commitment/detail/event/Events.vue
+++ elyoos/components/commitment/detail/event/Events.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="events-container" v-if="events.length > 0 || isAdmin">
+    <div id="events-container">
         <h2>{{$t('pages:detailCommitment.events.title')}}
             <span class="select-event-container">
-                (<span :class="{'selected': selectNext}" class="select-event">{{$t('pages:detailCommitment.events.upcoming')}}</span> |
-                <span :class="{'selected': !selectNext}" class="select-event">{{$t('pages:detailCommitment.events.past')}}</span>)</span>
+                (<span :class="{'selected': selectUpComing}" class="select-event" @click="upComingEvents()">{{$t('pages:detailCommitment.events.upcoming')}}</span> |
+                <span :class="{'selected': !selectUpComing}" class="select-event" @click="pastEvents()">{{$t('pages:detailCommitment.events.past')}}</span>)</span>
         </h2>
         <div id="event-admin-commands" v-if="isAdmin">
             <v-btn color="primary" @click="showCreateEventDialog = true">
@@ -11,8 +11,19 @@
                 {{$t('common:button.create')}}
             </v-btn>
         </div>
-        <div v-for="event in events">
-            <event :event="event"></event>
+        <div v-if="!loading">
+            <div v-for="event in events">
+                <event :event="event"></event>
+            </div>
+            <div v-if="events.length === 0">
+                <div class="ely-card no-events-description" v-if="selectUpComing">
+                    {{$t('pages:detailCommitment.events.noUpComingEvents')}}</div>
+                <div class="ely-card no-events-description" v-else>
+                    {{$t('pages:detailCommitment.events.noPastEvents')}}</div>
+            </div>
+        </div>
+        <div class="ely-loading-progress" v-else>
+            <v-progress-circular indeterminate color="primary"></v-progress-circular>
         </div>
         <create-event-dialog v-if="showCreateEventDialog" @close-dialog="showCreateEventDialog = false"
                              :init-topic="$store.state.commitment.commitment.topics"
@@ -29,7 +40,7 @@
         name: "commitmentEvents",
         components: {CreateEventDialog, Event},
         data() {
-            return {showCreateEventDialog: false, selectNext: true}
+            return {showCreateEventDialog: false, selectUpComing: true, loading: false}
         },
         computed: {
             events() {
@@ -38,7 +49,33 @@
             isAdmin() {
                 return this.$store.state.commitment.commitment.isAdmin;
             }
-        }
+        },
+        methods: {
+            async upComingEvents() {
+                this.selectUpComing = true;
+                this.loading = true;
+                try {
+                    await this.$store.dispatch('commitment/getEvents',
+                        {isUpComingEvents: true, commitmentId: this.$route.params.commitmentId});
+                } catch (error) {
+
+                } finally {
+                    this.loading = false;
+                }
+            },
+            async pastEvents() {
+                this.selectUpComing = false;
+                this.loading = true;
+                try {
+                    await this.$store.dispatch('commitment/getEvents',
+                        {isUpComingEvents: false, commitmentId: this.$route.params.commitmentId});
+                } catch (error) {
+
+                } finally {
+                    this.loading = false;
+                }
+            }
+        },
     }
 </script>
 
@@ -65,6 +102,9 @@
                     font-size: 20px;
                 }
             }
+        }
+        .no-events-description {
+            font-weight: 300;
         }
     }
 </style>
--- elyoos/store/commitment.js
+++ elyoos/store/commitment.js
@@ -5,7 +5,9 @@ export const state = () => ({
     commitment: {
         title: null, description: null, website: null, lang: null, numberOfWatches: 0, userWatchesCommitment: false,
         imageUrl: null, linkedWithQuestions: [], events: [], topics: []
-    }
+    },
+    isUpComingEvents: true,
+    eventPage: 0
 });
 
 export const getters = {
@@ -17,6 +19,15 @@ export const getters = {
 export const mutations = {
     SET_COMMITMENT(state, commitment) {
         state.commitment = commitment;
+    },
+    SET_IS_UP_COMING_EVENTS(state, isUpComingEvents) {
+        state.isUpComingEvents = isUpComingEvents;
+    },
+    SET_EVENTS_PAGE(state, eventPage) {
+        state.eventPage = eventPage;
+    },
+    SET_EVENTS(state, events) {
+        state.commitment.events = events;
     },
     SET_TOPICS(state, topics) {
         state.commitment.topics = topics;
@@ -50,6 +61,7 @@ export const actions = {
     async getCommitment({commit, state}, commitmentId) {
         let resp = await this.$axios.$get(`commitment`, {params: {commitmentId}});
         commit('SET_COMMITMENT', resp);
+        commit('SET_IS_UP_COMING_EVENTS', true);
     },
     async updateCommitment({commit, state}, {commitment, commitmentId, imageHasChanged}) {
         let commitmentToUpload = getCommitmentForUpload(commitment, imageHasChanged);
@@ -61,5 +73,15 @@ export const actions = {
             await this.$axios.$put(`/user/commitment/${commitmentId}`, commitmentToUpload);
         }
         commit('SET_COMMITMENT', commitment);
-    }
+    },
+    async getEvents({commit, state}, {commitmentId, isUpComingEvents}) {
+        if (isUpComingEvents !== state.isUpComingEvents) {
+            let events = await this.$axios.$get(`commitment/event`, {
+                params: {commitmentId, upComing: isUpComingEvents, page: 0}
+            });
+            commit('SET_EVENTS', events.events);
+            commit('SET_IS_UP_COMING_EVENTS', isUpComingEvents);
+            commit('SET_EVENTS_PAGE', 1);
+        }
+    },
 };
