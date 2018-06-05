bc9d01115a32e6db5d26a7165c47614a309e1ba4
Roger Waldvogel <info@elyoos.org>
2018-06-05 14:52:46 +0200

    Added review from momo&ronja.

--- elyoos/pages/vision/index.vue
+++ elyoos/pages/vision/index.vue
@@ -5,24 +5,24 @@
             <p>Wie verändern wir die Welt zu einem Ort, wo ein nachhaltiger Umgang mit der Natur, soziale Gerechtigkeit
                 und Frieden so selbstverständlich sind wie die Luft zum Atmen? Um dies zu verwirklichen ist es
                 unabdingbar, dass wir die notwendigen Fragen stellen und die passenden Antworten umsetzen. Dazu braucht
-                es Engagement in vielen verschiedenen Themenbereichen wie z.Bsp. Politik, Wirtschaft, Gesellschaftliche
-                Entwicklung, Umweltschutz, Spiritualität, Gesundheit, Bildung usw.</p>
-            <p>Elyoos soll als Katalysator dienen, welcher die bereits vorhanden Lösungsvorschläge sichtbar macht und
-                das Erarbeiten von Neuen unterstützt. Deshalb nutzt dieses soziale Netzwerke die Vorteile des Internets,
-                um mit vereinten Kräften einen Paradigmenwechsel zu ermöglichen. Einer welcher uns nicht länger nur von
-                einer besseren Zukunft träumen lässt, sondern diese in die Gegenwart bringt.</p>
+                es Engagement in verschiedenen Themenbereichen wie Politik, Wirtschaft, Gesellschaftliche Entwicklung,
+                Umweltschutz, Spiritualität, Gesundheit, Bildung und vielen mehr.</p>
+            <p>Elyoos dient als Katalysator, welcher die bereits vorhanden Lösungsvorschläge sichtbar macht und das
+                Erarbeiten von Neuen unterstützt. Das soziale Netzwerk nutzt dabei das Potenzial des Internets, um mit
+                vereinten Kräften einen Paradigmenwechsel zu ermöglichen. Einer welcher uns nicht länger nur von einer
+                besseren Zukunft träumen lässt, sondern diese in die Gegenwart bringt.</p>
         </div>
         <div v-else-if="selectedLanguage === 'en'">
             <h1>Our Vision</h1>
             <p>How do we change the world into a place where sustainable interaction with nature, social justice and
                 peace are as natural as the air we breathe? To achieve this, it is essential that we ask the necessary
-                questions and implement the appropriate answers. This requires commitment in many different areas such
-                as politics, economics, social development, environmental protection, spirituality, health, education,
-                etc.</p>
-            <p>Elyoos should serve as a catalyst, which makes the already existing solution proposals visible and
-                supports the development of new ones. Therefore, this social network uses the advantages of the Internet
-                to enable a paradigm shift with combined forces. One that no longer only makes us dream of a better
-                future, but brings it into the present.</p>
+                questions and implement the appropriate answers. This requires commitment in various areas such as
+                politics, business, social development, environmental protection, spirituality, health, education and
+                many more.</p>
+            <p>Elyoos serves as a catalyst, which makes the already existing solution proposals visible and supports the
+                development of new ones. The social network uses the potential of the Internet to make a paradigm shift
+                possible by joining forces. One that no longer only makes us dream of a better future, but brings it
+                into the present.</p>
         </div>
     </div>
 </template>
