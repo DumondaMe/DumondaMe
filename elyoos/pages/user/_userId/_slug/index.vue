133564b272f6f5a3c27d140c17902fad1d0e1ce8
Roger Waldvogel <info@elyoos.org>
2018-06-07 14:28:37 +0200

    First letter of import lower case.

--- elyoos/pages/user/_userId/_slug/index.vue
+++ elyoos/pages/user/_userId/_slug/index.vue
@@ -14,7 +14,7 @@
 <script>
     import DetailLayout from '~/components/layouts/Detail.vue';
     import UserProfile from '~/components/userProfile/UserProfile.vue';
-    import UserTrustCircle from '~/components/userProfile/TrustCircle/UserTrustCircle.vue';
+    import UserTrustCircle from '~/components/userProfile/trustCircle/UserTrustCircle.vue';
 
     export default {
         async fetch({params, store, error}) {
