59ed23ce16c04fd135a6851fdab9d57fda5d30c3
Roger Waldvogel <info@elyoos.org>
2018-06-13 09:01:43 +0200

    Using 2px for border radius.

--- elyoos/components/userProfile/trustCircle/User.vue
+++ elyoos/components/userProfile/trustCircle/User.vue
@@ -85,7 +85,7 @@
             img {
                 width: 100%;
                 height: 100%;
-                border-radius: 50%;
+                border-radius: 2px;
             }
         }
         .user-info-container {
