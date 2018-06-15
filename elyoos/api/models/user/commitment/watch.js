0139f44edc673c78264b7a609badf7ad45c213ae
Roger Waldvogel <info@elyoos.org>
2018-06-15 16:06:59 +0200

    Create notification when a user watches a commitment. Remove notification when a user delete watching a commitment.

--- elyoos/api/models/user/commitment/watch.js
+++ elyoos/api/models/user/commitment/watch.js
@@ -3,22 +3,61 @@
 const db = requireDb();
 const exceptions = require('elyoos-server-lib').exceptions;
 const time = require('elyoos-server-lib').time;
+const uuid = require('elyoos-server-lib').uuid;
 const logger = require('elyoos-server-lib').logging.getLogger(__filename);
 
+const addWatchNotificationExists = function (userId, commitmentId, watchAdded) {
+    return db.cypher().match(`(admin:User)-[:IS_ADMIN]->(c:Commitment {commitmentId: {commitmentId}})<-[:NOTIFICATION]-
+                (n:Notification {type: 'watchingCommitment'})`)
+        .where(`(n)-[:NOTIFIED]->(admin) AND NOT (:User {userId: {userId}})<-[:ORIGINATOR_OF_NOTIFICATION]-(n)`)
+        .set(`n`, {created: watchAdded})
+        .with(`n`)
+        .match(`(u:User {userId: {userId}})`)
+        .merge(`(n)-[:ORIGINATOR_OF_NOTIFICATION {created: {watchAdded}}]->(u)`)
+        .end({userId, commitmentId, watchAdded}).getCommand()
+};
+
+const addWatchNotificationNotExists = function (userId, commitmentId, watchAdded) {
+    let notificationId = uuid.generateUUID();
+    return db.cypher().match(`(admin:User)-[:IS_ADMIN]->(c:Commitment {commitmentId: {commitmentId}})`)
+        .where(`NOT (c)<-[:NOTIFICATION]-(:Notification {type: 'watchingCommitment'})-[:NOTIFIED]->(admin)`)
+        .merge(`(c)<-[:NOTIFICATION]-(n:Notification {type: 'watchingCommitment', created: {watchAdded}, 
+                 notificationId: {notificationId}})`)
+        .merge(`(n)-[:NOTIFIED]->(admin)`)
+        .with(`n`)
+        .match(`(u:User {userId: {userId}})`)
+        .merge(`(n)-[:ORIGINATOR_OF_NOTIFICATION {created: {watchAdded}}]->(u)`)
+        .end({userId, commitmentId, notificationId, watchAdded}).getCommand()
+};
+
 const addWatch = async function (userId, commitmentId) {
-    let created = time.getNowUtcTimestamp();
+    let commands = [], created = time.getNowUtcTimestamp();
+    commands.push(addWatchNotificationExists(userId, commitmentId, created));
+    commands.push(addWatchNotificationNotExists(userId, commitmentId, created));
+
     let response = await db.cypher()
         .match(`(user:User {userId: {userId}}), (commitment:Commitment {commitmentId: {commitmentId}})`)
         .where(`NOT (user)-[:WATCH]->(commitment) AND NOT (user)-[:IS_ADMIN]->(commitment)`)
         .merge(`(user)-[:WATCH {created: {created}}]->(commitment)`)
         .return(`user`)
-        .end({userId, commitmentId, created}).send();
+        .end({userId, commitmentId, created}).send(commands);
 
-    if (response.length === 0) {
+    if (response[2].length === 0) {
         throw new exceptions.InvalidOperation(`Watch could not be added from user ${userId} to commitment ${commitmentId}`);
     }
 
     logger.info(`User watches commitment ${commitmentId}`)
+};
+
+const removeWatchNotification = function (userId, commitmentId) {
+    return db.cypher().match(`(u:User {userId: {userId}})<-[rel:ORIGINATOR_OF_NOTIFICATION]-
+          (n:Notification {type: 'watchingCommitment'})-[:NOTIFICATION]->(c:Commitment {commitmentId: {commitmentId}})`)
+        .delete(`rel`)
+        .with(`n`)
+        .match('(c:Commitment)<-[relC:NOTIFICATION]-(notification)-[notified:NOTIFIED]->(:User)')
+        .where(`NOT (notification)-[:ORIGINATOR_OF_NOTIFICATION]->(:User)`)
+        .delete(`notification, notified, relC`)
+        .end({userId, commitmentId}).getCommand()
 };
 
 const removeWatch = async function (userId, commitmentId) {
@@ -26,7 +65,7 @@ const removeWatch = async function (userId, commitmentId) {
     await db.cypher()
         .match(`(:User {userId: {userId}})-[watch:WATCH]->(:Commitment {commitmentId: {commitmentId}})`)
         .delete(`watch`)
-        .end({userId, commitmentId}).send();
+        .end({userId, commitmentId}).send([removeWatchNotification(userId, commitmentId)]);
 
     logger.info(`User removed watch of commitment ${commitmentId}`)
 };
---
+++ elyoos/api/test/test/e2e/tests/user/commitment/watchCommitmentNotificationSpec.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const users = require('elyoos-server-test-util').user;
+const db = require('elyoos-server-test-util').db;
+const dbDsl = require('elyoos-server-test-util').dbDSL;
+const requestHandler = require('elyoos-server-test-util').requestHandler;
+const should = require('chai').should();
+const moment = require('moment');
+
+describe('Notification when user watches a commitment', function () {
+
+    let startTime;
+
+    //notification can not be added twice to the same user
+
+    beforeEach(async function () {
+        await dbDsl.init(5);
+        startTime = Math.floor(moment.utc().valueOf() / 1000);
+
+        dbDsl.createRegion('region', {});
+
+        dbDsl.createCommitment('1', {
+            adminId: '2', topics: ['Spiritual', 'Meditation'], language: 'de', created: 700,
+            website: 'https://www.example.org/', regions: ['region']
+        });
+
+        dbDsl.createCommitment('2', {
+            adminId: '1', topics: ['Spiritual', 'Meditation'], language: 'de', created: 700,
+            website: 'https://www.example.org/', regions: ['region']
+        });
+    });
+
+    afterEach(function () {
+        return requestHandler.logout();
+    });
+
+    it('Watching a commitment creates a new notification for the admin of the commitment', async function () {
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.put('/api/user/commitment/watch/1');
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(:User {userId: '2'})<-[:NOTIFIED]-
+        (notification:Notification {type: 'watchingCommitment'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(user)`)
+            .match(`(notification)-[:NOTIFICATION]->(c:Commitment)`)
+            .return('notification, c, user.userId AS userId, relNot.created AS created')
+            .end().send();
+        notification.length.should.equals(1);
+        should.exist(notification[0].notification.notificationId);
+        notification[0].notification.created.should.least(startTime);
+        notification[0].c.commitmentId.should.equals('1');
+        notification[0].userId.should.equals('1');
+        notification[0].created.should.least(startTime);
+    });
+
+    it('Notification is not added twice to the same user', async function () {
+        dbDsl.userWatchesCommitment('50', {
+            commitmentId: '1',
+            created: 678, watchingUsers: [{userId: '1', created: 555}]
+        });
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.put('/api/user/commitment/watch/1');
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(:User {userId: '2'})<-[:NOTIFIED]-
+        (notification:Notification {type: 'watchingCommitment'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(user)`)
+            .match(`(notification)-[:NOTIFICATION]->(c:Commitment)`)
+            .return('notification, c, user.userId AS userId, relNot.created AS created')
+            .end().send();
+        notification.length.should.equals(1);
+    });
+
+    it('Watching a commitment adds to existing notification the new watching user', async function () {
+        dbDsl.userWatchesCommitment('50', {
+            commitmentId: '1',
+            created: 678, watchingUsers: [{userId: '3', created: 555}, {userId: '4', created: 444}]
+        });
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.put('/api/user/commitment/watch/1');
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(:User {userId: '2'})<-[:NOTIFIED]-
+        (notification:Notification {type: 'watchingCommitment'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(user)`)
+            .match(`(notification)-[:NOTIFICATION]->(c:Commitment)`)
+            .return('notification, c, collect(user.userId) AS userIds, collect(relNot.created) AS created')
+            .end().send();
+        notification.length.should.equals(1);
+        notification[0].notification.notificationId.should.equals('50');
+        notification[0].notification.created.should.least(startTime);
+        notification[0].c.commitmentId.should.equals('1');
+        notification[0].userIds.length.should.equals(3);
+        notification[0].created.length.should.equals(3);
+    });
+
+    it('Delete watch of a commitment deletes notification', async function () {
+        dbDsl.userWatchesCommitment('50', {
+            commitmentId: '1',
+            created: 678, watchingUsers: [{userId: '1', created: 555}]
+        });
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.del('/api/user/commitment/watch', {commitmentId: '1'});
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(:User {userId: '2'})<-[:NOTIFIED]-
+        (notification:Notification {type: 'watchingCommitment'})`)
+            .return('notification')
+            .end().send();
+        notification.length.should.equals(0);
+    });
+
+    it('Delete watch of a commitment removes only relationship to notification', async function () {
+        dbDsl.userWatchesCommitment('50', {
+            commitmentId: '1',
+            created: 678, watchingUsers: [{userId: '1', created: 555}, {userId: '3', created: 666}]
+        });
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.del('/api/user/commitment/watch', {commitmentId: '1'});
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(:User {userId: '2'})<-[:NOTIFIED]-
+        (notification:Notification {type: 'watchingCommitment'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(user)`)
+            .return('notification, user.userId AS userId, relNot.created AS created')
+            .end().send();
+        notification.length.should.equals(1);
+        notification[0].notification.notificationId.should.equals('50');
+        notification[0].notification.created.should.equals(678);
+        notification[0].userId.should.equals('3');
+        notification[0].created.should.equals(666);
+    });
+});
