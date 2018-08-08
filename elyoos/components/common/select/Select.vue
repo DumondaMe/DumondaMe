7587f3f789ad6449434fb5c80edf23596ded6a9b
Roger Waldvogel <info@elyoos.org>
2018-08-08 09:30:54 +0200

    New select element.

---
+++ elyoos/components/common/select/Select.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="select-container">
+        <select-element :item="item" v-for="item in localItems" :key="item.id"
+                        :is-root="true" :select-multiple="selectMultiple"
+                        @select-changed="selectChanged">
+        </select-element>
+    </div>
+</template>
+
+<script>
+    import SelectElement from './SelectElement';
+    import Vue from 'vue';
+
+    export default {
+        props: ['items', 'selectMultiple'],
+        components: {SelectElement},
+        data() {
+            return {localItems: JSON.parse(JSON.stringify(this.items))}
+        },
+        watch: {
+            items(newItems) {
+                this.localItems = JSON.parse(JSON.stringify(newItems));
+                this.setIsSelected(this.localItems);
+            }
+        },
+        methods: {
+            setIsSelected(items) {
+                for (let item of items) {
+                    if (!item.isSelected) {
+                        Vue.set(item, 'isSelected', false);
+                    }
+                    if (item.subItems && item.subItems.length > 0) {
+                        this.setIsSelected(item.subItems);
+                    }
+                }
+            },
+            getSelected(items, selected) {
+                for (let item of items) {
+                    if (item.isSelected) {
+                        selected.push(item.id);
+                    }
+                    if (item.subItems && item.subItems.length > 0) {
+                        this.getSelected(item.subItems, selected);
+                    }
+                }
+                return selected;
+            },
+            selectChanged() {
+                let selected = [];
+                this.getSelected(this.localItems, selected);
+                this.$emit('select-changed', selected);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    #select-container {
+        border-top: 1px solid $divider;
+        border-left: 1px solid $divider;
+        border-right: 1px solid $divider;
+        max-width: 400px;
+        margin: 0 auto;
+    }
+</style>
---
+++ elyoos/components/common/select/SelectElement.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="select-item">
+        <div class="item-container" @click="select()"
+             :class="{'item-selected': item.isSelected, 'sub-item-selected': item.subItemIsSelected}">
+            <v-icon class="item-icon" v-show="item.isSelected">done</v-icon>
+            <div class="item" :class="{'main-bold': isRoot}">
+                {{item.description}}
+            </div>
+        </div>
+        <div class="sub-items" v-if="(item.isSelected || (item.subItemIsSelected)) && item.subItems &&
+                                        item.subItems.length > 0">
+            <select-element :item="subItem" v-for="subItem in item.subItems"
+                            :key="subItem.id" :is-root="false" :select-multiple="selectMultiple">
+            </select-element>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SelectElement from './SelectElement';
+
+    export default {
+        name: 'select-element',
+        props: ['item', 'isRoot', 'selectMultiple'],
+        components: {SelectElement},
+        methods: {
+            select() {
+                this.item.isSelected = !this.item.isSelected;
+                this.disSelectSubItems(this.item.subItems);
+                this.$emit('select-changed');
+            },
+            disSelectSubItems(subItems) {
+                for (let item of subItems) {
+                    item.isSelected = false;
+                    if (item.subItems && item.subItems.length > 0) {
+                        this.disSelectSubItems(item.subItems);
+                    }
+                }
+            },
+        }
+    }
+</script>
+
+<style lang="scss">
+    .select-item {
+        .item-container {
+            padding: 12px;
+            border-bottom: 1px solid $divider;
+            cursor: pointer;
+            .item-icon {
+                float: right;
+                color: $success-text;
+            }
+            .item {
+                user-select: none;
+            }
+            .item.main-bold {
+                color: $primary-color;
+                //font-weight: 500;
+            }
+        }
+        :hover.item-container {
+            background-color: #EEEEEE;
+        }
+        .item-container.item-selected {
+            font-weight: 500;
+            color: $success-text;
+            background-color: #EEEEEE;
+            .item {
+                color: $success-text;
+            }
+        }
+        .sub-item-selected {
+            font-weight: 500;
+            background-color: #EEEEEE;
+        }
+    }
+</style>
