13ad6f2e96bc31b88ee4bab55791dd3732839ec6
Roger Waldvogel <info@elyoos.org>
2018-08-09 09:30:57 +0200

    using ely select.

--- elyoos/components/region/dialog/Region.vue
+++ elyoos/components/region/dialog/Region.vue
@@ -5,8 +5,10 @@
         </div>
         <v-card-text id="dialog-create-region-commitment-content">
             <div class="user-description">{{description}}</div>
-            <select-region-container :regions="regions" select-multiple="true">
-            </select-region-container>
+            <ely-select :items="regions" :existing-items="existingRegions" :select-multiple="true"
+                        :single-selected-item-id="'international'" :dis-select-parent-items="true"
+                        @select-changed="selectChanged">
+            </ely-select>
         </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
@@ -14,7 +16,7 @@
             <v-btn color="primary" flat @click.native="$emit('close-dialog')">
                 {{$t("common:button.close")}}
             </v-btn>
-            <v-btn color="primary" @click.native="finish()" :loading="loading"
+            <v-btn color="primary" @click.native="$emit('finish', selectedRegions)" :loading="loading"
                    :disabled="selectedRegions.length === 0 || loading || !hasChanged">
                 {{actionButtonText}}
             </v-btn>
@@ -23,35 +25,38 @@
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
-    import SelectRegionContainer from '~/components/region/select/RegionContainer';
+    import ElySelect from '~/components/common/select/Select';
 
     export default {
-        props: ['actionButtonText', 'description', 'loading', 'initSelectedRegions'],
-        components: {SelectRegionContainer},
+        props: ['actionButtonText', 'description', 'loading', 'existingRegions'],
+        components: {ElySelect},
         data() {
-            return {loadingRegions: false}
+            return {regions: [], selectedRegions: [], loadingRegions: false, hasChanged: true}
         },
-        computed: {
-            hasChanged() {
-                if (this.initSelectedRegions && this.selectedRegions) {
-                    let initRegions = JSON.parse(JSON.stringify(this.initSelectedRegions));
-                    let selectedRegions = JSON.parse(JSON.stringify(this.selectedRegions));
-                    return JSON.stringify(initRegions.sort()) !== JSON.stringify(selectedRegions.sort());
+        async mounted() {
+            try {
+                this.loadingRegions = true;
+                this.regions = await this.$axios.$get(`/region`, {params: {language: this.$store.state.i18n.language}});
+                if (this.existingRegions) {
+                    this.selectedRegions = JSON.parse(JSON.stringify(this.existingRegions));
+                }
+                this.hasChanged = this.checkHasChanged();
+
+            } finally {
+                this.loadingRegions = false;
+            }
+        },
+        methods: {
+            checkHasChanged() {
+                if (this.existingRegions) {
+                    return this.existingRegions.length !== this.selectedRegions.length &&
+                        !this.existingRegions.every(existingRegion => this.selectedRegions.includes(existingRegion));
                 }
                 return true;
             },
-            ...mapGetters({selectedRegions: 'selectRegions/getSelected', regions: 'selectRegions/getRegions'})
-        },
-        async mounted() {
-            this.loadingRegions = true;
-            await this.$store.dispatch('selectRegions/getRegions');
-            this.$store.commit('selectRegions/SET_SELECTED_REGIONS', this.initSelectedRegions);
-            this.loadingRegions = false;
-        },
-        methods: {
-            finish() {
-                this.$emit('finish', this.selectedRegions);
+            selectChanged(selectedRegions) {
+                this.selectedRegions = selectedRegions;
+                this.hasChanged = this.checkHasChanged();
             }
         }
     }
