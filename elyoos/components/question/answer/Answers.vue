f73b6fce19003b0a0f8de7fc9f5b84599a515573
Roger Waldvogel <info@elyoos.org>
2018-08-27 16:18:07 +0200

    Scaffold for the footer of the answers.

--- elyoos/components/question/answer/Answers.vue
+++ elyoos/components/question/answer/Answers.vue
@@ -4,15 +4,37 @@
             <div v-for="answer in answers" itemprop="suggestedAnswer" itemscope class="feed-card ely-card"
                  itemtype="http://schema.org/Answer">
                 <text-card v-if="answer.answerType === 'Text'" :answer="answer" :hide-question="true">
+                    <answer-footer slot="footer" :creator="answer.creator" :number-of-up-votes="answer.upVotes"
+                                   :has-voted="answer.hasVoted" :is-admin="answer.isAdmin" :answer-id="answer.answerId"
+                                   :number-of-notes="answer.numberOfNotes">
+                    </answer-footer>
                 </text-card>
                 <youtube-card v-else-if="answer.answerType === 'Youtube'" :answer="answer" :hide-question="true">
+                    <answer-footer slot="footer" :creator="answer.creator" :number-of-up-votes="answer.upVotes"
+                                   :has-voted="answer.hasVoted" :is-admin="answer.isAdmin" :answer-id="answer.answerId"
+                                   :number-of-notes="answer.numberOfNotes">
+                    </answer-footer>
                 </youtube-card>
                 <link-card v-else-if="answer.answerType === 'Link'" :answer="answer" :hide-question="true">
+                    <answer-footer slot="footer" :creator="answer.creator" :number-of-up-votes="answer.upVotes"
+                                   :has-voted="answer.hasVoted" :is-admin="answer.isAdmin" :answer-id="answer.answerId"
+                                   :number-of-notes="answer.numberOfNotes">
+                    </answer-footer>
                 </link-card>
                 <book-card v-else-if="answer.answerType === 'Book'" :answer="answer" :hide-question="true">
+                    <answer-footer slot="footer" :creator="answer.creator" :number-of-up-votes="answer.upVotes"
+                                   :has-voted="answer.hasVoted" :is-admin="answer.isAdmin" :answer-id="answer.answerId"
+                                   :number-of-notes="answer.numberOfNotes">
+                    </answer-footer>
                 </book-card>
                 <commitment-card v-else-if="answer.answerType === 'Commitment'" :answer="answer"
                                  :hide-question="true">
+                    <commitment-answer-footer slot="footer"
+                                              :creator="answer.creator" :number-of-up-votes="answer.upVotes"
+                                              :has-voted="answer.hasVoted" :is-admin="answer.isAdmin"
+                                              :answer-id="answer.answerId"
+                                              :regions="answer.regions">
+                    </commitment-answer-footer>
                 </commitment-card>
             </div>
         </div>
@@ -25,9 +47,11 @@
     import TextCard from '~/components/feed/card/Text'
     import LinkCard from '~/components/feed/card/Link'
     import YoutubeCard from '~/components/feed/card/Youtube'
+    import AnswerFooter from './card/footer/CommonAnswer';
+    import CommitmentAnswerFooter from './card/footer/CommitmentAnswer';
 
     export default {
-        components: {TextCard, YoutubeCard, LinkCard, BookCard, CommitmentCard},
+        components: {TextCard, YoutubeCard, LinkCard, BookCard, CommitmentCard, AnswerFooter, CommitmentAnswerFooter},
         computed: {
             answers() {
                 return this.$store.state.question.question.answers;
---
+++ elyoos/components/question/answer/card/footer/CommonAnswer.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="card-footer-feed">
+        <div class="footer-icon">
+            <user-menu :menu-title="creatorTitle" :user-image="creator.userImagePreview"
+                       :user-name="creator.name" :user-id="creator.userId" :user-slug="creator.slug"
+                       :is-trust-user="creator.isTrustUser" :is-logged-in-user="creator.isLoggedInUser">
+                <div class="user-icon creator-icon" slot="icon">
+                    <img :src="creator.userImage">
+                </div>
+            </user-menu>
+        </div>
+        <div class="footer-icon">
+            <v-tooltip bottom>
+                <div slot="activator">
+                    <v-icon medium class="action-icon">
+                        mdi-message-reply-text
+                    </v-icon>
+                    <span class="footer-description number">{{numberOfNotes}}</span>
+                </div>
+                <span>{{$t("common:feedCard.note.tooltipDescription")}}</span>
+            </v-tooltip>
+        </div>
+        <v-spacer></v-spacer>
+        <div class="footer-up-vote-button">
+            <span class="description left-side">{{numberOfUpVotes}}</span>
+            <v-tooltip bottom v-if="!hasVoted || isAdmin">
+                <v-btn slot="activator" small fab color="not-up-voted"
+                       :disabled="isAdmin || upVoteRunning" @click="upVote()">
+                    <v-icon>mdi-thumb-up-outline</v-icon>
+                </v-btn>
+                <span>{{$t("common:feedCard.upVote.descriptionUserHasNotUpVoted")}}</span>
+            </v-tooltip>
+            <v-tooltip bottom v-else>
+                <v-btn slot="activator" fab small color="secondary" :disabled="isAdmin || upVoteRunning"
+                       @click="downVote()">
+                    <v-icon>mdi-thumb-up</v-icon>
+                </v-btn>
+                <span>{{$t("common:feedCard.upVote.descriptionUserHasUpVoted")}}</span>
+            </v-tooltip>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserMenu from '~/components/feed/card/footer/menu/User';
+
+    export default {
+        props: ['creator', 'numberOfNotes', 'numberOfUpVotes', 'hasVoted', 'isAdmin', 'answerId'],
+        components: {UserMenu},
+        data() {
+            return {
+                showLoginRequired: false, showCreateNoteDialog: false, showNotes: false, upVoteRunning: false,
+                sortNotes: null, showError: false
+            }
+        },
+        computed: {
+            creatorTitle() {
+                if (this.creator && this.creator.isLoggedInUser) {
+                    return this.$t("pages:feeds.menu.creatorAnswer.titleIsLoggedInUser");
+                }
+                return this.$t("pages:feeds.menu.creatorAnswer.title");
+            }
+        },
+        methods: {
+            async voteCommand(command) {
+                if (this.$store.state.auth.userIsAuthenticated) {
+                    try {
+                        this.upVoteRunning = true;
+                        await this.$store.dispatch(command, this.answerId);
+                    } catch (error) {
+                        this.showError = true;
+                    } finally {
+                        this.upVoteRunning = false;
+                    }
+                } else {
+                    this.showLoginRequired = true;
+                }
+            },
+            async upVote() {
+                this.voteCommand('question/upVoteAnswer');
+            },
+            async downVote() {
+                this.voteCommand('question/downVoteAnswer');
+            },
+            toggleNotes() {
+                if (!this.showNotes) {
+                    if (this.$store.state.auth.userIsAuthenticated && this.answer.numberOfNotes === 0) {
+                        this.showCreateNoteDialog = true;
+                    } else if (this.answer.numberOfNotes === 0) {
+                        this.showLoginRequired = true;
+                    } else {
+                        this.showNotes = true;
+                    }
+                } else {
+                    this.showNotes = false;
+                }
+            },
+            noteAdded() {
+                this.showCreateNoteDialog = false;
+                this.showNotes = true;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .feed-card {
+        .card-footer-feed {
+            .footer-up-vote-button {
+                .not-up-voted {
+                    background-color: #607D8B;
+                    i.v-icon {
+                        color: white;
+                    }
+                }
+                button {
+                    margin: 0;
+                }
+                .description.left-side {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: $secondary-text;
+                    margin-right: 8px;
+                }
+            }
+        }
+    }
+</style>
