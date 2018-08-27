859d3319a75cf3349a614410ce13219acc5f1f41
Roger Waldvogel <info@elyoos.org>
2018-08-27 16:52:20 +0200

    Pulled up vote functionality to separate component.

--- elyoos/components/question/answer/card/footer/CommonAnswer.vue
+++ elyoos/components/question/answer/card/footer/CommonAnswer.vue
@@ -21,32 +21,19 @@
             </v-tooltip>
         </div>
         <v-spacer></v-spacer>
-        <div class="footer-up-vote-button">
-            <span class="description left-side">{{numberOfUpVotes}}</span>
-            <v-tooltip bottom v-if="!hasVoted || isAdmin">
-                <v-btn slot="activator" small fab color="not-up-voted"
-                       :disabled="isAdmin || upVoteRunning" @click="upVote()">
-                    <v-icon>mdi-thumb-up-outline</v-icon>
-                </v-btn>
-                <span>{{$t("common:feedCard.upVote.descriptionUserHasNotUpVoted")}}</span>
-            </v-tooltip>
-            <v-tooltip bottom v-else>
-                <v-btn slot="activator" fab small color="secondary" :disabled="isAdmin || upVoteRunning"
-                       @click="downVote()">
-                    <v-icon>mdi-thumb-up</v-icon>
-                </v-btn>
-                <span>{{$t("common:feedCard.upVote.descriptionUserHasUpVoted")}}</span>
-            </v-tooltip>
-        </div>
+        <up-vote-button :number-of-up-votes="numberOfUpVotes" :has-voted="hasVoted"
+                        :is-admin="isAdmin" :answer-id="answerId">
+        </up-vote-button>
     </div>
 </template>
 
 <script>
     import UserMenu from '~/components/feed/card/footer/menu/User';
+    import UpVoteButton from './UpVote';
 
     export default {
         props: ['creator', 'numberOfNotes', 'numberOfUpVotes', 'hasVoted', 'isAdmin', 'answerId'],
-        components: {UserMenu},
+        components: {UserMenu, UpVoteButton},
         data() {
             return {
                 showLoginRequired: false, showCreateNoteDialog: false, showNotes: false, upVoteRunning: false,
@@ -62,26 +49,6 @@
             }
         },
         methods: {
-            async voteCommand(command) {
-                if (this.$store.state.auth.userIsAuthenticated) {
-                    try {
-                        this.upVoteRunning = true;
-                        await this.$store.dispatch(command, this.answerId);
-                    } catch (error) {
-                        this.showError = true;
-                    } finally {
-                        this.upVoteRunning = false;
-                    }
-                } else {
-                    this.showLoginRequired = true;
-                }
-            },
-            async upVote() {
-                this.voteCommand('question/upVoteAnswer');
-            },
-            async downVote() {
-                this.voteCommand('question/downVoteAnswer');
-            },
             toggleNotes() {
                 if (!this.showNotes) {
                     if (this.$store.state.auth.userIsAuthenticated && this.answer.numberOfNotes === 0) {
@@ -104,25 +71,5 @@
 </script>
 
 <style lang="scss">
-    .feed-card {
-        .card-footer-feed {
-            .footer-up-vote-button {
-                .not-up-voted {
-                    background-color: #607D8B;
-                    i.v-icon {
-                        color: white;
-                    }
-                }
-                button {
-                    margin: 0;
-                }
-                .description.left-side {
-                    font-size: 14px;
-                    font-weight: 700;
-                    color: $secondary-text;
-                    margin-right: 8px;
-                }
-            }
-        }
-    }
+
 </style>
---
+++ elyoos/components/question/answer/card/footer/UpVote.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <div class="footer-up-vote-button">
+            <span class="description left-side">{{numberOfUpVotes}}</span>
+            <v-tooltip bottom v-if="!hasVoted || isAdmin">
+                <v-btn slot="activator" small fab color="not-up-voted"
+                       :disabled="isAdmin || upVoteRunning" @click="upVote()">
+                    <v-icon>mdi-thumb-up-outline</v-icon>
+                </v-btn>
+                <span>{{$t("common:feedCard.upVote.descriptionUserHasNotUpVoted")}}</span>
+            </v-tooltip>
+            <v-tooltip bottom v-else>
+                <v-btn slot="activator" fab small color="up-voted" :disabled="isAdmin || upVoteRunning"
+                       @click="downVote()">
+                    <v-icon>mdi-thumb-up</v-icon>
+                </v-btn>
+                <span>{{$t("common:feedCard.upVote.descriptionUserHasUpVoted")}}</span>
+            </v-tooltip>
+        </div>
+        <login-required-dialog v-if="showLoginRequired" @close-dialog="showLoginRequired = false">
+        </login-required-dialog>
+        <v-snackbar top v-model="showError" color="error" :timeout="0">{{$t("common:error.unknown")}}
+            <v-btn dark flat @click="showError = false">{{$t("common:button.close")}}</v-btn>
+        </v-snackbar>
+    </div>
+</template>
+
+<script>
+    import UserMenu from '~/components/feed/card/footer/menu/User';
+    import LoginRequiredDialog from '~/components/common/dialog/LoginRequired';
+
+    export default {
+        props: ['numberOfUpVotes', 'hasVoted', 'isAdmin', 'answerId'],
+        components: {UserMenu, LoginRequiredDialog},
+        data() {
+            return {
+                showLoginRequired: false, upVoteRunning: false, showError: false
+            }
+        },
+        methods: {
+            async voteCommand(command) {
+                if (this.$store.state.auth.userIsAuthenticated) {
+                    try {
+                        this.upVoteRunning = true;
+                        await this.$store.dispatch(command, this.answerId);
+                    } catch (error) {
+                        this.showError = true;
+                    } finally {
+                        this.upVoteRunning = false;
+                    }
+                } else {
+                    this.showLoginRequired = true;
+                }
+            },
+            async upVote() {
+                this.voteCommand('question/upVoteAnswer');
+            },
+            async downVote() {
+                this.voteCommand('question/downVoteAnswer');
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .feed-card {
+        .card-footer-feed {
+            .footer-up-vote-button {
+                .not-up-voted {
+                    background-color: #607D8B;
+                    i.v-icon {
+                        color: white;
+                    }
+                }
+                .up-voted {
+                    background-color: #009688;
+                    i.v-icon {
+                        color: white;
+                    }
+                }
+                button {
+                    margin: 0;
+                }
+                .description.left-side {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: $secondary-text;
+                    margin-right: 8px;
+                }
+            }
+        }
+    }
+</style>
