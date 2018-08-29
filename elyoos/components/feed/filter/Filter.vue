7a100c32c384ac8f0ca8d142c5a161d6d5be47ed
Roger Waldvogel <info@elyoos.org>
2018-08-29 14:15:14 +0200

    same height of icons.

--- elyoos/components/feed/filter/Filter.vue
+++ elyoos/components/feed/filter/Filter.vue
@@ -141,8 +141,9 @@
                 .right-filter {
                     cursor: pointer;
                     display: inline-block;
+                    vertical-align: top;
                     margin-left: 16px;
-                    padding: 2px 0 4px 0;
+                    padding: 0 0 4px 0;
                     i.v-icon {
                         font-size: 20px;
                     }
