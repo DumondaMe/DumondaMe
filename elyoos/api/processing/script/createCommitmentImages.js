9c80598d57b733801f3cba571c40e894afe8b6b8
Roger Waldvogel <info@elyoos.org>
2018-08-30 15:24:05 +0200

    Commitment image size added.

--- elyoos/api/processing/script/createCommitmentImages.js
+++ elyoos/api/processing/script/createCommitmentImages.js
@@ -7,9 +7,14 @@ const addTitleImage = async function (pages) {
     for (let result of pages) {
         try {
             let titleImage = await cdn.getObject(`commitment/${result.commitmentId}/title.jpg`, process.env.BUCKET_PUBLIC);
-            let newTitleImage460x460 = await sharp(titleImage.Body).resize(460, 460).jpeg({quality: 93})
+            let newTitleImage460x460 = await sharp(titleImage.Body).resize(460, 460).jpeg({quality: 80})
+                .toBuffer();
+            let newTitleImage210x210 = await sharp(titleImage.Body).resize(210, 210).jpeg({quality: 80})
                 .toBuffer();
             await cdn.uploadBuffer(newTitleImage460x460, `commitment/${result.commitmentId}/460x460/title.jpg`, process.env.BUCKET_PUBLIC);
+            await cdn.uploadBuffer(newTitleImage210x210, `commitment/${result.commitmentId}/210x210/title.jpg`, process.env.BUCKET_PUBLIC);
+            await cdn.deleteFolder(`commitment/${result.commitmentId}/148x148/`, process.env.BUCKET_PUBLIC);
+            console.log(`Uploaded images for commitment ${result.commitmentId}`)
         } catch(error) {
             logger.error(`Error resize title image for commitment ${result.commitmentId}`);
         }
