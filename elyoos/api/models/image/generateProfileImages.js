8e8a3f34715eeb1b8f66e92443d03e4408538376
Roger Waldvogel <info@elyoos.org>
2018-09-09 18:33:09 +0200

    Preview 148px. Thumbnail 40px

--- elyoos/api/models/image/generateProfileImages.js
+++ elyoos/api/models/image/generateProfileImages.js
@@ -9,9 +9,9 @@ let cdn = require('elyoos-server-lib').cdn;
 module.exports = {
     generateProfileImage: async function (originalFilePath, userId) {
         let previewBuffer = await sharp(originalFilePath).background({r: 255, g: 255, b: 255, alpha: 0}).flatten()
-            .resize(100).max().jpeg({quality: 93}).withoutEnlargement().toBuffer();
+            .resize(148).max().jpeg({quality: 93}).withoutEnlargement().toBuffer();
         let thumbnailBuffer = await sharp(originalFilePath).background({r: 255, g: 255, b: 255, alpha: 0}).flatten()
-            .resize(48).max().jpeg({quality: 90}).withoutEnlargement().toBuffer();
+            .resize(40).max().jpeg({quality: 90}).withoutEnlargement().toBuffer();
         let profileBuffer = await sharp(originalFilePath).background({r: 255, g: 255, b: 255, alpha: 0}).flatten()
             .resize(400).max().jpeg({quality: 92}).withoutEnlargement().toBuffer();
 
