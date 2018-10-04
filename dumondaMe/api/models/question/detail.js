d0ffbc817250e11906719c5fcc6bd71b2fa5a95b
Roger Waldvogel <info@elyoos.org>
2018-10-04 15:04:28 +0200

    Privacy handling implemented for creator of the question.

--- dumondaMe/api/models/question/detail.js
+++ dumondaMe/api/models/question/detail.js
@@ -8,6 +8,28 @@ const answers = require('./answers');
 const slug = require('limax');
 const linkifyHtml = require('linkifyjs/html');
 
+const getCreator = async function (user, isTrustUser, creatorTrustUser, userId) {
+    if (user.privacyMode === 'public' || user.userId === userId ||
+        (user.privacyMode === 'publicEl' && userId !== null) ||
+        (user.privacyMode === 'onlyContact' && creatorTrustUser)) {
+        return {
+            isAnonymous: false,
+            name: user.name,
+            userId: user.userId,
+            slug: slug(user.name),
+            userImage: await cdn.getSignedUrl(`profileImage/${user.userId}/thumbnail.jpg`),
+            userImagePreview: await cdn.getSignedUrl(`profileImage/${user.userId}/profilePreview.jpg`),
+            isTrustUser,
+        };
+    } else {
+        return {
+            isAnonymous: true,
+            userImage: await cdn.getSignedUrl(`profileImage/default/thumbnail.jpg`),
+            userImagePreview: await cdn.getSignedUrl(`profileImage/default/profilePreview.jpg`)
+        }
+    }
+};
+
 const getQuestion = async function (questionId, answerId, language, userId, isSuperUser) {
     let response = await db.cypher().match(`(question:Question {questionId: {questionId}})<-[:IS_CREATOR]-(user:User)`)
         .optionalMatch(`(question)-[:ANSWER]->(answer)`)
@@ -19,6 +41,7 @@ const getQuestion = async function (questionId, answerId, language, userId, isSu
                  collect(DISTINCT {description: topic.${language}, id: topic.topicId}) AS topics,
                  EXISTS((:User {userId: {userId}})-[:IS_CREATOR]->(question)) AS isAdmin,
                  EXISTS((:User {userId: {userId}})-[:IS_CONTACT]->(user)) AS isTrustUser,
+                 EXISTS((user)-[:IS_CONTACT]->(:User {userId: {userId}})) AS creatorTrustUser,
                  EXISTS((:User {userId: {userId}})-[:WATCH]->(question)) AS userWatchesQuestion`)
         .end({questionId, userId}).send([answers.getAnswersCommand(questionId, answerId, 0, userId).getCommand()]);
     if (response[1].length === 1) {
@@ -34,14 +57,8 @@ const getQuestion = async function (questionId, answerId, language, userId, isSu
         question.numberOfWatches = questionResponse.numberOfWatches;
         question.numberOfAnswers = questionResponse.numberOfAnswers;
         question.userWatchesQuestion = questionResponse.userWatchesQuestion;
-        question.creator = {
-            name: questionResponse.user.name,
-            userId: questionResponse.user.userId,
-            slug: slug(questionResponse.user.name),
-            userImage: await cdn.getSignedUrl(`profileImage/${questionResponse.user.userId}/thumbnail.jpg`),
-            userImagePreview: await cdn.getSignedUrl(`profileImage/${questionResponse.user.userId}/profilePreview.jpg`),
-            isTrustUser: questionResponse.isTrustUser,
-        };
+        question.creator = await getCreator(questionResponse.user, questionResponse.isTrustUser,
+            questionResponse.creatorTrustUser, userId);
         question.hasMoreAnswers = response[0].length > answers.PAGE_SIZE;
         if (question.hasMoreAnswers) {
             response[0] = response[0].slice(0, answers.PAGE_SIZE);
---
+++ dumondaMe/api/test/test/e2e/tests/question/getQuestionPrivacySpec.js
@@ -0,0 +1,119 @@
+'use strict';
+
+let users = require('dumonda-me-server-test-util').user;
+let dbDsl = require('dumonda-me-server-test-util').dbDSL;
+let requestHandler = require('dumonda-me-server-test-util').requestHandler;
+let moment = require('moment');
+const should = require('chai').should();
+
+describe('Privacy settings of the user created a question or answered question', function () {
+
+    let startTime;
+
+    beforeEach(async function () {
+        await dbDsl.init(2);
+        await requestHandler.logout();
+        startTime = Math.floor(moment.utc().valueOf() / 1000);
+
+        dbDsl.createRegion('region-1', {de: 'Region1De', en: 'Region1En'});
+
+        dbDsl.createMainTopic({topicId: 'topic1', descriptionDe: 'topic1De', descriptionEn: 'topic1En'});
+
+        dbDsl.createCommitment('2', {
+            title: 'Das ist ein Engagement', regions: ['region-1'], adminId: '2', topics: ['topic1'],
+            language: 'de', created: 700, modified: 701, website: 'https://www.example.org/'
+        }, []);
+
+        dbDsl.createQuestion('1', {
+            creatorId: '2', question: 'Das ist eine Frage', description: 'Test dumonda.me change the world',
+            topics: ['topic1', 'topic2'], language: 'de', modified: 700
+        });
+    });
+
+    afterEach(function () {
+        return requestHandler.logout();
+    });
+
+    it('Hide creator of question when not logged in and privacy setting is publicEl', async function () {
+
+        dbDsl.setUserPrivacy('2', {privacyMode: 'publicEl'});
+        await dbDsl.sendToDb();
+        let res = await requestHandler.get('/api/question/detail/1', {language: 'de'});
+        res.status.should.equal(200);
+        res.body.questionId.should.equals('1');
+        res.body.creator.isAnonymous.should.equals(true);
+        res.body.creator.userImage.should.equals('profileImage/default/thumbnail.jpg');
+        res.body.creator.userImagePreview.should.equals('profileImage/default/profilePreview.jpg');
+        should.not.exist(res.body.creator.userId);
+        should.not.exist(res.body.creator.name);
+        should.not.exist(res.body.creator.slug);
+        should.not.exist(res.body.creator.isLoggedInUser);
+        should.not.exist(res.body.creator.isTrustUser);
+    });
+
+    it('Hide creator of question when logged in and privacy setting is onlyContact', async function () {
+        dbDsl.setUserPrivacy('2', {privacyMode: 'onlyContact'});
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/question/detail/1', {language: 'de'});
+        res.status.should.equal(200);
+        res.body.questionId.should.equals('1');
+        res.body.creator.isAnonymous.should.equals(true);
+        res.body.creator.userImage.should.equals('profileImage/default/thumbnail.jpg');
+        res.body.creator.userImagePreview.should.equals('profileImage/default/profilePreview.jpg');
+        should.not.exist(res.body.creator.userId);
+        should.not.exist(res.body.creator.name);
+        should.not.exist(res.body.creator.slug);
+        should.not.exist(res.body.creator.isLoggedInUser);
+        should.not.exist(res.body.creator.isTrustUser);
+    });
+
+    it('Show creator of question when logged in user', async function () {
+        dbDsl.setUserPrivacy('2', {privacyMode: 'onlyContact'});
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser2);
+        let res = await requestHandler.get('/api/question/detail/1', {language: 'de'});
+        res.status.should.equal(200);
+        res.body.questionId.should.equals('1');
+        res.body.creator.isAnonymous.should.equals(false);
+        res.body.creator.name.should.equals('user Meier2');
+        res.body.creator.userId.should.equals('2');
+        res.body.creator.slug.should.equals('user-meier2');
+        res.body.creator.userImage.should.equals('profileImage/2/thumbnail.jpg');
+        res.body.creator.userImagePreview.should.equals('profileImage/2/profilePreview.jpg');
+        res.body.creator.isTrustUser.should.equals(false);
+    });
+
+    it('Show creator of question when user is in trust circle', async function () {
+        dbDsl.setUserPrivacy('2', {privacyMode: 'onlyContact'});
+        dbDsl.createContactConnection('2', '1');
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/question/detail/1', {language: 'de'});
+        res.status.should.equal(200);
+        res.body.questionId.should.equals('1');
+        res.body.creator.isAnonymous.should.equals(false);
+        res.body.creator.name.should.equals('user Meier2');
+        res.body.creator.userId.should.equals('2');
+        res.body.creator.slug.should.equals('user-meier2');
+        res.body.creator.userImage.should.equals('profileImage/2/thumbnail.jpg');
+        res.body.creator.userImagePreview.should.equals('profileImage/2/profilePreview.jpg');
+        res.body.creator.isTrustUser.should.equals(false);
+    });
+
+    it('Show creator of question when user is logged in and privacy is publicEl', async function () {
+        dbDsl.setUserPrivacy('2', {privacyMode: 'publicEl'});
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/question/detail/1', {language: 'de'});
+        res.status.should.equal(200);
+        res.body.questionId.should.equals('1');
+        res.body.creator.isAnonymous.should.equals(false);
+        res.body.creator.name.should.equals('user Meier2');
+        res.body.creator.userId.should.equals('2');
+        res.body.creator.slug.should.equals('user-meier2');
+        res.body.creator.userImage.should.equals('profileImage/2/thumbnail.jpg');
+        res.body.creator.userImagePreview.should.equals('profileImage/2/profilePreview.jpg');
+        res.body.creator.isTrustUser.should.equals(false);
+    });
+});
