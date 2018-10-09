a3ff0bf787676a7bc533061c0c1126266d7b7ce7
Roger Waldvogel <info@elyoos.org>
2018-10-09 10:08:07 +0200

    Removed unused feed help. Added information for the user in the sidebar.

---
+++ dumondaMe/components/feed/info/Activity.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="feed-activity-info ely-card">
+        <h3 class="feed-desktop-sidebar-title">{{$t('pages:feeds.activityInfo.title')}}</h3>
+        <div class="description">{{$t('pages:feeds.activityInfo.description')}}</div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+    .feed-activity-info {
+        margin-bottom: 18px;
+        font-size: 14px;
+        font-weight: 300;
+        h3.feed-desktop-sidebar-title {
+            margin-bottom: 12px;
+        }
+    }
+</style>
