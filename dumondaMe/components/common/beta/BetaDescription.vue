f0dad208400334554d7e86f197e9446c864b9c26
Roger Waldvogel <info@elyoos.org>
2018-10-23 11:37:02 +0200

    Bugfix: using class instead of id.

--- dumondaMe/components/common/beta/BetaDescription.vue
+++ dumondaMe/components/common/beta/BetaDescription.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="beta-version-description" class="ely-card">
+    <div class="beta-version-description ely-card">
         <h3 class="beta-version-title">{{$t('common:beta.title')}}</h3>
         <div class="description">{{$t('common:beta.description')}}</div>
     </div>
@@ -9,7 +9,7 @@
 </script>
 
 <style lang="scss">
-    #beta-version-description {
+    .beta-version-description {
         margin-bottom: 12px;
         font-size: 14px;
         font-weight: 300;
