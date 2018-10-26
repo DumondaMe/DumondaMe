bb51b5cc52c05006d179c99e950710fd3ee218f3
Roger Waldvogel <info@elyoos.org>
2018-10-26 17:32:59 +0200

    Property added to show if more users, questions or commitments available.

--- dumondaMe/api/models/search/index.js
+++ dumondaMe/api/models/search/index.js
@@ -6,15 +6,23 @@ const commitment = require('./commitment/index');
 const commitmentResponse = require('./commitment/response');
 const question = require('./question/index');
 const questionResponse = require('./question/response');
+const moreSearchResult = require('../util/moreSearchResults');
+
+const NUMBER_OF_ELEMENTS = 6;
 
 const search = async function (query, language, userId) {
-    let result = await user.searchCommand(query, language, userId, 0, 6)
-        .send([commitment.searchCommand(query, language, userId, 0, 6).getCommand(),
-            question.searchCommand(query, language, userId, 0, 6).getCommand()]);
+    let result = await user.searchCommand(query, language, userId, 0, NUMBER_OF_ELEMENTS + 1)
+        .send([commitment.searchCommand(query, language, userId, 0, NUMBER_OF_ELEMENTS + 1).getCommand(),
+            question.searchCommand(query, language, userId, 0, NUMBER_OF_ELEMENTS + 1).getCommand()]);
+
+    let hasMoreUsers = moreSearchResult.getHasMoreResults(result[2], NUMBER_OF_ELEMENTS);
+    let hasMoreQuestions = moreSearchResult.getHasMoreResults(result[1], NUMBER_OF_ELEMENTS);
+    let hasMoreCommitments = moreSearchResult.getHasMoreResults(result[0], NUMBER_OF_ELEMENTS);
+
     return {
-        users: await userResponse.getResponse(result[2], userId),
-        questions: await questionResponse.getResponse(result[1], userId),
-        commitments: commitmentResponse.getResponse(result[0])
+        users: await userResponse.getResponse(result[2], userId), hasMoreUsers,
+        questions: await questionResponse.getResponse(result[1], userId), hasMoreQuestions,
+        commitments: commitmentResponse.getResponse(result[0]), hasMoreCommitments
     };
 };
 
