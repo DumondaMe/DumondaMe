c40d4fdd4790b7bcc3528a9b4964cb156cc8fd44
Roger Waldvogel <info@elyoos.org>
2018-10-29 14:07:33 +0100

    Updated vision text.

--- dumondaMe/pages/vision/index.vue
+++ dumondaMe/pages/vision/index.vue
@@ -5,24 +5,24 @@
             <p>Wie verändern wir die Welt zu einem Ort, wo ein nachhaltiger Umgang mit der Natur, soziale Gerechtigkeit,
                 eine hohe Lebensqualität und Frieden so selbstverständlich ist wie die Luft zum Atmen? Um dies zu
                 verwirklichen ist es unabdingbar, dass wir die notwendigen Fragen stellen und die passenden Antworten
-                umsetzen. Dazu braucht es Engagement in verschiedenen Themenbereichen wie Politik, Wirtschaft,
-                Gesellschaftliche Entwicklung, Umweltschutz, Spiritualität, Gesundheit, Bildung und vielen mehr.</p>
+                umsetzen. Dazu braucht es Engagement in verschiedenen Themenbereichen wie Umweltschutz, Wirtschaft,
+                Gesundheit, Bildung, Spiritualität und vielen mehr.</p>
             <p>DumondaMe dient als Katalysator, welcher die bereits vorhanden Lösungsvorschläge sichtbar macht und das
-                Erarbeiten von Neuen unterstützt. Das soziale Netzwerk nutzt dabei das Potenzial des Internets, um mit
+                Erarbeiten von neuen unterstützt. Das soziale Netzwerk nutzt dabei das Potenzial des Internets, um mit
                 vereinten Kräften einen Paradigmenwechsel zu ermöglichen. Einer welcher uns nicht länger nur von einer
                 besseren Zukunft träumen lässt, sondern diese in die Gegenwart bringt.</p>
         </div>
         <div v-else-if="selectedLanguage === 'en'">
             <h1>Our Vision</h1>
-            <p>How do we change the world into a place where sustainable interaction with nature, social justice and
-                peace are as natural as the air we breathe? To achieve this, it is essential that we ask the necessary
-                questions and implement the appropriate answers. This requires commitment in various areas such as
-                politics, business, social development, environmental protection, spirituality, health, education and
-                many more.</p>
-            <p>DumondaMe serves as a catalyst, which makes the already existing solution proposals visible and supports the
-                development of new ones. The social network uses the potential of the Internet to make a paradigm shift
-                possible by joining forces. One that no longer only makes us dream of a better future, but brings it
-                into the present.</p>
+            <p>How do we change the world into a place where a sustainable interaction with nature, social justice, a
+                high quality of life and peace are as self-evident as the air we breathe? In order to achieve this, it
+                is indispensable that we ask the necessary questions and implement the appropriate answers. This
+                requires commitment in various areas such as environmental protection, the economy, health, education,
+                spirituality and many more.</p>
+            <p>DumondaMe serves as a catalyst, which makes already existing solutions visible and supports the
+                development of new ones. The social network uses the potential of the Internet in order to enable a
+                paradigm shift with united forces. One that no longer lets us only dream of a better future, but brings
+                it into the present.</p>
         </div>
     </div>
 </template>
