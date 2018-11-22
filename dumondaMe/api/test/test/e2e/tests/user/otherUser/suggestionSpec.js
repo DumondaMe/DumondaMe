b41eb9c2686ca9e5ef1e58e62f5a684a9494dc5d
Roger Waldvogel <info@dumonda.me>
2018-11-22 11:54:45 +0100

    Do not show logged in user. Wait for image response.

--- dumondaMe/api/test/test/e2e/tests/user/otherUser/suggestionSpec.js
+++ dumondaMe/api/test/test/e2e/tests/user/otherUser/suggestionSpec.js
@@ -131,7 +131,7 @@ describe('Suggestion of other users for trust circle', function () {
         res.body.users[1].profileUrl.should.equal('profileImage/5/thumbnail.jpg');
     });
 
-    it('When no person in trust circle then sort by most user have suggested user in trust circle', async function () {
+    it('When no person in trust circle then suggested user -> equal topic, equal region, number of trust circle', async function () {
         dbDsl.createRegion('international', {de: 'internationalDe', en: 'internationalEn'});
         dbDsl.createRegion('region-1', {parentRegionId: 'international', de: 'Region1De', en: 'Region1En'});
         dbDsl.createRegion('region-2', {parentRegionId: 'international', de: 'Region2De', en: 'Region2En'});
@@ -193,6 +193,48 @@ describe('Suggestion of other users for trust circle', function () {
         res.body.users[2].profileUrl.should.equal('profileImage/5/thumbnail.jpg');
     });
 
+    it('When no person in trust circle then suggested user -> number of trust circle', async function () {
+        dbDsl.createRegion('international', {de: 'internationalDe', en: 'internationalEn'});
+        dbDsl.createRegion('region-1', {parentRegionId: 'international', de: 'Region1De', en: 'Region1En'});
+
+        dbDsl.createMainTopic({topicId: 'topic1', descriptionDe: 'topic1De', descriptionEn: 'topic1En'});
+        dbDsl.createMainTopic({topicId: 'topic2', descriptionDe: 'topic2De', descriptionEn: 'topic2En'});
+        dbDsl.createSubTopic({
+            parentTopicId: 'topic2', topicId: 'topic21', descriptionDe: 'topic21De', descriptionEn: 'topic21En'
+        });
+        dbDsl.createSubTopic({
+            parentTopicId: 'topic2', topicId: 'topic22', descriptionDe: 'topic22De', descriptionEn: 'topic22En'
+        });
+
+        dbDsl.createContactConnection('2', '5');
+        dbDsl.createContactConnection('3', '5');
+        dbDsl.createContactConnection('4', '5');
+
+        dbDsl.createContactConnection('8', '6');
+        dbDsl.createContactConnection('7', '6');
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/user/otherUser/suggestion', {skip: 0, limit: 10});
+        res.status.should.equal(200);
+
+        res.body.trustCircleSuggestion.should.equal(false);
+        res.body.hasMoreUsers.should.equal(false);
+        res.body.users.length.should.equal(2);
+
+        res.body.users[0].userId.should.equal('5');
+        res.body.users[0].name.should.equal('user Meier5');
+        res.body.users[0].numberOfIntersectingTrustCircle.should.equal(0);
+        res.body.users[0].slug.should.equal('user-meier5');
+        res.body.users[0].profileUrl.should.equal('profileImage/5/thumbnail.jpg');
+
+        res.body.users[1].userId.should.equal('6');
+        res.body.users[1].name.should.equal('user Meier6');
+        res.body.users[1].numberOfIntersectingTrustCircle.should.equal(0);
+        res.body.users[1].slug.should.equal('user-meier6');
+        res.body.users[1].profileUrl.should.equal('profileImage/6/thumbnail.jpg');
+    });
+
     it('Do not show user when privacy setting of user is set to onlyContact (trustCircleSuggestion = true)', async function () {
         dbDsl.createContactConnection('1', '2');
         dbDsl.createContactConnection('2', '5');
@@ -241,5 +283,21 @@ describe('Suggestion of other users for trust circle', function () {
         res.body.users.length.should.equal(1);
         res.body.users[0].userId.should.equal('5');
         res.body.users[0].profileUrl.should.equal('profileImage/5/thumbnail.jpg');
+    });
+
+    it('Do not recommend logged in user', async function () {
+        dbDsl.createContactConnection('1', '2');
+        dbDsl.createContactConnection('2', '1');
+        dbDsl.createContactConnection('2', '5');
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/user/otherUser/suggestion', {skip: 0, limit: 10});
+        res.status.should.equal(200);
+
+        res.body.trustCircleSuggestion.should.equal(true);
+        res.body.hasMoreUsers.should.equal(false);
+        res.body.users.length.should.equal(1);
+        res.body.users[0].userId.should.equal('5');
     });
 });
