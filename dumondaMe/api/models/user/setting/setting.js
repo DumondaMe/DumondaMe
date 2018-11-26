bf16485cce6a1130ae4d881c93364c65143f7140
Roger Waldvogel <info@dumonda.me>
2018-11-26 15:07:51 +0100

    Get languages and topics.

--- dumondaMe/api/models/user/setting/setting.js
+++ dumondaMe/api/models/user/setting/setting.js
@@ -1,11 +1,38 @@
 let db = requireDb();
 
+let sortTopics = function (topics, language) {
+    topics.sort((a, b) => {
+        if (a[language] < b[language]) {
+            return -1;
+        } else if (a[language] > b[language]) {
+            return 1;
+        }
+        return 0;
+    });
+};
+
+let getResponse = function (setting) {
+    sortTopics(setting.topics, setting.language);
+
+    setting.interestedTopics = [];
+    for (let topic of setting.topics) {
+        setting.interestedTopics.push({
+            topicId: topic.topicId,
+            description: topic[setting.language]
+        })
+    }
+    delete setting.topics;
+    return setting;
+};
+
 let getUserSetting = async function (userId) {
 
-    let resp = await  db.cypher().match(`(u:User {userId: {userId}})`)
-        .return(`u.privacyMode AS privacyMode, u.showProfileActivity AS showProfileActivity`)
+    let resp = await db.cypher().match(`(u:User {userId: {userId}})`)
+        .optionalMatch(`(u)-[:INTERESTED]->(topic:Topic)`)
+        .return(`u.privacyMode AS privacyMode, u.showProfileActivity AS showProfileActivity, u.languages AS languages,
+                 u.language AS language, collect(topic) AS topics`)
         .end({userId}).send();
-    return resp[0];
+    return getResponse(resp[0]);
 };
 
 module.exports = {
--- dumondaMe/api/test/test/e2e/tests/user/settings/getUserSettingSpec.js
+++ dumondaMe/api/test/test/e2e/tests/user/settings/getUserSettingSpec.js
@@ -8,6 +8,15 @@ describe('Getting user setting', function () {
 
     beforeEach(async function () {
         await dbDsl.init(8);
+
+        dbDsl.createMainTopic({topicId: 'topic1', descriptionDe: 'topic1De', descriptionEn: 'topic1En'});
+        dbDsl.createMainTopic({topicId: 'topic2', descriptionDe: 'topic2De', descriptionEn: 'topic2En'});
+        dbDsl.createSubTopic({
+            parentTopicId: 'topic2', topicId: 'topic21', descriptionDe: 'topic21De', descriptionEn: 'topic21En'
+        });
+        dbDsl.createSubTopic({
+            parentTopicId: 'topic2', topicId: 'topic22', descriptionDe: 'topic22De', descriptionEn: 'topic22En'
+        });
     });
 
     afterEach(function () {
@@ -16,11 +25,22 @@ describe('Getting user setting', function () {
 
     it('Get user settings when logged in', async function () {
         dbDsl.setUserProfileActivityPrivacy('1', {showProfileActivity: false});
+        dbDsl.interestedTopics('1', {topics: ['topic1', 'topic22']});
         await dbDsl.sendToDb();
         await requestHandler.login(users.validUser);
         let res = await requestHandler.get('/api/user/settings');
         res.status.should.equal(200);
         res.body.privacyMode.should.equal('public');
+
+        res.body.languages.length.should.equal(1);
+        res.body.languages[0].should.equal('de');
+
+        res.body.interestedTopics.length.should.equal(2);
+        res.body.interestedTopics[0].topicId.should.equal('topic1');
+        res.body.interestedTopics[0].description.should.equal('topic1De');
+        res.body.interestedTopics[1].topicId.should.equal('topic22');
+        res.body.interestedTopics[1].description.should.equal('topic22De');
+
         res.body.showProfileActivity.should.equal(false);
     });
 
