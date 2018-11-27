581b83fbc6f46ad1aba6f548ff2773827eb88255
Roger Waldvogel <info@dumonda.me>
2018-11-27 18:29:32 +0100

    Added explanation for question, commitment and trust circle to welcome screen.

---
+++ dumondaMe/components/info/welcomeDialog/Question.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-card id="welcome-question-container">
+        <div>
+            <slot name="header"></slot>
+        </div>
+        <v-card-text class="mobile-dialog-content">
+            <div id="image-question-container">
+                <img :src="imageUrl"/>
+            </div>
+            <div class="help-hint">{{$t("dialog:welcome.stepQuestion.hint")}}</div>
+            <div class="question-description" v-html="$t('dialog:welcome.stepQuestion.description')"></div>
+            <ul>
+                <li>{{$t("dialog:welcome.stepQuestion.link")}}</li>
+                <li>{{$t("dialog:welcome.stepQuestion.youtube")}}</li>
+                <li>{{$t("dialog:welcome.stepQuestion.books")}}</li>
+                <li>{{$t("dialog:welcome.stepQuestion.text")}}</li>
+                <li>{{$t("dialog:welcome.stepQuestion.commitment")}}</li>
+            </ul>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="$emit('close-dialog')">
+                {{$t("common:button.close")}}
+            </v-btn>
+            <v-btn color="primary" @click="$emit('next')">
+                {{$t("common:button.next")}}
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        computed: {
+            imageUrl() {
+                return `${process.env.staticUrl}/img/welcome/question.jpg`;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    #welcome-question-container {
+
+        .question-description {
+            margin-top: 18px;
+            font-weight: 300;
+            b {
+                font-weight: 500;
+                color: $primary-color;
+            }
+        }
+
+        ul {
+            margin-top: 12px;
+            li {
+                font-weight: 300;
+            }
+        }
+
+        .help-hint {
+            margin-bottom: 12px;
+            color: $secondary-color;
+            border-left: 2px solid $secondary-color;
+            padding-left: 12px;
+        }
+
+        #image-question-container {
+            width: 400px;
+            margin: 12px auto;
+            @media screen and (max-width: $xs) {
+                width: 300px;
+            }
+            @media screen and (max-width: 350px) {
+                width: 280px;
+            }
+            img {
+                width: 100%;
+                border-radius: 6px;
+            }
+        }
+    }
+</style>
---
+++ dumondaMe/components/info/welcomeDialog/TrustCircle.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-card id="welcome-trust-circle-container">
+        <div>
+            <slot name="header"></slot>
+        </div>
+        <v-card-text class="mobile-dialog-content">
+            <div id="image-trust-circle-container">
+                <img :src="imageUrl"/>
+            </div>
+            <div class="trust-circle-description" v-html="$t('dialog:welcome.stepTrustCircle.description')"></div>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="$emit('next')">
+                {{$t("common:button.finish")}}
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        computed: {
+            imageUrl() {
+                return `${process.env.staticUrl}/img/welcome/trustCircle.jpg`;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    #welcome-trust-circle-container {
+
+        .trust-circle-description {
+            margin-top: 18px;
+            font-weight: 300;
+            b {
+                font-weight: 500;
+                color: $primary-color;
+            }
+        }
+
+        .help-hint {
+            margin-top: 12px;
+            color: $secondary-color;
+            border-left: 2px solid $secondary-color;
+            padding-left: 12px;
+        }
+
+        #image-trust-circle-container {
+            width: 400px;
+            margin: 12px auto;
+            @media screen and (max-width: $xs) {
+                width: 300px;
+            }
+            @media screen and (max-width: 350px) {
+                width: 280px;
+            }
+            img {
+                width: 100%;
+                border-radius: 6px;
+            }
+        }
+    }
+</style>
