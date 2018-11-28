cace4b924b9fb57bf8c8ed6f3c5cedd2b50891f8
Roger Waldvogel <info@dumonda.me>
2018-11-28 11:30:06 +0100

    Save info state to session.

--- dumondaMe/api/models/user/setting/welcome.js
+++ dumondaMe/api/models/user/setting/welcome.js
@@ -2,10 +2,12 @@
 
 let db = requireDb();
 
-let welcomeFinish = async function (userId) {
+let welcomeFinish = async function (userId, req) {
     await db.cypher().match(`(user:User {userId: {userId}})`)
         .set('user', {infoState: 1})
         .end({userId}).send();
+    req.user.infoState = 1;
+    req.session.save();
 };
 
 module.exports = {
