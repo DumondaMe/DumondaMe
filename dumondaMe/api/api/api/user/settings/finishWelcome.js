30e9b2d296c8be8cafabf0af27987d72d19f8ac7
Roger Waldvogel <info@dumonda.me>
2018-11-28 11:30:06 +0100

    Save info state to session.

--- dumondaMe/api/api/api/user/settings/finishWelcome.js
+++ dumondaMe/api/api/api/user/settings/finishWelcome.js
@@ -6,7 +6,7 @@ const asyncMiddleware = require('dumonda-me-server-lib').asyncMiddleware;
 module.exports = function (router) {
 
     router.post('/', auth.isAuthenticated(), asyncMiddleware(async (req, res) => {
-        await welcome.welcomeFinish(req.user.id);
+        await welcome.welcomeFinish(req.user.id, req);
         res.status(200).end();
     }));
 };
