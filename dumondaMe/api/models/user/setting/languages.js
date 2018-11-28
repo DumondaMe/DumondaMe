374b81cc72e65369aae1bd3f3cca879743aac188
Roger Waldvogel <info@dumonda.me>
2018-11-28 16:15:21 +0100

    Save languages to session after save to database.

--- dumondaMe/api/models/user/setting/languages.js
+++ dumondaMe/api/models/user/setting/languages.js
@@ -2,10 +2,12 @@
 
 let db = requireDb();
 
-let changeLanguagesSettings = async function (userId, languages) {
+let changeLanguagesSettings = async function (userId, languages, req) {
     await db.cypher().match(`(user:User {userId: {userId}})`)
         .set('user', {languages})
         .end({userId}).send();
+    req.user.languages = languages;
+    req.session.save();
 };
 
 module.exports = {
