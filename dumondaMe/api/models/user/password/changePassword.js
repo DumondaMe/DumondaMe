12596bc9605900591eed743460aa6f9296542057
Roger Waldvogel <info@dumonda.me>
2018-11-29 11:38:20 +0100

    Changed to async/await style.

--- dumondaMe/api/models/user/password/changePassword.js
+++ dumondaMe/api/models/user/password/changePassword.js
@@ -5,33 +5,23 @@ let passwordEncryption = require('dumonda-me-server-lib').passwordEncryption;
 let exceptions = require('dumonda-me-server-lib').exceptions;
 let logger = require('dumonda-me-server-lib').logging.getLogger(__filename);
 
-let checkActualPassword = function (userId, actualPassword, req) {
-
-    return db.cypher().match('(u:User {userId: {userId}})')
+let checkActualPassword = async function (userId, actualPassword, req) {
+    let user = await db.cypher().match('(u:User {userId: {userId}})')
         .return('u.password AS password')
-        .end({userId: userId})
-        .send()
-        .then(function (user) {
-            return passwordEncryption.comparePassword(actualPassword, user[0].password);
-        })
-        .then(function (samePassword) {
-            if (!samePassword) {
-                return exceptions.getInvalidOperation('Wrong actual password for changing password', logger, req);
-            }
-        });
+        .end({userId: userId}).send();
+
+    let samePassword = await passwordEncryption.comparePassword(actualPassword, user[0].password);
+    if (!samePassword) {
+        return exceptions.getInvalidOperation('Wrong actual password for changing password', logger, req);
+    }
 };
 
-let changePassword = function (userId, newPassword, actualPassword, req) {
-    return checkActualPassword(userId, actualPassword, req)
-        .then(function () {
-            return passwordEncryption.generatePasswordHash(newPassword);
-        })
-        .then(function (hash) {
-            return db.cypher().match('(u:User {userId: {userId}})')
-                .set('u', {password: hash})
-                .end({userId: userId})
-                .send();
-        });
+let changePassword = async function (userId, newPassword, actualPassword, req) {
+    await checkActualPassword(userId, actualPassword, req);
+    let hash = await passwordEncryption.generatePasswordHash(newPassword);
+    return db.cypher().match('(u:User {userId: {userId}})')
+        .set('u', {password: hash})
+        .end({userId: userId}).send();
 };
 
 module.exports = {
