796af5904157f0213b93cd264d6bbf14c82dc3f1
Roger Waldvogel <info@dumonda.me>
2018-11-29 16:26:37 +0100

    Improved query search quality.

--- dumondaMe/api/models/search/autocomplete.js
+++ dumondaMe/api/models/search/autocomplete.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const db = requireDb();
+const queryParser = require('./queryParser');
 
 const search = async function (query, userId) {
-    query = `${query}~`;
-    let result = await db.cypher().call(`apoc.index.search("entities", {query}) YIELD node AS result`)
+    query = `("${query.trim()}"~20)^20 ${queryParser.wordsQuery(query, '')}`;
+    let result = await db.cypher().call(`apoc.index.search("entities", {query}) YIELD node AS result, weight`)
         .where(`{userId} IS NOT NULL OR result:Commitment OR result:Question OR 
                 ({userId} IS NULL AND result:User AND result.privacyMode = 'public')`)
         .unwind(`[result.title, result.name, result.question] AS property`)
-        .with(`property`)
+        .with(`property, weight`)
         .where(`property IS NOT NULL`)
-        .return(`DISTINCT property`)
-        .limit(`7`).end({query, userId}).send();
+        .return(`DISTINCT property, max(weight) AS maxWeight`)
+        .orderBy(`maxWeight DESC`).limit(`7`).end({query, userId}).send();
     return result.map(x => x.property)
 };
 
--- dumondaMe/api/test/test/e2e/tests/search/searchQuestionPrivacySpec.js
+++ dumondaMe/api/test/test/e2e/tests/search/searchQuestionPrivacySpec.js
@@ -9,12 +9,18 @@ describe('Privacy setting for creator of searched question', function () {
 
     beforeEach(async function () {
         await dbDsl.init(2);
+        dbDsl.createRegion('region-1', {de: 'Region1De', en: 'Region1En'});
         dbDsl.createMainTopic({topicId: 'topic1', descriptionDe: 'topic1De', descriptionEn: 'topic1En'});
 
         dbDsl.createQuestion('10', {
             creatorId: '2', question: 'Das ist eine Frage', created: 500, modified: 700,
             description: 'Test dumonda.me change the world1', topics: ['topic1'], language: 'de'
         });
+        dbDsl.createCommitment('1', {
+            title: 'Das ist eine Frage', regions: ['region-1'],
+            adminId: '2', topics: ['topic1'], language: 'de', created: 700, modified: 777,
+            website: 'https://www.example.org/'
+        }, []);
     });
 
     afterEach(function () {
@@ -23,11 +29,15 @@ describe('Privacy setting for creator of searched question', function () {
 
     it('Hide user when privacy setting to publicEl and not logged in', async function () {
         dbDsl.setUserPrivacy('2', {privacyMode: 'publicEl'});
+        dbDsl.createQuestion('11', {
+            creatorId: '2', question: 'Frage ein ist Das', created: 500, modified: 700,
+            description: 'Test dumonda.me change the world1', topics: ['topic1'], language: 'de'
+        });
         await dbDsl.sendToDb();
         await dbDsl.setApocIndex();
-        let res = await requestHandler.get('/api/search', {query: 'Das ist eine Frage', lang: 'de'});
+        let res = await requestHandler.get('/api/search', {query: 'Das ist eine Frag ', lang: 'de'});
         res.status.should.equal(200);
-        res.body.questions.length.should.equals(1);
+        res.body.questions.length.should.equals(2);
         res.body.questions[0].questionId.should.equals('10');
         should.not.exist(res.body.questions[0].user.userId);
         should.not.exist(res.body.questions[0].user.name);
@@ -35,6 +45,8 @@ describe('Privacy setting for creator of searched question', function () {
         res.body.questions[0].user.userImage.should.equals('profileImage/default/thumbnail.jpg');
         res.body.questions[0].user.userImagePreview.should.equals('profileImage/default/profilePreview.jpg');
         res.body.questions[0].user.isAnonymous.should.equals(true);
+
+        res.body.questions[1].questionId.should.equals('11');
     });
 
     it('Hide user when privacy setting to onlyContact and not logged in', async function () {
