03a00bf6e437de70b8c4c97feed89f5f0236190b
Roger Waldvogel <info@dumonda.me>
2018-11-30 10:58:22 +0100

    Show existing question when user creates new question.

--- dumondaMe/components/question/dialog/Question.vue
+++ dumondaMe/components/question/dialog/Question.vue
@@ -7,12 +7,31 @@
             <v-form v-model="valid">
                 <v-layout row wrap>
                     <v-flex xs12>
+                        <v-select v-model="question.lang" id="select-language"
+                                  :label="$t('pages:feeds.selectLanguage')"
+                                  :rules="[ruleSelectRequired($t('validation:fieldRequired'))]"
+                                  :items="getLanguages()"
+                                  single-line required>
+                        </v-select>
+                    </v-flex>
+                    <v-flex xs12>
                         <v-text-field type="text" v-model="question.question" name="question"
                                       :label="$t('common:question')"
                                       :rules="[ruleFieldRequired($t('validation:fieldRequired')),
                                                ruleToManyChars($t('validation:toManyChars'), 140)]"
-                                      :counter="140">
+                                      :counter="140" :loading="loadingExistingQuestions">
                         </v-text-field>
+                        <div class="existing-questions-container">
+                            <div class="existing-question" v-for="existingQuestion in existingQuestions">
+                                <div class="question" @click="goToQuestion(existingQuestion)">
+                                    {{existingQuestion.question}}
+                                </div>
+                                <div class="number-of-answers">
+                                    {{$t('pages:question.createDialog.numberOfAnswers',
+                                    {count: existingQuestion.numberOfAnswers})}}
+                                </div>
+                            </div>
+                        </div>
                     </v-flex>
                     <v-flex xs12>
                         <v-textarea type="text" v-model="question.description" name="description" auto-grow
@@ -21,14 +40,6 @@
                                     :rules="[ruleToManyChars($t('validation:toManyChars'), 2000)]"
                                     :counter="2000">
                         </v-textarea>
-                    </v-flex>
-                    <v-flex xs12>
-                        <v-select v-model="question.lang" id="select-language"
-                                  :label="$t('pages:feeds.selectLanguage')"
-                                  :rules="[ruleSelectRequired($t('validation:fieldRequired'))]"
-                                  :items="getLanguages()"
-                                  single-line required>
-                        </v-select>
                     </v-flex>
                 </v-layout>
             </v-form>
@@ -39,11 +50,11 @@
         <v-divider></v-divider>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" flat :disabled="loading"
-                   @click.native="$emit('close-dialog')">{{$t("common:button.close")}}
+            <v-btn color="primary" flat
+                   @click.native="$emit('close-dialog')" :disabled="loading">{{$t("common:button.close")}}
             </v-btn>
-            <v-btn color="primary" @click.native="finish" :loading="loading"
-                   :disabled="!valid || loading || !hasChanged">
+            <v-btn color="primary" @click.native="finish"
+                   :disabled="!valid || loading || !hasChanged || loadingExistingQuestions">
                 {{actionButtonText}}
             </v-btn>
         </v-card-actions>
@@ -53,16 +64,21 @@
 <script>
     import validationRules from '~/mixins/validationRules.js';
     import languages from '~/mixins/languages.js';
+    import debounce from 'debounce';
 
     export default {
         props: ['initQuestion', 'actionButtonText', 'loading', 'isModifyMode'],
         data: function () {
             return {
                 question: JSON.parse(JSON.stringify(this.initQuestion)),
-                questionCompare: JSON.parse(JSON.stringify(this.initQuestion)), valid: false
+                questionCompare: JSON.parse(JSON.stringify(this.initQuestion)), valid: false,
+                loadingExistingQuestions: false, existingQuestions: []
             };
         },
         computed: {
+            questionModel() {
+                return this.question.question;
+            },
             hasChanged() {
                 if (this.isModifyMode) {
                     return (this.question.question !== this.questionCompare.question ||
@@ -86,7 +102,28 @@
                     params.questionId = this.question.questionId;
                 }
                 this.$emit('finish', params);
+            },
+            goToQuestion(question) {
+                this.$emit('close-dialog');
+                this.$router.push({
+                    name: 'question-questionId-slug',
+                    params: {questionId: question.questionId, slug: question.slug}
+                });
             }
+        },
+        watch: {
+            questionModel: debounce(async function (newQuestion) {
+                if (typeof newQuestion === 'string' && newQuestion.trim().length > 10 && !this.isModifyMode) {
+                    try {
+                        this.loadingExistingQuestions = true;
+                        let response = await this.$axios.$get('search/questions',
+                            {params: {query: newQuestion, lang: this.question.lang, skip: 0, limit: 15}});
+                        this.existingQuestions = response.questions;
+                    } finally {
+                        this.loadingExistingQuestions = false;
+                    }
+                }
+            }, 500)
         }
     }
 </script>
@@ -99,9 +136,32 @@
             color: $secondary-text;
             margin-top: 12px;
         }
+
+        .existing-questions-container {
+            .existing-question {
+                margin-bottom: 8px;
+
+                .question {
+                    cursor: pointer;
+                    font-size: 16px;
+                    font-weight: 500;
+                }
+
+                :hover.question {
+                    text-decoration: underline;
+                }
+
+                .number-of-answers {
+                    font-size: 14px;
+                    color: $secondary-text;
+                }
+            }
+        }
+
         :hover.asking-question-help-link {
             text-decoration: underline;
         }
+
         #select-language {
             label {
                 display: inline-block;
