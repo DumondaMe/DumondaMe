d5fd4df7a525b0bcfe1fd4a5bce691eaba398fc5
Roger Waldvogel <info@dumonda.me>
2019-01-03 08:26:11 +0100

    Using new controller style.

--- dumondaMe/api/api/api/import/contact/gmail/index.js
+++ dumondaMe/api/api/api/import/contact/gmail/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-let validation = require('dumonda-me-server-lib').jsonValidation;
-let auth = require('dumonda-me-server-lib').auth;
-let importGmail = requireModel('import/gmail/gmail');
-let controllerErrors = require('dumonda-me-server-lib').controllerErrors;
-let logger = require('dumonda-me-server-lib').logging.getLogger(__filename);
+const validation = require('dumonda-me-server-lib').jsonValidation;
+const auth = require('dumonda-me-server-lib').auth;
+const importGmail = requireModel('import/gmail/gmail');
+const asyncMiddleware = require('dumonda-me-server-lib').asyncMiddleware;
 
-let schemaImportGmailContacts = {
+const schemaImportGmailContacts = {
     name: 'getGmailContacts',
     type: 'object',
     additionalProperties: false,
@@ -18,15 +17,9 @@ let schemaImportGmailContacts = {
 
 module.exports = function (router) {
 
-    router.get('/', auth.isAuthenticated(), function (req, res) {
-
-        return controllerErrors('Error occurs when importing gmail contacts', req, res, logger, function () {
-            return validation.validateQueryRequest(req, schemaImportGmailContacts, logger).then(function (request) {
-                logger.info('Request import of gmail contacts', req);
-                return importGmail.import(req.user.id, request, req);
-            }).then(function (page) {
-                res.status(200).json(page);
-            });
-        });
-    });
+    router.get('/', auth.isAuthenticated(), asyncMiddleware(async (req, res) => {
+        let request = await validation.validateRequest(req, schemaImportGmailContacts);
+        let page = await importGmail.import(req.user.id, request, req);
+        res.status(200).json(page);
+    }));
 };
