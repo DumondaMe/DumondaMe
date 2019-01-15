32f32f946b0e9676615a7fd8aeeb356f8452b3dd
Roger Waldvogel <info@dumonda.me>
2019-01-15 10:37:34 +0100

    Unsubscribe for invited user and answer question messages.

---
+++ dumondaMe/pages/unsubscribe/invitedUser/_email/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="unsubscribe-invited-user">
+        <unsubscribe-box :unsubscribe-description="$t('pages:unsubscribe.invitedUser.description',
+        {email: $route.params.email})">
+        </unsubscribe-box>
+    </div>
+</template>
+
+<script>
+    import UnsubscribeBox from '~/components/unsubscribe/UnsubscribeBox';
+
+    export default {
+        async fetch({app, error, params}) {
+            try {
+                await app.$axios.$post(`unsubscribe/invitedUser`, {email: params.email});
+            } catch (e) {
+                error({statusCode: e.statusCode});
+            }
+        },
+        components: {UnsubscribeBox}
+    }
+</script>
+
+<style lang="scss">
+    #unsubscribe-invited-user {
+        margin-top: 30px;
+    }
+</style>
