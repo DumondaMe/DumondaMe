faa58576d61b03397ad5aa9cbb996139451119e8
Roger Waldvogel <info@dumonda.me>
2019-01-15 14:58:42 +0100

    Setting email notification settings implemented.

---
+++ dumondaMe/api/api/api/user/settings/emailNotifications.js
@@ -0,0 +1,28 @@
+'use strict';
+const auth = require('dumonda-me-server-lib').auth;
+const logger = require('dumonda-me-server-lib').logging.getLogger(__filename);
+const emailNotifications = requireModel('user/setting/emailNotifications');
+const validation = require('dumonda-me-server-lib').jsonValidation;
+const asyncMiddleware = require('dumonda-me-server-lib').asyncMiddleware;
+
+const schemaEmailNotificationSettings = {
+    name: 'changeEmailNotificationsSettings',
+    type: 'object',
+    additionalProperties: false,
+    required: ['enableEmailNotifications', 'enableInviteToAnswerQuestion'],
+    properties: {
+        enableEmailNotifications: {type: 'boolean'},
+        enableInviteToAnswerQuestion: {type: 'boolean'}
+    }
+};
+
+module.exports = function (router) {
+
+    router.put('/', auth.isAuthenticated(), asyncMiddleware(async (req, res) => {
+        let request = await validation.validateRequest(req, schemaEmailNotificationSettings);
+        logger.info(`User ${req.user.id} changes email notification settings`, req);
+        await emailNotifications.changeSettings(req.user.id, request.enableEmailNotifications,
+            request.enableInviteToAnswerQuestion);
+        res.status(200).end();
+    }));
+};
