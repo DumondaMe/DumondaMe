28c3e8e5e45a274d59561d734b714e9de98e9bb2
Roger Waldvogel <info@dumonda.me>
2019-01-16 13:46:32 +0100

    Delete EmailSent label when email sending failed.

--- eMailService/test/notifications/addedToTrustCircle/sendUreadNotificationsSpec.js
+++ eMailService/test/notifications/addedToTrustCircle/sendUreadNotificationsSpec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const dbDsl = require('dumonda-me-server-test-util').dbDSL;
+const db = require('dumonda-me-server-test-util').db;
 const eMail = require('dumonda-me-server-lib').eMail;
 const sinon = require('sinon');
 const testee = requireSrc('notifications');
@@ -34,6 +35,31 @@ describe('Send unread notification when user has been added to trust circle', fu
             numberOfNotifications: 1,
             unsubscribeLink: `${process.env.DUMONDA_ME_DOMAIN}unsubscribe/notifications/user2@irgendwo.ch`
         }, 'de', 'user2@irgendwo.ch').should.be.true;
+
+        let notification = await db.cypher().match("(notification:Notification:EmailSent {notificationId: '50'})")
+            .return(`notification`).end().send();
+        notification.length.should.equals(1);
+    });
+
+    it('If an error occurred then remove EmailSent label ', async function () {
+        dbDsl.notificationUserAddedToTrustCircle('50', {
+            userId: '2',
+            trustCircleUsers: [{userId: '3', created: 555}, {userId: '4', created: 444}], created: 600
+        });
+        await dbDsl.sendToDb();
+        stubSendEMail.rejects({});
+
+        await testee.sendUnreadNotifications();
+
+        stubSendEMail.calledWith("notification", {
+            numberOfNotifications: 1,
+            unsubscribeLink: `${process.env.DUMONDA_ME_DOMAIN}unsubscribe/notifications/user2@irgendwo.ch`
+        }, 'de', 'user2@irgendwo.ch').should.be.true;
+
+        let notification = await db.cypher().match("(notification:Notification {notificationId: '50'})")
+            .where(`NOT notification:EmailSent`)
+            .return(`notification`).end().send();
+        notification.length.should.equals(1);
     });
 
     it('A notification that has already been sent should not be sent again.', async function () {
