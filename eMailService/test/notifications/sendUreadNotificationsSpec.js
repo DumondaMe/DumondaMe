2e82ce0353ebabbc88e573b31915c0c3ac211777
Roger Waldvogel <info@dumonda.me>
2019-01-16 14:07:42 +0100

    Test sending multiple unread notifications.

---
+++ eMailService/test/notifications/sendUreadNotificationsSpec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const dbDsl = require('dumonda-me-server-test-util').dbDSL;
+const db = require('dumonda-me-server-test-util').db;
+const eMail = require('dumonda-me-server-lib').eMail;
+const sinon = require('sinon');
+const testee = requireSrc('notifications');
+
+describe('Send email when multiple unread notification exist', function () {
+
+    let stubSendEMail, sandbox;
+
+    beforeEach(async function () {
+        await dbDsl.init(6);
+        sandbox = sinon.sandbox.create();
+        stubSendEMail = sandbox.stub(eMail, 'sendEMail');
+        stubSendEMail.resolves({});
+
+        dbDsl.createQuestion('1', {
+            creatorId: '2', question: 'Das ist eine FragöÖÄäÜü', description: 'description', topics: ['Spiritual'],
+            language: 'de'
+        });
+        dbDsl.createTextAnswer('5', {
+            creatorId: '2', questionId:'1', answer: 'Answer'
+        });
+        dbDsl.createNote('50', {answerId: '5', creatorId: '2', created: 555});
+
+        dbDsl.notificationCreateNote('100', {questionId: '1', answerId: '5', noteId: '50', created: 678});
+        dbDsl.notificationUserAddedToTrustCircle('101', {
+            userId: '2',
+            trustCircleUsers: [{userId: '3', created: 555}, {userId: '4', created: 444}], created: 600
+        });
+        dbDsl.notificationCreateAnswer('102', {questionId: '1', answerId: '5', created: 678});
+
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('Send unread notification', async function () {
+        await dbDsl.sendToDb();
+
+        await testee.sendUnreadNotifications();
+
+        stubSendEMail.calledWith("notification", {
+            numberOfNotifications: 3,
+            unsubscribeLink: `${process.env.DUMONDA_ME_DOMAIN}unsubscribe/notifications/user2@irgendwo.ch`
+        }, 'de', 'user2@irgendwo.ch').should.be.true;
+
+        let notification = await db.cypher().match("(notification:Notification:EmailSent)")
+            .return(`notification`).end().send();
+        notification.length.should.equals(3);
+    });
+});
