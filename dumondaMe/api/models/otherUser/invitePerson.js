a9ad65d68bee40801a52e40bc6262001f0478161
Roger Waldvogel <info@dumonda.me>
2019-02-06 14:01:10 +0100

    Setting relationship to send invitations to friends.

--- dumondaMe/api/models/otherUser/invitePerson.js
+++ dumondaMe/api/models/otherUser/invitePerson.js
@@ -2,103 +2,33 @@
 
 let db = requireDb();
 let _ = require('lodash');
-let eMailQueue = require('dumonda-me-server-lib').eMailQueue;
 
-let getNormalizedEmails = function (emails) {
-    let normalizedEmails = [];
-    emails.forEach(function (email) {
-        normalizedEmails.push(email.toLowerCase());
-    });
-    return normalizedEmails;
+const inviteUser = async function (emails, userId) {
+    await db.cypher().unwind(`{emails} AS email`)
+        .merge(`(invitedUser:InvitedUser {emailNormalized: email})`)
+        .onCreate(`SET invitedUser.email = email, invitedUser:EMailNotificationEnabled`)
+        .with(`invitedUser`)
+        .where(`invitedUser:EMailNotificationEnabled`)
+        .match(`(user:User {userId: {userId}})`)
+        .where(`NOT (user)-[:HAS_INVITED]->(invitedUser)`)
+        .merge(`(user)-[:HAS_INVITED]->(invitedUser)`)
+        .merge(`(user)-[:SENDING_EMAIL_PENDING]->(invitedUser)`)
+        .end({emails, userId}).send();
 };
 
-let getEmails = function (eMailsNormalized, eMails) {
-    let result = [];
-    eMailsNormalized.forEach(function (eMailNormalized) {
-        let eMail = _.find(eMails, function (eMail) {
-            return eMail.toLowerCase() === eMailNormalized;
-        });
-        result.push({email: eMail, emailNormalized: eMailNormalized});
-    });
-    return result;
+const getNotExistingUsers = async function (emails) {
+    let existingUsers = await db.cypher().match(`(user:User)`)
+        .where(`user.emailNormalized IN {emails}`)
+        .return(`user.emailNormalized AS emailNormalized`)
+        .end({emails}).send();
+    return _.difference(emails, existingUsers.map((user) => user.emailNormalized));
 };
 
-let getEmailsInvitedUser = function (userId, emails) {
-    let normalizedEmails = getNormalizedEmails(emails);
-    return db.cypher().match("(sender:User {userId: {userId}})")
-        .optionalMatch("(user:User)")
-        .where("user.emailNormalized IN {emails}")
-        .return("collect(user.email) AS emails, sender")
-        .union()
-        .match("(sender:User {userId: {userId}})")
-        .optionalMatch("(user:UnsubscribeInvitation)")
-        .where("user.email IN {emails}")
-        .return("collect(user.email) AS emails, sender")
-        .union()
-        .match("(sender:User {userId: {userId}})-[:HAS_INVITED]->(user:InvitedUser)")
-        .where("user.emailNormalized IN {emails}")
-        .return("collect(user.emailNormalized) AS emails, sender")
-        .end({emails: normalizedEmails, userId: userId}).send()
-        .then(function (resp) {
-            if (resp.length > 0) {
-                let sender = resp[0].sender, emailsToSubtract = resp[0].emails;
-                if (resp.length > 1) {
-                    emailsToSubtract = emailsToSubtract.concat(resp[1].emails);
-                    if (resp.length > 2) {
-                        emailsToSubtract = emailsToSubtract.concat(resp[2].emails);
-                    }
-                }
-                return {
-                    name: sender.name,
-                    emails: getEmails(_.difference(normalizedEmails, emailsToSubtract), emails)
-                };
-            }
-            return {emails: []};
-        });
-};
-
-let getUserIdsInvitedUserWithAccount = function (userId, emails) {
-    return db.cypher().match("(sender:User {userId: {userId}}), (user:User)")
-        .where("user.emailNormalized IN {emails} AND NOT (sender)-[:IS_CONTACT]->(user)")
-        .return("collect(user.userId) AS userIds").end({emails: getNormalizedEmails(emails), userId: userId}).send()
-        .then(function (resp) {
-            if (resp.length === 1) {
-                return {userIds: resp[0].userIds};
-            }
-            return {userIds: []};
-        });
-};
-
-let addInviteUser = function (userId, emails, message) {
-    return db.cypher().match("(sender:User {userId: {userId}})")
-        .foreach(`(invitedUser IN {emails} | 
-        CREATE UNIQUE (sender)-[:HAS_INVITED]->
-        (:InvitedUser {email: invitedUser.email, emailNormalized: invitedUser.emailNormalized, message: {message}}))`, true)
-        .end({emails: emails, userId: userId, message: message}).send();
-};
-
-let addInviteToExistingUser = function (userId, userIds) {
-    return db.cypher().match("(sender:User {userId: {userId}}), (existingUser:User)")
-        .where("existingUser.userId IN {userIds}")
-        .createUnique(`(sender)-[:HAS_INVITED]->(existingUser)`)
-        .end({userIds: userIds, userId: userId}).send();
-};
-
-let sendInvitation = function (userId, emails, message) {
-    message = message || null;
-    return getEmailsInvitedUser(userId, emails).then(function (data) {
-        if (data.emails.length > 0) {
-            return addInviteUser(userId, data.emails, message).then(function () {
-                eMailQueue.createImmediatelyJob('sendInviteEmail', {userId: userId, name: data.name});
-            });
-        }
-    }).then(function () {
-        return getUserIdsInvitedUserWithAccount(userId, emails);
-    }).then(function (data) {
-        return addInviteToExistingUser(userId, data.userIds);
-    });
+const setInvitationFlag = async function (userId, emails) {
+    let usersToInvite = await getNotExistingUsers(emails);
+    await inviteUser(usersToInvite, userId);
 };
 
 module.exports = {
-    sendInvitation
+    setInvitationFlag
 };
