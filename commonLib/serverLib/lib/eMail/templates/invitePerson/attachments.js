dc3faae27ea3368baccc8831e82094dd048e565e
Roger Waldvogel <info@dumonda.me>
2019-02-07 09:55:58 +0100

    Added images for logo and video. Using same text as for invitation to answer question.

--- commonLib/serverLib/lib/eMail/templates/invitePerson/attachments.js
+++ commonLib/serverLib/lib/eMail/templates/invitePerson/attachments.js
@@ -8,6 +8,14 @@ let getAttachments = function (templateData) {
             filename: path.basename(templateData.userImage.name),
             path: templateData.userImage.name,
             cid: 'url'
+        }, {
+            filename: 'urlVideoPreview.png',
+            path: path.resolve(__dirname, 'img/urlVideoPreview.png'),
+            cid: 'urlVideoPreview'
+        }, {
+            filename: 'DumondaMeLogo.png',
+            path: path.resolve(__dirname, 'img/DumondaMeLogo.png'),
+            cid: 'urlDumondaMeLogo'
         }],
         tempFiles: [templateData.userImage]
     };
