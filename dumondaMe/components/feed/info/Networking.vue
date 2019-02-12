c120250dea6f6fbe46fc536b7d01cf1c79c2bf07
Roger Waldvogel <info@dumonda.me>
2019-02-12 14:35:19 +0100

    Added networking card to the landing page.

---
+++ dumondaMe/components/feed/info/Networking.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="feed-networking-container ely-card">
+        <h3 class="feed-desktop-sidebar-title">{{$t('pages:feeds.networking.title')}}</h3>
+        <img :src="networkingImage" :srcset="networking2xImage" class="networking-image">
+        <div class="feed-networking-description">{{$t('pages:feeds.networking.description')}}</div>
+        <v-btn outline color="primary" @click="showImportContactDialog = true">
+            {{$t('pages:feeds.networking.actionButton')}}
+        </v-btn>
+        <import-contact-dialog v-if="showImportContactDialog" @close-dialog="showImportContactDialog = false">
+        </import-contact-dialog>
+    </div>
+</template>
+
+<script>
+    import ImportContactDialog from '~/components/import/ImportContactDialog';
+
+    export default {
+        components: {ImportContactDialog},
+        data() {
+            return {showImportContactDialog: false}
+        },
+        computed: {
+            networkingImage() {
+                return `${process.env.staticUrl}/img/landingPage/networkingImage.jpg`;
+            },
+            networking2xImage() {
+                return `${process.env.staticUrl}/img/landingPage/networkingImage_2x.jpg 2x`;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .feed-networking-container {
+        margin-bottom: 18px;
+        font-size: 14px;
+        font-weight: 300;
+
+        h3.feed-desktop-sidebar-title {
+            margin-bottom: 12px;
+        }
+
+        .networking-image {
+            width: 100%;
+            border-radius: 4px;
+            margin-bottom: 8px;
+        }
+
+        .feed-networking-description {
+            margin-bottom: 8px;
+        }
+
+        button {
+            margin-left: 0;
+        }
+
+    }
+</style>
