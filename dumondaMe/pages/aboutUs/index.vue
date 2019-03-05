eb7c5907113410937394b2e15400457298acb771
Roger Waldvogel <info@dumonda.me>
2019-03-05 07:53:21 +0100

    Added nils solanki to about us text.

--- dumondaMe/pages/aboutUs/index.vue
+++ dumondaMe/pages/aboutUs/index.vue
@@ -75,6 +75,24 @@
                     </p>
                 </div>
             </div>
+            <div class="team-sub-title">Holistische Erlebnisgestaltung mit
+                <a target="_blank" href="https://solanki.ch/">Solanki</a></div>
+            <div class="about-user">
+                <div>
+                    <img class="profile-image" :src="imageNils"/>
+                </div>
+                <div>
+                    <h3 class="name">Nils Solanki</h3>
+                    <div class="ely-function"></div>
+                    <div class="email-address without-function"><a
+                            href="mailto:jeannine@momoundronja.ch">services@solanki.ch</a></div>
+                    <p class="about-text">Auf DumondaMe vernetzen sich Menschen, über alle Gesellschaftsbereiche
+                        hinweg, auf der Suche nach schöpferischen Antworten auf die spannendsten und drängendsten Fragen
+                        unserer Zeit. Mit Solanki unterstütze ich das Team darin, die Vision gemeinsam zu entfalten und
+                        aus den Herzen und Köpfen heraus ein freudvolles (digitales) Erlebnis zu gestalten.
+                    </p>
+                </div>
+            </div>
             <div class="team-sub-title">Ehrenamtliche Helfer</div>
             <p>Des Weiteren werden wir von vielen Menschen auf ehrenamtlicher Basis unterstützt. An dieser Stelle vielen
                 herzlichen Dank für euer freiwilliges Wirken, ohne welches dieses Projekt nicht realisierbar wäre.
@@ -153,6 +171,24 @@
                     </p>
                 </div>
             </div>
+            <div class="team-sub-title">Holistic experience design with
+                <a target="_blank" href="https://solanki.ch/">Solanki</a></div>
+            <div class="about-user">
+                <div>
+                    <img class="profile-image" :src="imageNils"/>
+                </div>
+                <div>
+                    <h3 class="name">Nils Solanki</h3>
+                    <div class="ely-function"></div>
+                    <div class="email-address without-function"><a
+                            href="mailto:jeannine@momoundronja.ch">services@solanki.ch</a></div>
+                    <p class="about-text">On DumondaMe people network across all areas of society in search of creative
+                        answers to the most exciting and urgent questions of our time. With Solanki, I support the team
+                        in unfolding the vision together and creating a joyful (digital) experience out of the hearts
+                        and minds.
+                    </p>
+                </div>
+            </div>
             <div class="team-sub-title">Volunteers</div>
             <p>Furthermore, we are supported by many people on an voluntary basis. Thank you very much for your work,
                 without which this project would not be possible.
@@ -185,6 +221,9 @@
             },
             imageJeannine() {
                 return `${process.env.staticUrl}/img/about/jeannine_brutschin.jpg`
+            },
+            imageNils() {
+                return `${process.env.staticUrl}/img/about/nils_solanki.jpg`
             }
         }
     }
