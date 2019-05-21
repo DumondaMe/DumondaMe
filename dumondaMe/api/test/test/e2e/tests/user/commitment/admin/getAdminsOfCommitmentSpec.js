f64b72b6e5f2d848e797628e7302536ec3e793d7
roger <info@dumonda.me>
2019-05-21 15:43:01 +0200

    Getting users request to be admin of commitment.

--- dumondaMe/api/test/test/e2e/tests/user/commitment/admin/getAdminsOfCommitmentSpec.js
+++ dumondaMe/api/test/test/e2e/tests/user/commitment/admin/getAdminsOfCommitmentSpec.js
@@ -30,6 +30,11 @@ describe('Getting all admins of an commitment', function () {
         dbDsl.addAdminToCommitment({commitmentId: '1', userId: '3'});
         dbDsl.addAdminToCommitment({commitmentId: '1', userId: '4'});
 
+        dbDsl.notificationRequestAdminOfCommitment('11',
+            {commitmentId: '1', existingAdminId: '1', newAdminId: '3', created: 500, read: true});
+        dbDsl.notificationRequestAdminOfCommitment('11',
+            {commitmentId: '1', existingAdminId: '1', newAdminId: '3', created: 500});
+
         await dbDsl.sendToDb();
         await requestHandler.login(users.validUser);
         let res = await requestHandler.get('/api/user/commitment/admin', {commitmentId: '1'});
@@ -53,6 +58,45 @@ describe('Getting all admins of an commitment', function () {
         res.body.admin[2].slug.should.equals('user-meier4');
         res.body.admin[2].profileUrl.should.equal("profileImage/4/thumbnail.jpg");
         res.body.admin[2].isLoggedInUser.should.equal(false);
+
+        res.body.adminRequested.length.should.equals(0);
+    });
+
+    it('Show user requested to be admin of commitment', async function () {
+
+        dbDsl.notificationRequestAdminOfCommitment('11',
+            {commitmentId: '1', existingAdminId: '1', newAdminId: '3', created: 500});
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/user/commitment/admin', {commitmentId: '1'});
+        res.status.should.equal(200);
+
+        res.body.admin.length.should.equals(1);
+        res.body.admin[0].userId.should.equals('1');
+
+        res.body.adminRequested.length.should.equals(1);
+        res.body.adminRequested[0].userId.should.equals('3');
+        res.body.adminRequested[0].name.should.equals('user Meier3');
+        res.body.adminRequested[0].slug.should.equals('user-meier3');
+        res.body.adminRequested[0].profileUrl.should.equal("profileImage/3/thumbnail.jpg");
+        res.body.adminRequested[0].isLoggedInUser.should.equal(false);
+    });
+
+    it('Hide user requested to be admin of commitment when notification is read', async function () {
+
+        dbDsl.notificationRequestAdminOfCommitment('11',
+            {commitmentId: '1', existingAdminId: '1', newAdminId: '3', created: 500, read: true});
+
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.get('/api/user/commitment/admin', {commitmentId: '1'});
+        res.status.should.equal(200);
+
+        res.body.admin.length.should.equals(1);
+        res.body.admin[0].userId.should.equals('1');
+
+        res.body.adminRequested.length.should.equals(0);
     });
 
     it('Show only admins if user is admin of commitment', async function () {
