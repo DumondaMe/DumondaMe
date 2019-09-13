3381b271ffa2002329685589d41dd4fb04010b3d
roger <info@dumonda.me>
2019-09-13 17:18:49 +0200

    Added clap icon example for custon icons.

---
+++ dumondaMe/components/icons/Clap.vue
@@ -0,0 +1,58 @@
+<template>
+    <svg class="dumonda-me-clap-icon" id="Layer_1" style="enable-background:new 0 0 1000 1000;" version="1.1"
+         viewBox="0 0 1000 1000" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+         xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">
+            .st0 {
+                fill: #191919;
+            }
+
+            .st1 {
+                fill: #EDEDED;
+            }
+
+            .st2 {
+                opacity: 0.2;
+                fill: #FFFFFF;
+            }
+
+            .st3 {
+                fill: url(#XMLID_6_);
+            }
+
+            .st4 {
+                fill: url(#XMLID_7_);
+            }
+        </style>
+        <polygon class="st0" id="XMLID_43_" points="332.4,352.2 506.7,526.5 158.2,526.5 "/>
+        <polygon class="st0" id="XMLID_46_" points="855.2,526.5 506.7,526.5 680.9,352.2 "/>
+        <rect class="st0" height="348.5" id="XMLID_3_" width="697" x="158.2" y="526.5"/>
+        <g id="XMLID_2_"><polygon class="st1" id="XMLID_39_" points="332.4,352.2 158.2,526.5 158.2,352.2  "/>
+            <polygon class="st1" id="XMLID_44_" points="680.9,352.2 506.7,526.5 332.4,352.2  "/>
+            <polygon class="st1" id="XMLID_40_" points="855.2,352.2 855.2,526.5 680.9,352.2  "/></g>
+        <polygon class="st2" id="XMLID_64_" points="855.2,352.2 855.2,875 332.4,352.2 "/>
+        <g id="XMLID_1_"><polygon class="st1" id="XMLID_56_" points="331.8,338.9 319.9,339.7 158.2,352.2 144.8,178.4 242.1,261.9  "/>
+            <polygon class="st1" id="XMLID_49_" points="679.4,312.1 331.8,338.9 492.3,151.7  "/>
+            <polygon class="st0" id="XMLID_55_" points="492.3,151.7 331.8,338.9 242.1,261.9 144.8,178.4 157.7,177.5  "/>
+            <polygon class="st1" id="XMLID_45_" points="853.1,298.8 679.4,312.1 839.7,125  "/>
+            <polygon class="st0" id="XMLID_52_" points="839.7,125 679.4,312.1 492.3,151.7  "/>
+            <polygon class="st0" id="XMLID_53_" points="492.3,151.7 331.8,338.9 242.1,261.9 144.8,178.4 157.7,177.5  "/>
+            <polygon class="st0" id="XMLID_51_" points="839.7,125 679.4,312.1 492.3,151.7  "/></g>
+        <rect class="st1" height="232.3" id="XMLID_31_" width="580.9" x="216.2" y="584.6"/>
+        <linearGradient gradientUnits="userSpaceOnUse" id="XMLID_6_" x1="506.68" x2="506.68" y1="816.913" y2="860.4783"><stop offset="0" style="stop-color:#FFFFFF;stop-opacity:0.1"/>
+            <stop offset="1" style="stop-color:#000000;stop-opacity:0"/></linearGradient>
+        <rect class="st3" height="43.6" id="XMLID_30_" width="580.9" x="216.2" y="816.9"/>
+        <polygon class="st2" id="XMLID_62_"
+                 points="679.4,312.1 331.8,338.9 319.9,339.7 242.1,261.9 157.7,177.5 492.3,151.7 839.7,125   853.1,298.8 "/>
+        <linearGradient gradientUnits="userSpaceOnUse" id="XMLID_7_" x1="506.68" x2="506.68" y1="352.2172"
+                        y2="395.7824"><stop offset="0" style="stop-color:#FFFFFF;stop-opacity:0.1"/>
+            <stop offset="1" style="stop-color:#000000;stop-opacity:0"/></linearGradient>
+        <rect class="st4" height="43.6" id="XMLID_4_" width="697" x="158.2" y="352.2"/></svg>
+
+</template>
+
+
+<style lang="scss">
+    .dumonda-me-clap-icon {
+        fill: currentColor;
+    }
+</style>
