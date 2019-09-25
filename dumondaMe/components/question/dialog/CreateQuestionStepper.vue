27c41610a625e5055af881eb7fd5a7585267d480
roger <info@dumonda.me>
2019-09-25 10:23:40 +0200

    Added title to create question dialog on mobile.

--- dumondaMe/components/question/dialog/CreateQuestionStepper.vue
+++ dumondaMe/components/question/dialog/CreateQuestionStepper.vue
@@ -1,12 +1,17 @@
 <template>
-    <stepper-header>
-        <step :name="$t('common:question')" :number="1" :selected-step="selectedStep"
-              @navigate-to-step="(number) => $emit('navigate-to-step', number)">
-        </step>
-        <v-divider></v-divider>
-        <step :name="$t('common:topic')" :number="2" :selected-step="selectedStep">
-        </step>
-    </stepper-header>
+    <div>
+        <div id="mobile-create-question-dialog-header">
+            {{$t('pages:question.createDialog.title')}}
+        </div>
+        <stepper-header>
+            <step :name="$t('common:question')" :number="1" :selected-step="selectedStep"
+                  @navigate-to-step="(number) => $emit('navigate-to-step', number)">
+            </step>
+            <v-divider></v-divider>
+            <step :name="$t('common:topic')" :number="2" :selected-step="selectedStep">
+            </step>
+        </stepper-header>
+    </div>
 </template>
 
 <script>
@@ -18,3 +23,15 @@
         components: {StepperHeader, Step},
     }
 </script>
+
+<style lang="scss">
+    #mobile-create-question-dialog-header {
+        @media screen and (min-width: $xs) {
+            display: none;
+        }
+
+        padding: 16px 24px;
+        font-weight: 500;
+        border-bottom: 1px solid $divider;
+    }
+</style>
