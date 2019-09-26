2432e1519ca1ac7d480bd2499d8a36c79e3ba781
roger <info@dumonda.me>
2019-09-26 07:28:54 +0200

    Added scaffold for new topic page.

---
+++ dumondaMe/components/topic/Card.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="dumonda-me-topic-card ely-card">
+        <img :src="topicImage" class="topic-image"/>
+        <h2>{{topic}}</h2>
+        <div class="navigation-container">
+            <!--<v-btn rounded outlined color="primary">Fragen</v-btn>
+            <v-btn rounded outlined color="primary">Engagements</v-btn>-->
+            <v-btn small outlined fab color="primary">
+                <v-icon size="18">mdi-help</v-icon>
+            </v-btn>
+            <v-btn small outlined fab color="primary">
+                <v-icon size="18">mdi-human-handsup</v-icon>
+            </v-btn>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['topic', 'topicId'],
+        data() {
+            return {}
+        },
+        computed: {
+            topicImage() {
+                return `https://s3.eu-central-1.amazonaws.com/localtest.dumonda.me/topic/${this.topicId}/preview.jpg`;
+            }
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+
+<style lang="scss">
+    .dumonda-me-topic-card.ely-card {
+        max-width: 282px;
+        padding-right: 0;
+        padding-left: 0;
+        padding-top: 0;
+        margin-right: 18px;
+        margin-bottom: 18px;
+
+        .topic-image {
+            max-width: 100%;
+            border-top-left-radius: 4px;
+            border-top-right-radius: 4px;
+        }
+
+        h2 {
+            text-align: center;
+            padding: 0 8px;
+            font-weight: 500;
+            height: 60px;
+
+        }
+
+        .navigation-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-top: 8px;
+
+            .v-btn {
+                margin-right: 8px;
+                margin-left: 8px;
+                margin-bottom: 8px;
+            }
+        }
+    }
+</style>
---
+++ dumondaMe/pages/topics/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="dumonda-me-topics">
+        <card topic="Bildung & Lernen" :topic-id="'5932e562e0f5e000'"></card>
+        <card topic="Gesellschaft" :topic-id="'5945285f05691000'"></card>
+        <card topic="Gesundheit" :topic-id="'594528935ce91000'"></card>
+        <card topic="Konsum & Suffizienz" :topic-id="'qJItLlh8wrXZTgb'"></card>
+        <card topic="Kunst & Kultur" :topic-id="'59452a3ef8a91000'"></card>
+        <card topic="Landwirtschaft & Lebensmittel" :topic-id="'5945298a6b291000'"></card>
+        <card topic="Spiritualität & Bewusstsein" :topic-id="'59452a1840691000'"></card>
+        <card topic="Technologie" :topic-id="'5945287e93a91000'"></card>
+        <card topic="Umwelt" :topic-id="'593c1ecffb909000'"></card>
+        <card topic="Wirtschaft & Finanzen" :topic-id="'5932e90a1835e000'"></card>
+        <card topic="Wissenschaft" :topic-id="'594529f733291000'"></card>
+    </div>
+</template>
+
+<script>
+    import Card from '~/components/topic/Card';
+
+    export default {
+        async fetch({store, error}) {
+            /*try {
+                await store.dispatch(`setting/getSetting`);
+            } catch (e) {
+                if (e && e.request && e.request.res && e.request.res.statusCode) {
+                    error({statusCode: e.request.res.statusCode});
+                } else {
+                    error({statusCode: 600});
+                }
+            }
+            store.commit('toolbar/SHOW_BACK_BUTTON');*/
+        },
+        components: {Card}
+    }
+</script>
+
+<style lang="scss">
+    #dumonda-me-topics {
+        padding-top: 28px;
+        display: flex;
+        flex-wrap: wrap;
+
+    }
+</style>
