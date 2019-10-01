37c966ec0409c4369695e4d83e1762d64eed14de
roger <info@dumonda.me>
2019-10-01 11:07:39 +0200

    Added youtube video button.

--- dumondaMe/components/publicLandingPage/WhatIsDumondaMe.vue
+++ dumondaMe/components/publicLandingPage/WhatIsDumondaMe.vue
@@ -2,11 +2,13 @@
     <div class="public-why-dumonda-me explanation-element">
         <div class="public-image" @click="openExplainVideo" v-if="!showMobile">
             <img :src="explainVideo">
+            <img :src="videoButton" id="video-button">
         </div>
         <div class="public-description-container">
             <h2 class="title-description">{{$t('pages:landingPagePublic.whatIsDumondaMe.title')}}</h2>
             <div class="public-image" @click="openExplainVideo" v-if="showMobile">
                 <img :src="explainVideo">
+                <img :src="videoButton" id="video-button">
             </div>
             <div class="description">
                 {{$t('pages:landingPagePublic.whatIsDumondaMe.description')}}
@@ -25,6 +27,9 @@
             explainVideo() {
                 return `${process.env.staticUrl}/img/landingPagePublic/explainVideo.jpg`;
             },
+            videoButton() {
+                return `${process.env.staticUrl}/img/youtube.png`;
+            }
         },
         methods: {
             openExplainVideo() {
@@ -37,7 +42,23 @@
 <style lang="scss">
     .public-why-dumonda-me {
         .public-image {
+            position: relative;
             cursor: pointer;
+
+            img#video-button {
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                width: 70px;
+                margin-left: -35px;
+                margin-top: -25px;
+
+                @media screen and (max-width: 900px) {
+                    width: 60px;
+                    margin-left: -30px;
+                    margin-top: -21px;
+                }
+            }
         }
     }
 </style>
