d41dbbd4e8547b9ce04d5cec2713499e4488ea88
roger <info@dumonda.me>
2019-10-11 14:57:23 +0200

    Open menu on hover. Click on icon initiate navigation.

--- dumondaMe/components/feed/card/footer/menu/Commitment.vue
+++ dumondaMe/components/feed/card/footer/menu/Commitment.vue
@@ -1,7 +1,8 @@
 <template>
-    <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="280" min-width="290" offset-y>
+    <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="280" min-width="290" offset-y
+            open-on-hover open-delay="700" offset-overflow>
         <template v-slot:activator="{ on }">
-            <div v-on="on">
+            <div v-on="on" @click="goToCommitment()">
                 <slot name="icon"></slot>
             </div>
         </template>
