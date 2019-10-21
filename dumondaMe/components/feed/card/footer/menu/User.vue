ce5e84b4d6df6ea2c69198160f9ba9fcd663f760
roger <info@dumonda.me>
2019-10-21 15:56:23 +0200

    Show menu on mobile for anonymous user.

--- dumondaMe/components/feed/card/footer/menu/User.vue
+++ dumondaMe/components/feed/card/footer/menu/User.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="280" min-width="290" offset-y
-                open-on-hover open-delay="700" offset-overflow :disabled="$vuetify.breakpoint.smAndDown">
+                :open-on-hover="!$vuetify.breakpoint.smAndDown" open-delay="700" offset-overflow
+                :disabled="$vuetify.breakpoint.smAndDown && !user.isAnonymous">
             <template v-slot:activator="{ on }">
                 <div v-on="on" @click="goToProfile">
                     <slot name="icon"></slot>
