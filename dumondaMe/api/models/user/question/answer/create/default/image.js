fbb60244df17d99770cef0e41665f6b463e8dde5
roger <info@dumonda.me>
2019-10-25 09:28:51 +0200

    Using inside parameter for fit.

--- dumondaMe/api/models/user/question/answer/create/default/image.js
+++ dumondaMe/api/models/user/question/answer/create/default/image.js
@@ -7,15 +7,15 @@ const uploadImages = async function (imagePath, answerId) {
     if (typeof imagePath === 'string') {
         await image.uploadImage(2000, 3200, {
             quality: 80, originalImagePath: imagePath, cdnPath: `defaultAnswer/${answerId}/title.jpg`,
-            bucket: process.env.BUCKET_PUBLIC
+            bucket: process.env.BUCKET_PUBLIC, fit: 'inside'
         });
         await image.uploadImage(500, 800, {
             quality: 80, originalImagePath: imagePath, cdnPath: `defaultAnswer/${answerId}/500x800/title.jpg`,
-            bucket: process.env.BUCKET_PUBLIC
+            bucket: process.env.BUCKET_PUBLIC, fit: 'inside'
         });
         await image.uploadImage(1000, 1600, {
             quality: 80, originalImagePath: imagePath, cdnPath: `defaultAnswer/${answerId}/1000x1600/title.jpg`,
-            bucket: process.env.BUCKET_PUBLIC
+            bucket: process.env.BUCKET_PUBLIC, fit: 'inside'
         });
     }
 };
