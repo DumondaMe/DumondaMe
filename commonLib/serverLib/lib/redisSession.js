b1a7a1133a5f3869985efb451dfc5b14a9626bb9
roger <info@dumonda.me>
2019-10-25 17:52:52 +0200

    Updated connect redis to 4.0.3. Added redis 2.8.0. Updated express session to 1.17.0

--- commonLib/serverLib/lib/redisSession.js
+++ commonLib/serverLib/lib/redisSession.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let session = require('express-session');
+const redis = require('redis');
+const session = require('express-session');
 let RedisStore = require('connect-redis')(session);
 let logger = require('./logging').getLogger(__filename);
 
@@ -8,7 +9,8 @@ module.exports = function (settings, redisConfig) {
     let env = process.env.NODE_ENV || 'development';
     if ('testing' !== env) {
         logger.info('Set Redis for session store');
-        settings.store = new RedisStore(redisConfig);
+        let client = redis.createClient(redisConfig);
+        settings.store = new RedisStore({client});
     } else {
         logger.info('Not set Redis for session store. Use Memory');
     }
