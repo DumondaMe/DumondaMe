78d6b1a1ab07feff5684d6ec332bf4665fb6f29d
roger <info@dumonda.me>
2019-10-29 17:31:58 +0100

    Using proximity matching only for two words or more.

--- dumondaMe/api/models/search/commitment/index.js
+++ dumondaMe/api/models/search/commitment/index.js
@@ -7,7 +7,7 @@ const db = requireDb();
 
 const searchCommand = function (query, language, userId, skip, limit) {
     query = queryParser.cleanQuery(query);
-    let queryString = `Commitment.title:("${query.trim()}"~20)^5 ${queryParser.wordsQuery(query, 'Commitment.title:')}`;
+    let queryString = `${queryParser.proximityMatchingQuery(query.trim(), 'Commitment.title:(')} ${queryParser.wordsQuery(query, 'Commitment.title:')}`;
     return db.cypher().call(`apoc.index.search("entities", {queryString}) YIELD node AS commitment, weight`)
         .optionalMatch(`(commitment)<-[:WATCH]-(watchingUser:User)`)
         .optionalMatch(`(commitment)-[:BELONGS_TO_REGION]->(r:Region)`)
