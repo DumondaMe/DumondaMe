376ba24083be9cb53ffc04472385e9174994ec8d
roger <info@dumonda.me>
2019-11-28 09:06:02 +0100

    Scaffold to send more details in email notification. Detail for newQuestion email notification.

--- eMailService/src/notifications/index.js
+++ eMailService/src/notifications/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const db = requireDb();
+const previewNotification = require('./previewNotification');
+const titleNotification = require('./titleNotification');
 const eMail = require('dumonda-me-server-lib').eMail;
 const logger = require('dumonda-me-server-lib').logging.getLogger(__filename);
 
@@ -19,8 +21,11 @@ const sendEMails = async function (users) {
     for (let user of users) {
         try {
             let unsubscribeLink = `${process.env.DUMONDA_ME_DOMAIN}unsubscribe/notifications/${user.email}`;
+            let preview = await previewNotification.getPreviewOfNotification(user.notifications, user.language);
+            let title = await titleNotification.getTitleOfNotification(user.notifications, user.language);
             await eMail.sendEMail('notification',
-                {unsubscribeLink, numberOfNotifications: user.numberOfNotifications}, user.language, user.email);
+                {unsubscribeLink, numberOfNotifications: user.numberOfNotifications, preview, title},
+                user.language, user.email);
             logger.info(`Send new notification email to ${user.email}`);
         } catch (error) {
             failedNotifications.push(user.notificationIds);
@@ -40,10 +45,16 @@ const sendUnreadNotifications = async function () {
             .match(`(user:User:EMailNotificationEnabled)<-[:NOTIFIED]-(notification:Notification:Unread)`)
             .where(`NOT notification:EmailSent AND NOT EXISTS(user.disableNewNotificationEmail) ` +
                 `AND NOT user:HarvestingUser`)
+            .optionalMatch(`(notification)-[:ORIGINATOR_OF_NOTIFICATION]->(originator:User)`)
+            .with(`user, notification, collect(originator) AS originators`)
+            .optionalMatch(`(notification)-[:NOTIFICATION]->(notificationObject)`)
+            .with(`user, notification, originators, collect(notificationObject) AS notificationObjects`)
             .addCommand(` SET notification:EmailSent`)
             .return(`DISTINCT user.emailNormalized AS email, user.language AS language, 
                      COUNT(notification) AS numberOfNotifications, 
-                     collect(notification.notificationId) AS notificationIds`)
+                     collect(notification.notificationId) AS notificationIds,
+                     collect({notification: notification, originators: originators, 
+                     notificationObjects: notificationObjects}) AS notifications`)
             .orderBy(`user.emailNormalized`)
             .skip(skip)
             .limit(LIMIT).end().send();
