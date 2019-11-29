ff7ae8364d7ca0089f94341d754f3a0ae9d3a40a
roger <info@dumonda.me>
2019-11-29 10:06:33 +0100

    Added christina tschopp and using only project team as title.

--- dumondaMe/pages/aboutUs/index.vue
+++ dumondaMe/pages/aboutUs/index.vue
@@ -9,6 +9,7 @@
                 Verein DumondaMe ist das tragende Gefäss unseres Wirkens. Die Statuten des Vereins findest du <a
                         target="_blank" :href="bylaws">hier.</a>
             </p>
+            <div class="team-sub-title">Das Projektteam</div>
             <div class="about-user">
                 <div>
                     <img class="profile-image" :src="imageRoger"/>
@@ -23,6 +24,22 @@
                         Software Systemen. Es ist für mich eine Herzensangelegenheit, dass die neuen
                         Technologien zum Wohle der Allgemeinheit eingesetzt werden. Diese Zielsetzung war
                         für mich die Hauptmotivation, um das Projekt DumondaMe ins Leben zu rufen.
+                    </p>
+                </div>
+            </div>
+            <div class="about-user">
+                <div>
+                    <img class="profile-image" :src="imageJeannine"/>
+                </div>
+                <div>
+                    <h3 class="name">Jeannine Brutschin</h3>
+                    <div class="ely-function"></div>
+                    <div class="email-address without-function"><a
+                            href="mailto:jeannine@dumonda.me">jeannine@dumonda.me</a></div>
+                    <p class="about-text">Mit momo&ronja möchte ich zeigen, wie wir unser volles Potenzial leben und in
+                        Verbundenheit wirksam sein können. Wir alle können einen Beitrag zu einer leuchtenden Zukunft
+                        leisten, einfach indem wir uns trauen, wichtige Fragen zu stellen. Was inspiriert dich? Wie kann
+                        es gehen? Wer hilft mit? DumondaMe unterstützt uns darin, gemeinsam Antworten zu finden.
                     </p>
                 </div>
             </div>
@@ -43,8 +60,6 @@
                     </p>
                 </div>
             </div>
-            <div class="team-sub-title">Ganzheitliche Projektbegleitung durch
-                <a target="_blank" href="https://momoundronja.ch/">momo&ronja</a></div>
             <div class="about-user">
                 <div>
                     <img class="profile-image" :src="imageKim"/>
@@ -53,7 +68,7 @@
                     <h3 class="name">Kim Jana Degen</h3>
                     <div class="ely-function"></div>
                     <div class="email-address without-function"><a
-                            href="mailto:kimjana@momoundronja.ch">kimjana@momoundronja.ch</a></div>
+                            href="mailto:kimjana@dumonda.me">kimjana@dumonda.me</a></div>
                     <p class="about-text">Ich unterstütze mit momo&ronja die Verwirklichung von Herzensprojekten wie
                         DumondaMe. Diese Plattform erleichtert das Teilen von Wissen und Erfahrungen für den Aufbau
                         einer zukunftsfähigen Gesellschaft und genau das brauchen wir jetzt. Unser kraftvolles Teamwork
@@ -61,24 +76,6 @@
                     </p>
                 </div>
             </div>
-            <div class="about-user">
-                <div>
-                    <img class="profile-image" :src="imageJeannine"/>
-                </div>
-                <div>
-                    <h3 class="name">Jeannine Brutschin</h3>
-                    <div class="ely-function"></div>
-                    <div class="email-address without-function"><a
-                            href="mailto:jeannine@momoundronja.ch">jeannine@momoundronja.ch</a></div>
-                    <p class="about-text">Mit momo&ronja möchte ich zeigen, wie wir unser volles Potenzial leben und in
-                        Verbundenheit wirksam sein können. Wir alle können einen Beitrag zu einer leuchtenden Zukunft
-                        leisten, einfach indem wir uns trauen, wichtige Fragen zu stellen. Was inspiriert dich? Wie kann
-                        es gehen? Wer hilft mit? DumondaMe unterstützt uns darin, gemeinsam Antworten zu finden.
-                    </p>
-                </div>
-            </div>
-            <div class="team-sub-title">Holistische Erlebnisgestaltung mit
-                <a target="_blank" href="https://solanki.ch/">Solanki</a></div>
             <div class="about-user">
                 <div>
                     <img class="profile-image" :src="imageNils"/>
@@ -90,8 +87,27 @@
                             href="mailto:services@solanki.ch">services@solanki.ch</a></div>
                     <p class="about-text">Auf DumondaMe vernetzen sich Menschen, über alle Gesellschaftsbereiche
                         hinweg, auf der Suche nach schöpferischen Antworten auf die spannendsten und drängendsten Fragen
-                        unserer Zeit. Mit Solanki unterstütze ich das Team darin, die Vision gemeinsam zu entfalten und
+                        unserer Zeit. Ich unterstütze das Team darin, die Vision gemeinsam zu entfalten und
                         aus den Herzen und Köpfen heraus ein freudvolles (digitales) Erlebnis zu gestalten.
+                    </p>
+                </div>
+            </div>
+            <div class="about-user">
+                <div>
+                    <img class="profile-image" :src="imageChristina"/>
+                </div>
+                <div>
+                    <h3 class="name">Christina Tschopp</h3>
+                    <div class="ely-function"></div>
+                    <div class="email-address without-function"><a
+                            href="mailto:christina.tschopp@soziokratiezentrum.ch">christina.tschopp@soziokratiezentrum.ch</a>
+                    </div>
+                    <p class="about-text">Ich bin überzeugt, dass es neben Linkedin, Twitter und Facebook usw. eine
+                        Plattform braucht, welche werbefrei ist und nicht von Klickraten lebt. Die Fragen und die
+                        Sachlichkeit soll die Community leiten. Ich beobachte auf einigen Plattformen im Moment eine
+                        Verwässerung und Überschwemmung von zweckfremden Inhalten durch reine Profilierungsaktivitäten
+                        von Personen und Unternehmen, welche durch die Algorithmen der Plattformbetreiber befeuert
+                        werden.
                     </p>
                 </div>
             </div>
@@ -106,6 +122,7 @@
                 organizational form is sociocracy. The non-profit society DumondaMe is the supporting organization of
                 our work. You can find the statutes of the society <a target="_blank" :href="bylaws">here.</a>
             </p>
+            <div class="team-sub-title">The project team</div>
             <div class="about-user">
                 <div>
                     <img class="profile-image" :src="imageRoger"/>
@@ -119,6 +136,22 @@
                         development of complex software systems. It is a matter close to my heart that the new
                         technologies are used for the benefit of the general public. This objective was the main
                         motivation for me to start the project DumondaMe.
+                    </p>
+                </div>
+            </div>
+            <div class="about-user">
+                <div>
+                    <img class="profile-image" :src="imageJeannine"/>
+                </div>
+                <div>
+                    <h3 class="name">Jeannine Brutschin</h3>
+                    <div class="ely-function"></div>
+                    <div class="email-address without-function"><a
+                            href="mailto:jeannine@dumonda.me">jeannine@dumonda.me</a></div>
+                    <p class="about-text">With momo&ronja I would like to show how we can live our full potential and be
+                        effective in solidarity. We can all contribute to a bright future simply by daring to ask
+                        important questions. What inspires you? How can it work? Who helps? DumondaMe supports us in
+                        finding answers together.
                     </p>
                 </div>
             </div>
@@ -139,8 +172,6 @@
                     </p>
                 </div>
             </div>
-            <div class="team-sub-title">Ganzheitliche Projektbegleitung durch
-                <a target="_blank" href="https://momoundronja.ch/">momo&ronja</a></div>
             <div class="about-user">
                 <div>
                     <img class="profile-image" :src="imageKim"/>
@@ -149,7 +180,7 @@
                     <h3 class="name">Kim Jana Degen</h3>
                     <div class="ely-function"></div>
                     <div class="email-address without-function"><a
-                            href="mailto:kimjana@momoundronja.ch">kimjana@momoundronja.ch</a></div>
+                            href="mailto:kimjana@dumonda.me">kimjana@dumonda.me</a></div>
                     <p class="about-text">With momo&ronja I support the realization of heart projects like DumondaMe.
                         This platform facilitates the sharing of knowledge and experience to build a sustainable society
                         and that is exactly what we need now. Our powerful teamwork with the focus on the vision gives
@@ -157,24 +188,6 @@
                     </p>
                 </div>
             </div>
-            <div class="about-user">
-                <div>
-                    <img class="profile-image" :src="imageJeannine"/>
-                </div>
-                <div>
-                    <h3 class="name">Jeannine Brutschin</h3>
-                    <div class="ely-function"></div>
-                    <div class="email-address without-function"><a
-                            href="mailto:jeannine@momoundronja.ch">jeannine@momoundronja.ch</a></div>
-                    <p class="about-text">With momo&ronja I would like to show how we can live our full potential and be
-                        effective in solidarity. We can all contribute to a bright future simply by daring to ask
-                        important questions. What inspires you? How can it work? Who helps? DumondaMe supports us in
-                        finding answers together.
-                    </p>
-                </div>
-            </div>
-            <div class="team-sub-title">Holistic experience design with
-                <a target="_blank" href="https://solanki.ch/">Solanki</a></div>
             <div class="about-user">
                 <div>
                     <img class="profile-image" :src="imageNils"/>
@@ -188,6 +201,24 @@
                         answers to the most exciting and urgent questions of our time. With Solanki, I support the team
                         in unfolding the vision together and creating a joyful (digital) experience out of the hearts
                         and minds.
+                    </p>
+                </div>
+            </div>
+            <div class="about-user">
+                <div>
+                    <img class="profile-image" :src="imageChristina"/>
+                </div>
+                <div>
+                    <h3 class="name">Christina Tschopp</h3>
+                    <div class="ely-function"></div>
+                    <div class="email-address without-function"><a
+                            href="mailto:christina.tschopp@soziokratiezentrum.ch">christina.tschopp@soziokratiezentrum.ch</a>
+                    </div>
+                    <p class="about-text">I'm convinced that besides Linkedin, Twitter and Facebook etc. it needs a
+                        platform that is ad-free and doesn't live on click rates. The questions and the objectivity
+                        should guide the community. On some platforms I am currently observing a dilution and flooding
+                        of unwanted content through pure profiling activities of individuals and companies, which are
+                        fired by the algorithms of the platform operators.
                     </p>
                 </div>
             </div>
@@ -226,6 +257,9 @@
             },
             imageJeannine() {
                 return `${process.env.staticUrl}/img/about/jeannine_brutschin.jpg`
+            },
+            imageChristina() {
+                return `${process.env.staticUrl}/img/about/christina_tschopp.jpg`
             },
             imageNils() {
                 return `${process.env.staticUrl}/img/about/nils_solanki.jpg`
