811d833237eff1ce3eea4d84a0c524ddd136f660
roger <info@dumonda.me>
2019-11-30 17:35:08 +0100

    Removed disableInviteAnswerQuestionNotification and disableNewNotificationEmail. Added setting interval to send email notifications.

--- commonLib/serverTestUtil/lib/dbDSL.js
+++ commonLib/serverTestUtil/lib/dbDSL.js
@@ -23,13 +23,13 @@ let init = function (numberOfUser, isDumondaMeAdmin) {
     return db.clearDatabase().then(function () {
         dbConnectionHandling.getCommands().push(db.cypher().create(`(:User:EMailNotificationEnabled {email: 'user@irgendwo.ch', emailNormalized: 'user@irgendwo.ch', password: '$2a$10$JlKlyw9RSpt3.nt78L6VCe0Kw5KW4SPRaCGSPMmpW821opXpMgKAm', 
         name: 'user Meier', surname: 'Meier', forename:'user', userDescription: 'superman', userId: '1', lastSetupAccount: 500, dumondaMeAdmin: {dumondaMeAdmin}, language: 'de', languages: ['de'],
-        userLocationDescription: 'irgendwo', privacyMode: 'public', showProfileActivity: true})`)
+        userLocationDescription: 'irgendwo', privacyMode: 'public', showProfileActivity: true, emailNotificationInterval: 86400})`)
             .end({dumondaMeAdmin: isDumondaMeAdmin}).getCommand());
         for (i = 0; i < numberOfUser - 1; i++) {
             userId = i + 2;
             dbConnectionHandling.getCommands().push(db.cypher().create(`(:User:EMailNotificationEnabled {name: 'user Meier${userId}', surname: 'Meier${userId}', forename:'user', userDescription: 'superman${userId}', 
             password: '$2a$10$JlKlyw9RSpt3.nt78L6VCe0Kw5KW4SPRaCGSPMmpW821opXpMgKAm', userId: '${userId}', lastSetupAccount: 500, email: 'user${userId}@irgendwo.ch', 
-            language: 'de', languages: ['de'], emailNormalized: 'user${userId}@irgendwo.ch', privacyMode: 'public', showProfileActivity: true})`).end().getCommand());
+            language: 'de', languages: ['de'], emailNormalized: 'user${userId}@irgendwo.ch', privacyMode: 'public', showProfileActivity: true, emailNotificationInterval: 86400})`).end().getCommand());
         }
     });
 };
@@ -123,7 +123,6 @@ module.exports = {
     notificationRequestAdminOfCommitment: notification.requestAdminOfCommitment,
 
     disableEMailNotification: eMailNotificationSettings.disableEMailNotification,
-    disableEMailNotificationForInvitedUser: eMailNotificationSettings.disableEMailNotificationForInvitedUser,
-    disableInviteAnswerQuestionNotification: eMailNotificationSettings.disableInviteAnswerQuestionNotification,
-    disableNewNotificationEmail: eMailNotificationSettings.disableNewNotificationEmail
+    emailNotificationSetEMailInterval: eMailNotificationSettings.setEMailInterval,
+    disableEMailNotificationForInvitedUser: eMailNotificationSettings.disableEMailNotificationForInvitedUser
 };
--- dumondaMe/components/setting/EmailNotification.vue
+++ dumondaMe/components/setting/EmailNotification.vue
@@ -2,24 +2,25 @@
     <div class="email-notification-setting-container">
         <div class="security-sub-title">{{$t('pages:settings.email.emailNotification.title')}}</div>
         <div id="top-checkbox">
-            <v-checkbox v-model="enabledEmailNotifications"
-                        :label="$t('pages:settings.email.emailNotification.emailNotifications')"
-                        color="primary" :disabled="loading">
-            </v-checkbox>
-        </div>
-        <v-divider class="email-notification-divider"></v-divider>
-        <div v-if="enabledEmailNotifications" id="sub-checkboxes">
-            <v-checkbox v-model="enableInviteToAnswerQuestion"
-                        :label="$t('pages:settings.email.emailNotification.inviteToAnswerQuestion')"
-                        color="primary" :disabled="loading || !enabledEmailNotifications">
-            </v-checkbox>
-            <v-checkbox v-model="enableNewNotifications"
-                        :label="$t('pages:settings.email.emailNotification.newNotifications')"
-                        color="primary" :disabled="loading || !enabledEmailNotifications">
-            </v-checkbox>
-        </div>
-        <div v-else class="email-disabled-description">
-            {{$t('pages:settings.email.emailNotification.disabledDescription')}}
+            <div class="notification-description">{{$t('pages:settings.email.emailNotification.description')}}</div>
+            <v-radio-group v-model="emailNotificationInterval" column id="select-interval-container"
+                           @change="setEmailNotificationInterval" :loading="loading">
+                <v-radio :label="$t('pages:settings.email.emailNotification.never')"
+                         value="never" color="primary">
+                </v-radio>
+                <v-radio :label="$t('pages:settings.email.emailNotification.hour')"
+                         value="hour" color="primary">
+                </v-radio>
+                <v-radio :label="$t('pages:settings.email.emailNotification.day')"
+                         value="day" color="primary">
+                </v-radio>
+                <v-radio :label="$t('pages:settings.email.emailNotification.3days')"
+                         value="3days" color="primary">
+                </v-radio>
+                <v-radio :label="$t('pages:settings.email.emailNotification.week')"
+                         value="week" color="primary">
+                </v-radio>
+            </v-radio-group>
         </div>
     </div>
 </template>
@@ -28,57 +29,16 @@
     export default {
         data() {
             return {
-                enabledEmailNotifications: this.$store.state.setting.emailNotifications.enabledEmailNotifications,
-                enableInviteToAnswerQuestion: this.$store.state.setting.emailNotifications.enableInviteToAnswerQuestion,
-                enableNewNotifications: this.$store.state.setting.emailNotifications.enableNewNotifications,
-                loading: false, previouslyEnabledEmailNotifications: false
+                emailNotificationInterval: this.$store.state.setting.emailNotificationInterval, loading: false
             }
-        },
-        mounted() {
-            this.disableAllNotifications();
         },
         methods: {
-            async setEmailNotifications(enableEmailNotifications, enableInviteToAnswerQuestion, enableNewNotifications) {
+            async setEmailNotificationInterval() {
                 try {
                     this.loading = true;
-                    this.previouslyEnabledEmailNotifications = false;
-                    await this.$store.dispatch('setting/setEmailNotification', {
-                        enableEmailNotifications, enableInviteToAnswerQuestion, enableNewNotifications
-                    });
+                    await this.$store.dispatch('setting/setEmailNotification', this.emailNotificationInterval);
                 } finally {
                     this.loading = false;
-                }
-            },
-            disableAllNotifications() {
-                if (!this.enableInviteToAnswerQuestion && !this.enableNewNotifications) {
-                    this.enabledEmailNotifications = false;
-                    return true;
-                }
-                return false;
-            }
-        },
-        watch: {
-            async enabledEmailNotifications() {
-                await this.setEmailNotifications(this.enabledEmailNotifications, this.enabledEmailNotifications,
-                    this.enabledEmailNotifications);
-                if (this.enabledEmailNotifications) {
-                    this.previouslyEnabledEmailNotifications = true;
-                    this.enableInviteToAnswerQuestion = true;
-                    this.enableNewNotifications = true;
-                }
-            },
-            async enableInviteToAnswerQuestion() {
-                if ((!this.previouslyEnabledEmailNotifications || !this.enableInviteToAnswerQuestion) &&
-                    !this.disableAllNotifications()) {
-                    await this.setEmailNotifications(this.enabledEmailNotifications, this.enableInviteToAnswerQuestion,
-                        this.enableNewNotifications);
-                }
-            },
-            async enableNewNotifications() {
-                if ((!this.previouslyEnabledEmailNotifications || !this.enableNewNotifications) &&
-                    !this.disableAllNotifications()) {
-                    await this.setEmailNotifications(this.enabledEmailNotifications, this.enableInviteToAnswerQuestion,
-                        this.enableNewNotifications);
                 }
             }
         }
@@ -88,31 +48,9 @@
 <style lang="scss">
     .email-notification-setting-container {
         #top-checkbox {
-            .v-input {
-                margin-top: 0;
-
-                .v-input__slot {
-                    margin-bottom: 4px;
-                }
+            .notification-description {
+                font-weight: 300;
             }
-        }
-
-        #sub-checkboxes {
-            .v-input {
-                margin-top: 0;
-
-                .v-input__slot {
-                    margin-bottom: 4px;
-                }
-            }
-        }
-
-        .email-notification-divider {
-            margin-bottom: 32px;
-        }
-
-        .email-disabled-description {
-            font-weight: 300;
         }
     }
 </style>
