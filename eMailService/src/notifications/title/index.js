20146b911f76e3febb5171ed0044ecc08cd0b31e
roger <info@dumonda.me>
2019-11-30 12:52:14 +0100

    Handling anonymous persons. Added description and text when answer has been created.

--- eMailService/src/notifications/title/index.js
+++ eMailService/src/notifications/title/index.js
@@ -1,19 +1,21 @@
 const i18next = require('i18next');
+const user = require('../user');
 
 const createdAnswer = function (notification, language) {
     let answerCreator = notification.originators[0];
     let question = notification.notificationObjects.find((n) => typeof n.questionId === 'string');
     return i18next.t('notification:createdAnswer.title',
-        {name: answerCreator.name, question: question.question, lng: language});
+        {name: user.getUserName(answerCreator, language), question: question.question, lng: language});
 };
 
 const newQuestion = function (notification, language) {
     let questionCreator = notification.originators[0];
-    return i18next.t('notification:newQuestion.title', {name: questionCreator.name, lng: language});
+    return i18next.t('notification:newQuestion.title',
+        {name: user.getUserName(questionCreator, language), lng: language});
 };
 
 const addedToTrustCircle = function (notification, language) {
-    let user = notification.originators[0];
+    let user = notification.originators[0].originator;
     return i18next.t('notification:addedToTrustCircle.title', {name: user.name, lng: language});
 };
 
