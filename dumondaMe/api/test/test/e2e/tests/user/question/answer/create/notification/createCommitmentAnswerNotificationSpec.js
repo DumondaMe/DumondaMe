12b14f32642844cd68e605285f97c92ba414a44d
roger <info@dumonda.me>
2019-11-30 11:47:05 +0100

    To not create notification twice for user created a question and answered by user of trust circle.

--- dumondaMe/api/test/test/e2e/tests/user/question/answer/create/notification/createCommitmentAnswerNotificationSpec.js
+++ dumondaMe/api/test/test/e2e/tests/user/question/answer/create/notification/createCommitmentAnswerNotificationSpec.js
@@ -208,6 +208,36 @@ describe('Notification when user creates commitment answer for a question', func
         notification[1].created.should.least(startTime);
     });
 
+    it('Create a notification only once when user created question and lists the creator of the answer in the Trust Cirlce.', async function () {
+        dbDsl.createContactConnection('2', '1');
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/question/answer/commitment/1', {
+            commitmentId: '10', description: 'This is a commitment', language: 'de'
+        });
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(notifiedUser:User)<-[:NOTIFIED]-
+        (notification:Notification:Unread {type: 'createdAnswer'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(user)`)
+            .match(`(notification)-[:NOTIFICATION]->(question:Question)`)
+            .match(`(notification)-[:NOTIFICATION]->(answer:Answer)`)
+            .match(`(notification)-[:NOTIFICATION]->(commitment:Commitment)`)
+            .return('DISTINCT notification, question, answer, commitment, user.userId AS userId, relNot.created AS created,' +
+                'notifiedUser.userId AS notifiedUserId')
+            .orderBy(`notifiedUserId`)
+            .end().send();
+
+        notification.length.should.equals(1);
+        should.exist(notification[0].notification.notificationId);
+        notification[0].notification.created.should.least(startTime);
+        notification[0].question.questionId.should.equals('1');
+        notification[0].commitment.commitmentId.should.equals('10');
+        notification[0].answer.description.should.equals('This is a commitment');
+        notification[0].userId.should.equals('1');
+        notification[0].notifiedUserId.should.equals('2');
+        notification[0].created.should.least(startTime);
+    });
+
     it('Create no notification when commitment answer has been created by creator of question', async function () {
         await dbDsl.sendToDb();
         await requestHandler.login(users.validUser2);
--- dumondaMe/api/test/test/e2e/tests/user/question/answer/create/notification/createYoutubeAnswerNotificationSpec.js
+++ dumondaMe/api/test/test/e2e/tests/user/question/answer/create/notification/createYoutubeAnswerNotificationSpec.js
@@ -189,6 +189,35 @@ describe('Notification when user creates youtube answers for a question', functi
         notification[1].created.should.least(startTime);
     });
 
+    it('Create a notification only once when user created the question and lists the creator of the answer in the Trust Cirlce.', async function () {
+        sandbox.stub(rp, 'get');
+        dbDsl.createContactConnection('2', '1');
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/question/answer/youtube/1', {
+            link: 'https://www.youtube.com/watch?v=Lhku7ZBWEK8',
+            title: 'titleYoutube', description: 'descriptionYoutube'
+        });
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(notifiedUser:User)<-[:NOTIFIED]-
+        (notification:Notification:Unread {type: 'createdAnswer'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(user:User)`)
+            .match(`(notification)-[:NOTIFICATION]->(question:Question)`)
+            .match(`(notification)-[:NOTIFICATION]->(answer:Answer)`)
+            .return('DISTINCT notification, question, answer, user.userId AS userId, notifiedUser.userId AS notifiedUserId, ' +
+                'relNot.created AS created')
+            .orderBy(`notifiedUserId`)
+            .end().send();
+        notification.length.should.equals(1);
+        should.exist(notification[0].notification.notificationId);
+        notification[0].notification.created.should.least(startTime);
+        notification[0].question.questionId.should.equals('1');
+        notification[0].answer.title.should.equals('titleYoutube');
+        notification[0].userId.should.equals('1');
+        notification[0].notifiedUserId.should.equals('2');
+        notification[0].created.should.least(startTime);
+    });
+
     it('Create no notification when youtube answer has been created by creator of question', async function () {
         await dbDsl.sendToDb();
         await requestHandler.login(users.validUser2);
