edf6950a8c56beb3f3dab2111bc8af7d81e86973
roger <info@dumonda.me>
2019-12-20 16:25:13 +0100

    Notify other creators of notes when a new note has been created for an answer.

--- dumondaMe/api/test/test/e2e/tests/user/question/answer/note/createNoteNotificationSpec.js
+++ dumondaMe/api/test/test/e2e/tests/user/question/answer/note/createNoteNotificationSpec.js
@@ -55,6 +55,35 @@ describe('Notification when user creates a note for an answer', function () {
         notification[0].created.should.least(startTime);
     });
 
+    it('Create notification for all users which have created a note', async function () {
+        dbDsl.createDefaultAnswer('5', {
+            creatorId: '2', questionId:'1', answer: 'Answer'
+        });
+        dbDsl.createNote('50', {answerId: '5', creatorId: '2', created: 555});
+        dbDsl.createNote('51', {answerId: '5', creatorId: '3', created: 556});
+        dbDsl.createNote('52', {answerId: '5', creatorId: '4', created: 557});
+        dbDsl.createNote('53', {answerId: '5', creatorId: '4', created: 558});
+        dbDsl.createNote('54', {answerId: '5', creatorId: '4', created: 558});
+        dbDsl.createNote('55', {answerId: '5', creatorId: '1', created: 555});
+        await dbDsl.sendToDb();
+        await requestHandler.login(users.validUser);
+        let res = await requestHandler.post('/api/user/question/answer/note', {answerId: '5', text: 'This is a note'});
+        res.status.should.equal(200);
+
+        let notification = await db.cypher().match(`(notified:User)<-[:NOTIFIED]-
+        (notification:Notification:Unread {type: 'createdNote'})-[relNot:ORIGINATOR_OF_NOTIFICATION]->(originator)`)
+            .match(`(notification)-[:NOTIFICATION]->(question:Question)`)
+            .match(`(notification)-[:NOTIFICATION]->(answer:Answer)`)
+            .match(`(notification)-[:NOTIFICATION]->(note:Note)`)
+            .return('DISTINCT notification, question, answer, note, originator, notified')
+            .orderBy('notified.userId DESC')
+            .end().send();
+        notification.length.should.equals(3);
+        notification[0].notified.userId.should.equals('4');
+        notification[1].notified.userId.should.equals('3');
+        notification[2].notified.userId.should.equals('2');
+    });
+
     it('No notification when note for answer has been created by creator of answer', async function () {
         dbDsl.createDefaultAnswer('5', {
             creatorId: '2', questionId:'1', answer: 'Answer'
