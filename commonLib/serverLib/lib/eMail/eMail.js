46f1139cb0763982b9af7aa6d638fee1243733cc
roger <info@dumonda.me>
2020-11-24 19:09:37 +0100

    chg: added maildev for handling emails in local development.

--- commonLib/serverLib/lib/eMail/eMail.js
+++ commonLib/serverLib/lib/eMail/eMail.js
@@ -5,16 +5,26 @@ let AWS = require('aws-sdk');
 let _ = require('lodash');
 let logger = require('../logging').getLogger(__filename);
 
-if ('production' === process.env.NODE_ENV || 'development' === process.env.NODE_ENV) {
-    AWS.config.credentials = new AWS.EC2MetadataCredentials({
-        httpOptions: {timeout: 10000}
+let transporter;
+
+if ('maildev' === process.env.EMAIL_TYPE) {
+    transporter = nodemailer.createTransport({
+        host: 'localhost',
+        port: process.env.MAILDEV_PORT,
+        ignoreTLS: true
+    })
+} else {
+    if ('production' === process.env.NODE_ENV || 'development' === process.env.NODE_ENV) {
+        AWS.config.credentials = new AWS.EC2MetadataCredentials({
+            httpOptions: {timeout: 10000}
+        });
+    }
+    AWS.config.region = process.env.AWS_REGION_EMAIL;
+
+    transporter = nodemailer.createTransport({
+        SES: new AWS.SES({apiVersion: '2010-12-01'}), sendingRate: 8
     });
 }
-AWS.config.region = process.env.AWS_REGION_EMAIL;
-
-let transporter = nodemailer.createTransport({
-    SES: new AWS.SES({apiVersion: '2010-12-01'}), sendingRate: 8
-});
 
 let emailTemplates = require('./templates');
 
